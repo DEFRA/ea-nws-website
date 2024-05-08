0f69dbf49d705566f35af187bc30ed82f9992e97
thomas barron <thomas.barron@uk.leidos.com>
2024-05-08 12:52:36 +0000

    amend pages

--- frontend/src/pages/SignOutAutomatically.js
+++ frontend/src/pages/SignOutAutomatically.js
@@ -1,7 +1,7 @@
 import Header from "../gov-uk-components/Header";
 import Footer from "../gov-uk-components/Footer";
 import Button from "../gov-uk-components/Button";
-export default function SignOutAutomatically(){
+export default function SignOutAutomatically() {
 
         function test() {
             console.log("hello")

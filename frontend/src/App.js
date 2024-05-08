d7a46a7808796b84f389d22babf59e9d4b7ed6f0
thomas barron <thomas.barron@uk.leidos.com>
2024-05-08 12:52:30 +0000

    add import of new page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import SignOutAutomatically from "./pages/SignOutAutomatically";
 import SignOutManually from "./pages/SignOutManually"
+import SignBackIn from "./pages/SignBackIn";
 import StartPage from "./pages/StartPage";
 
 export default function App() {
   return (
     <>
-      <SignOutManually/>
+      <SignBackIn/>
     </>
   );
 }

4e72b32761afe89575f2300f5368759647a6528e
thomas barron <thomas.barron@uk.leidos.com>
2024-05-08 12:40:06 +0000

    add import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import SignOutAutomatically from "./pages/SignOutAutomatically";
+import SignOutManually from "./pages/SignOutManually"
 import StartPage from "./pages/StartPage";
 
 export default function App() {
   return (
     <>
-      <SignOutAutomatically/>
+      <SignOutManually/>
     </>
   );
 }

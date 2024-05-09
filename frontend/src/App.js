5e84dd14336eff31bd479ebc0108b274efb1d211
cameron.purves <cameron.purves@leidos.com>
2024-05-09 13:30:48 +0000

    added routing setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
-import ConfirmDeleteContactDetailsPage from "./pages/ContactDetails/ConfirmDeleteContactDetailsPage";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import routes from "./routes/routes";
 
 export default function App() {
   return (
-    <>
-      <ConfirmDeleteContactDetailsPage />
-    </>
+    <BrowserRouter basename="/">
+      <Routes>
+        {routes.map((route, index) => (
+          <Route key={index} path={route.path} element={route.component} />
+        ))}
+      </Routes>
+    </BrowserRouter>
   );
 }

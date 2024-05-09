7df82431a4e70c9e3c7ee90d401031ac246f1b5e
thomas barron <thomas.barron@uk.leidos.com>
2024-05-09 14:30:07 +0000

    change how routes are set up

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -4,9 +4,25 @@ import SignOutManually from "../pages/SignOutManually";
 import SignOutAutomatically from "../pages/SignOutAutomatically";
 import SignBackIn from "../pages/SignBackIn";
 
-const routes = [{ path: "/start", component: <StartPage /> },
-{path: "/signoutmanually", component: <SignOutManually/>},
-{path: "/signoutautomatically", component: <SignOutAutomatically/>},
-{path:"/signbackin", component: <SignBackIn/>}];
+//start routes
+const startRoutes = [{ path: "/start", component: <StartPage /> }]
+
+//contact routes
+const contactRoutes = [
+{
+    path: "/signoutmanually", 
+    component: <SignOutManually/>
+},
+{
+    path: "/signoutautomatically",
+    component: <SignOutAutomatically/>
+},
+{
+    path:"/signbackin",
+     component: <SignBackIn/>
+}
+];
+
+const routes = [...startRoutes,...contactRoutes]
 
 export default routes;

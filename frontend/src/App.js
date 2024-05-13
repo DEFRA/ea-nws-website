a2474d570c5b23154340196d903b7a8d1a1586f7
thomas barron <thomas.barron@uk.leidos.com>
2024-05-13 14:39:41 +0000

    getting sign in only pages to render propely

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import routes from "./routes/routes";
+import userIsSigndout from "./pages/signOut/checkUserSignIn"; 
+import SignBackIn from "./pages/signOut/SignBackIn";
+
+
 
 export default function App() {
+  const test_var = false
+  // bug here for not working on what pages to render
+  if (userIsSigndout === true && (routes.path !== "/start") && (routes.path !== "/signinpage") && (routes.path !== "checkyouremailpage")) {
+    return <SignBackIn/>;
+  }
+  else{
+
   return (
     <BrowserRouter basename="/">
       <Routes>
@@ -12,4 +23,5 @@ export default function App() {
       </Routes>
     </BrowserRouter>
   );
+}
 }

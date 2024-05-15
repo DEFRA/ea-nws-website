dec5b658f9da7fc61223b366fd94437f0a867314
camille.lecuyer <camille.lecuyer@leidos.com>
2024-05-15 13:52:39 +0000

    apiservice space build errors

--- backend/server/services/ApiService.js
+++ backend/server/services/ApiService.js
@@ -1,23 +1,21 @@
 const apiCall = async (raw, call) => {
-    let responseData;
-    const url = "http://localhost:9000/"+ call
-    try{
-        const response = await fetch(url, 
-        {
-          method: "POST",
-          mode: 'cors',
-          credentials: 'same-origin',
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: raw,
-        })
-        responseData = await response.json();        
-      }
-      catch (error) {
-        console.log("ERROR: ", error);
-      }
-      return responseData;
-}
+  let responseData;
+  const url = "http://localhost:9000/" + call;
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      mode: "cors",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: raw,
+    });
+    responseData = await response.json();
+  } catch (error) {
+    console.log("ERROR: ", error);
+  }
+  return responseData;
+};
 
 module.exports = apiCall;

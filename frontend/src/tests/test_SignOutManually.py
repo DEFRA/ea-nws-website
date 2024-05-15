7de1f73fc9a2d04fcdfa983a2e659364bef495c9
thomas barron <thomas.barron@uk.leidos.com>
2024-05-15 14:28:36 +0000

    signout manually tests

--- frontend/src/tests/test_SignOutManually.py
+++ frontend/src/tests/test_SignOutManually.py
@@ -1,5 +1,6 @@
 from re import A
 import pytest
+from requests import get
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
@@ -11,3 +12,10 @@ def test_signOutManually_render(get_browser):
     assert browser.title == "React App"
     assert "You've signed out" in browser.page_source
     assert browser.current_url == url
+
+
+def test_signOutManually_button(get_browser):
+    browser = get_browser
+    browser.get(url)
+    browser.find_element(By.CLASS_NAME,"govuk-button").click()
+    assert browser.current_url == "http://localhost:3000/SignInPage"

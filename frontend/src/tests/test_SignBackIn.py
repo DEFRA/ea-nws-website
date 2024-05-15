a160b696ca18e7dc3bbf4800cdd02a22b3af16f4
thomas barron <thomas.barron@uk.leidos.com>
2024-05-15 13:59:44 +0000

    write tests

---
+++ frontend/src/tests/test_SignBackIn.py
@@ -0,0 +1,20 @@
+from re import A
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+url = "http://localhost:3000/SignBackIn"
+def test_signBackIn_render(get_browser):
+    browser = get_browser
+    browser.get(url)
+    assert browser.title == "React App"
+    assert "You need to sign back in to view this page" in browser.page_source
+    assert browser.current_url == url
+
+
+def test_signBackIn_SignIn_button(get_browser):
+    browser = get_browser
+    browser.get(url)
+    browser.find_element(By.CLASS_NAME,"govuk-button").click()
+    assert browser.current_url == "http://localhost:3000/SignInPage"

6cb9e6963006a139ba0d56390c84b03f5ee2c9d5
thomas barron <thomas.barron@uk.leidos.com>
2024-05-15 11:06:29 +0000

    keep user signed out of pages that need authemtication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,9 +7,14 @@ import SignBackIn from "./pages/signOut/SignBackIn";
 
 
 export default function App() {
-  const test_var = false
-  // bug here for not working on what pages to render
-  if (userIsSigndout === true && (routes.path !== "/start") && (routes.path !== "/signinpage") && (routes.path !== "checkyouremailpage")) {
+  
+  
+  let userLoggedout = userIsSigndout()
+  //for testing
+  console.log(userLoggedout)
+  //this if statement may increase as non authentication needed pages are developed
+  if ((userLoggedout === true) && (window.location.href !== "http://localhost:3000/Start" && window.location.href !== "http://localhost:3000/SignInPage")) {
+    console.log(window.location.href)
     return <SignBackIn/>;
   }
   else{
@@ -17,9 +22,11 @@ export default function App() {
   return (
     <BrowserRouter basename="/">
       <Routes>
+        
         {routes.map((route, index) => (
           <Route key={index} path={route.path} element={route.component} />
         ))}
+        
       </Routes>
     </BrowserRouter>
   );

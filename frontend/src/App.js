a7f6620eb1f6ffdfe520117547451ffd6e9e5cff
thomas barron <thomas.barron@uk.leidos.com>
2024-05-17 08:33:39 +0000

    setting up new routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,8 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import routes from "./routes/routes";
-import SignBackIn from "./pages/signOut/SignBackIn";
-import userIsSigndout from "./services/CheckUserSignInService"
-
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import SignBackIn from './pages/signOut/SignBackIn'
+import { routes, unAuthRoutes } from './routes/routes'
+import userIsSigndout from './services/CheckUserSignInService'
 
 export default function App() {
   /* the two var bellow checks if the user is logged out. 
@@ -12,28 +11,58 @@ export default function App() {
    cant access some pages without user being present
    takes too long to log into account just to test pages
    each test file says what comment to comment/uncomment for each test*/
-  
+
   //let userLoggedout = false //use for testing
   let userLoggedout = userIsSigndout() // use  this for actual program
-  
+
   console.log(userLoggedout)
   //this if statement may increase as non authentication needed pages are developed
-  if ((userLoggedout === true) && (window.location.href !== "http://localhost:3000/Start" && window.location.href !== "http://localhost:3000/SignInPage")) {
-    console.log(window.location.href)
-    return <SignBackIn/>;
-  }
-  else{
+  // if (
+  //   userLoggedout === true &&
+  //   window.location.href !== 'http://localhost:3000/Start' &&
+  //   window.location.href !== 'http://localhost:3000/SignInPage'
+  // ) {
+  //   console.log(window.location.href)
+  //   return <SignBackIn />
+  // } else {
 
-  return (
+  return userLoggedout ? (
     <BrowserRouter basename="/">
       <Routes>
-        
+        {unAuthRoutes.map((unAuthRoutes, index) => (
+          <Route key={index} path="/SignBackIn" element=<SignBackIn /> />
+        ))}
+      </Routes>
+    </BrowserRouter>
+  ) : (
+    <BrowserRouter basename="/">
+      <Routes>
         {routes.map((route, index) => (
           <Route key={index} path={route.path} element={route.component} />
         ))}
-        
       </Routes>
     </BrowserRouter>
-  );
+  )
+
+  // return (
+  //   <BrowserRouter basename='/'>
+  //     <Routes>
+  //     {unAuthRoutes.map((route,index) = (
+  //       Route key={index} path={unAuthRoutes.path} element={unAuthRoutes.component} />
+  //     ))}
+  //     </Routes>
+  //   </BrowserRouter>
+  // ):
+
+  // return (
+  //   <BrowserRouter basename="/">
+  //     <Routes>
+  //       {routes.map((route, index) => (
+  //         <Route key={index} path={route.path} element={route.component} />
+  //       ))}
+  //     </Routes>
+  //   </BrowserRouter>
+  // )
 }
-}
+
+//}
--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -1,51 +1,59 @@
-import React from "react";
-import StartPage from "../pages/StartPage";
-import SignOutManually from "../pages/signOut/SignOutManually";
-import SignOutAutomatically from "../pages/signOut/SignOutAutomatically";
-import SignBackIn from "../pages/signOut/SignBackIn";
-import SignInPage from "../pages/signIn/SignInStartPage";
-import CheckYourEmailPage from "../pages/signIn/SignInValidatePage";
-import InitialEmailRegistrationPage from "../pages/register/InitialEmailRegistrationPage";
-import ValidateEmailForRegistration from "../pages/register/ValidateEmailForRegistration";
+import React from 'react'
+import StartPage from '../pages/StartPage'
+import InitialEmailRegistrationPage from '../pages/register/InitialEmailRegistrationPage'
+import ValidateEmailForRegistration from '../pages/register/ValidateEmailForRegistration'
+import SignInPage from '../pages/signIn/SignInStartPage'
+import CheckYourEmailPage from '../pages/signIn/SignInValidatePage'
+import SignBackIn from '../pages/signOut/SignBackIn'
+import SignOutAutomatically from '../pages/signOut/SignOutAutomatically'
+import SignOutManually from '../pages/signOut/SignOutManually'
 
 //start routes
-const startRoutes = [{ path: "/start", component: <StartPage /> }]
+
+const authenticatedRoutes = [
+  {
+    path: '/SignOutManually',
+    component: <SignOutManually />
+  },
+  {
+    path: '/SignOutAutomatically',
+    component: <SignOutAutomatically />
+  }
+]
 
 //contact routes
-const contactRoutes = [
-{
-    path: "/SignOutManually", 
-    component: <SignOutManually/>
-},
-{
-    path: "/SignOutAutomatically",
-    component: <SignOutAutomatically/>
-},
-{
-    path:"/SignBackIn",
-     component: <SignBackIn/>
+const unauthenticatedRoutes = [
+  {
+    path: '/SignBackIn',
+    component: <SignBackIn />
+  },
+  {
+    path: '/start',
+    component: <StartPage />
+  },
 
-},
+  { path: '/Start', component: <StartPage /> },
+  {
+    path: '/SignInPage',
+    component: <SignInPage />
+  },
+  {
+    path: '/CheckYourEmailPage',
+    component: <CheckYourEmailPage />
+  },
+  {
+    path: '/register',
+    component: <InitialEmailRegistrationPage />
+  },
+  {
+    path: '/ValidateEmailForRegistration',
+    component: <ValidateEmailForRegistration />
+  }
+]
 
-{   path: "/Start",
-    component: <StartPage /> 
-},
-{ 
-    path: "/SignInPage", 
-    component: <SignInPage /> 
-},
-{   
-    path: "/CheckYourEmailPage",
-     component: <CheckYourEmailPage /> 
-},{ path: "/register", component: <InitialEmailRegistrationPage /> },
-{ path: "/ValidateEmailForRegistration", component: <ValidateEmailForRegistration /> },
+export const unAuthRoutes = [...unauthenticatedRoutes]
+export const authRoutes = [...authenticatedRoutes]
+export const routes = [...authRoutes, ...unAuthRoutes]
+//const routes = [...unAuthRoutes, ...authRoutes]
 
-];
-
-const routes = [...startRoutes,...contactRoutes]
-
-
-
-    
-
-export default routes;
+//export default routes

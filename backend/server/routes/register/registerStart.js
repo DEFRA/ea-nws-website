0f48b773759f29b8edd8e2a6cadb9b033250c6be
camille.lecuyer <camille.lecuyer@leidos.com>
2024-05-17 09:26:54 +0000

    register start update

--- backend/server/routes/register/registerStart.js
+++ backend/server/routes/register/registerStart.js
@@ -1,50 +1,51 @@
-const apiCall = require("../../services/ApiService");
-const emailValidation = require("../../services/Validations/EmailValidation");
+const apiCall = require('../../services/ApiService')
+const emailValidation = require('../../services/Validations/EmailValidation')
 
 const apiRegisterCall = async (email) => {
-  let isValid = 400;
-  let desc;
-  console.log("reached api call");
-  let registerToken = "";
-  const raw = JSON.stringify({ email: email });
-  console.log("Received from front-end: ", raw);
+  let isValid = 400
+  let desc = ''
+  console.log('reached api call')
+  let registerToken = ''
+  const raw = JSON.stringify({ email: email })
+  console.log('Received from front-end: ', raw)
   if (!emailValidation(email)) {
-    console.log("returning code 101", email);
-    return { code: 101, desc: "Invalid email" };
+    console.log('returning code 101', email)
+    return { code: 101, desc: 'Invalid email' }
   }
 
-  const responseData = await apiCall(raw, "member/registerStart");
-  if (Object.prototype.hasOwnProperty.call(responseData, "desc")) {
-    isValid = responseData.code;
-    desc = responseData.desc;
+  const responseData = await apiCall(raw, 'member/registerStart')
+  console.log('Received from API: ', responseData)
+  if (Object.prototype.hasOwnProperty.call(responseData, 'desc')) {
+    isValid = responseData.code
+    desc = responseData.desc
+    return { code: isValid, desc: desc }
   } else {
-    console.log("responseData", responseData);
-    isValid = responseData.code;
-    registerToken = responseData.registerToken;
+    console.log('responseData', responseData)
+    isValid = responseData.code
+    registerToken = responseData.registerToken
+    return { code: isValid, registerToken: registerToken }
   }
-  console.log("Received from API: ", responseData);
-  return { code: isValid, registerToken: registerToken };
-};
+}
 
 module.exports = [
   {
-    method: ["POST", "PUT"],
-    path: "/registerStart",
+    method: ['POST', 'PUT'],
+    path: '/registerStart',
     handler: async (request, h) => {
       try {
-        const { email } = request.payload;
+        const { email } = request.payload
         // do some email validation
-        const apiResponse = await apiRegisterCall(email);
+        const apiResponse = await apiRegisterCall(email)
         const response = {
           code: apiResponse.code,
-          registerToken: apiResponse.registerToken,
-        };
+          registerToken: apiResponse.registerToken
+        }
 
-        return h.response(response);
+        return h.response(response)
       } catch (error) {
-        console.error("Error:", error);
-        return h.response({ message: "Internal server error" }).code(500);
+        console.error('Error:', error)
+        return h.response({ message: 'Internal server error' }).code(500)
       }
-    },
-  },
-];
+    }
+  }
+]

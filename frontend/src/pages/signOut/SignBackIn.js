947e42312722cf13b0711824e59a68eeb27c45bc
thomas barron <thomas.barron@uk.leidos.com>
2024-05-20 11:25:29 +0000

    refractoring

--- frontend/src/pages/signOut/SignBackIn.js
+++ frontend/src/pages/signOut/SignBackIn.js
@@ -1,14 +1,13 @@
-import Header from '../../gov-uk-components/Header'
-import Footer from '../../gov-uk-components/Footer'
-import Button from '../../gov-uk-components/Button'
-import routes from '../../routes/routes'
 import { useNavigate } from 'react-router-dom'
+import Button from '../../gov-uk-components/Button'
+import Footer from '../../gov-uk-components/Footer'
+import Header from '../../gov-uk-components/Header'
 
 export default function SignBackIn() {
   const navigate = useNavigate()
+
   function redirect() {
     navigate('/SignInPage')
-    //window.location.href = "/SignInPage";
   }
 
   return (
@@ -19,7 +18,7 @@ export default function SignBackIn() {
           You need to sign back in to view this page
         </h1>
         <Button
-          text={'sign in'}
+          text={'Sign in'}
           className={'govuk-button'}
           onClick={redirect}
         />

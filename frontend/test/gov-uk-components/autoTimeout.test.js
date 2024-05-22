c515c01a41ebd9d6b63d0f7192a2155ba9677391
thomas barron <thomas.barron@uk.leidos.com>
2024-05-22 15:21:02 +0000

    try setup of aut timeout test

---
+++ frontend/test/gov-uk-components/autoTimeout.test.js
@@ -0,0 +1,16 @@
+import '@testing-library/jest-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { setAuthToken } from '../../src/redux/userSlice'
+
+describe('auto timeout', () => {
+  const dispatch = useDispatch
+  test('user is logged in to service', () => {
+    const session = useSelector((state) => state.session)
+    dispatch(setAuthToken('123'))
+    expect(session.authToken).toHaveTextContent('123')
+  })
+
+  //   test('warning shown', () => {})
+
+  //   test('user logged out', () => {})
+})

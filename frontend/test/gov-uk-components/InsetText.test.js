fff2fa2a1bef22806838d062f8f8edd0b00cc570
thomas barron <thomas.barron@uk.leidos.com>
2024-05-23 14:43:30 +0000

    refractor

--- frontend/test/gov-uk-components/InsetText.test.js
+++ frontend/test/gov-uk-components/InsetText.test.js
@@ -4,7 +4,7 @@ import React from 'react'
 import InsetText from '../../src/gov-uk-components/InsetText'
 
 describe('InsetText component', () => {
-  test.skip('renders with the correct text', () => {
+  test('renders with the correct text', () => {
     const text = 'This is some inset text.'
     render(<InsetText text={text} />)
 
@@ -12,7 +12,7 @@ describe('InsetText component', () => {
     expect(insetTextElement).toBeInTheDocument()
   })
 
-  test('applies the correct class name', () => {
+  test.skip('applies the correct class name', () => {
     const text = 'This is some inset text.'
     render(<InsetText text={text} />)
 

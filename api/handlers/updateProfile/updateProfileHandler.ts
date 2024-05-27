cbcbd0853f28fe0b653885c2a75efee0b90c1490
camille.lecuyer <camille.lecuyer@leidos.com>
2024-05-27 13:10:40 +0000

    API corrected

--- api/handlers/updateProfile/updateProfileHandler.ts
+++ api/handlers/updateProfile/updateProfileHandler.ts
@@ -2,9 +2,9 @@ import Hapi from '@hapi/hapi'
 import type { Context } from 'openapi-backend'
 const responseCodes = require('../responseCodes')
 
-async function updateProfile(context: Context, req: Hapi.Request) {
+async function getUpdateProfile(context: Context, req: Hapi.Request) {
   console.log('payload', req.payload)
-  return { OK: 'OK' }
+  return { ...responseCodes.SUCCESS, Desc: 'It works' }
 }
 
-module.exports = { updateProfile }
+module.exports = { getUpdateProfile }

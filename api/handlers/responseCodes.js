a0ec81757df844d19d9bc48edf85b301ef577d35
camille.lecuyer <camille.lecuyer@leidos.com>
2024-05-28 16:17:46 +0000

    api response code update

--- api/handlers/responseCodes.js
+++ api/handlers/responseCodes.js
@@ -3,5 +3,5 @@ module.exports = {
     SUCCESS: { code: 200 },
     UNKNOWN_EMAIL: { code: 106, desc: 'unknown email' },
     INVALID_TOKEN: { code: 101, desc: 'InvalidToken' },
-    BAD_REQUEST: { code: 400, desc: 'BadRequest' }
+    INVALID_CODE: { code: 101, desc: 'Invalid Code' }
 };

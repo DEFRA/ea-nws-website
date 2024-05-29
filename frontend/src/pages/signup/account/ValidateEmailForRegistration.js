4ef34db222140bd54e98c0041cad8e6fa9aa935a
thomas barron <thomas.barron@uk.leidos.com>
2024-05-29 11:57:51 +0000

    fix build

--- frontend/src/pages/signup/account/ValidateEmailForRegistration.js
+++ frontend/src/pages/signup/account/ValidateEmailForRegistration.js
@@ -1,76 +1,87 @@
 import { useState } from 'react'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
+import Button from '../../../gov-uk-components/Button'
+import ErrorSummary from '../../../gov-uk-components/ErrorSummary'
 import Footer from '../../../gov-uk-components/Footer'
 import Header from '../../../gov-uk-components/Header'
-import Input from '../../../gov-uk-components/Input'
+import TextInput from '../../../gov-uk-components/Input'
 import InsetText from '../../../gov-uk-components/InsetText'
-const backendCall = require('../../../services/BackendService')
-const userEmail = window.sessionStorage.getItem('userEmail')
-const registerToken = window.sessionStorage.getItem('registerToken')
+import PhaseBanner from '../../../gov-uk-components/PhaseBanner'
+import { backendCall } from '../../../services/BackendService'
+import codeValidation from '../../../services/Validations/CodeValidation'
 
-const validateNumber = (input, digits) => {
-  const numberPattern = new RegExp(`^[0-9]{${digits}}$`)
-  return numberPattern.test(input)
-}
+export default function SignUpValidationPage() {
+  const location = useLocation()
 
-const ValidateEmailForRegistrationForm = (props) => {
-  const [errorMessage, setErrorMessage] = useState('')
+  const navigate = useNavigate()
+  const registerToken = location.state.registerToken
+
   const [code, setCode] = useState('')
+  const [error, setError] = useState('')
 
   const handleSubmit = async (event) => {
-    if (code === '') {
-      setErrorMessage('Enter code')
-      return
+    const validationError = codeValidation(code, 6)
+    setError(validationError)
+    if (validationError === '') {
+      const data = {
+        registerToken: registerToken,
+        code: code
+      }
+
+      const { errorMessage } = await backendCall(
+        data,
+        'signupValidate',
+        navigate
+      )
+
+      if (errorMessage !== null) {
+        setError(errorMessage.desc)
+      } else {
+        navigate('/signup/contactpreferences')
+      }
     }
-    if (!validateNumber(code, 6)) {
-      setErrorMessage('Code must be 6 numbers')
-      return
-    }
-    const apiReturn = await validateCode(code)
-    if (!apiReturn) {
-      setErrorMessage('Invalid code')
-      return
-    }
-    window.location.replace('Start')
   }
 
-  const validateCode = async (code) => {
-    const raw = JSON.stringify({ registerToken, code })
-
-    const responseData = await backendCall(raw, 'registerValidate')
-    if (responseData.hasOwnProperty('code')) {
-      return false
-    }
-
-    window.sessionStorage.setItem('authToken', responseData.authToken)
-    window.sessionStorage.setItem('profile', responseData.profile)
-    window.sessionStorage.setItem('registration', responseData.registration)
-    return true
-  }
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <Input name='Enter code' onChange={(val) => setCode(val)} />
-      {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
-      <button type='submit' className='govuk-button' data-module='govuk-button'>
-        Continue
-      </button>
-    </form>
-  )
-}
-
-export default function ValidateEmailForRegistration () {
   return (
     <>
       <Header />
-      <div className='govuk-width-container'>
-        <a href='register' className='govuk-back-link'>
+      <div className="govuk-width-container">
+        <PhaseBanner />
+        <Link to="/signup" className="govuk-back-link">
           Back
-        </a>
-        <h2 className='govuk-heading-l'>Check your email</h2>
-        <div className='govuk-body'>
-          We've sent a code to:
-          <InsetText text={userEmail} />
-          <ValidateEmailForRegistrationForm />
+        </Link>
+        {error && <ErrorSummary errorList={[error]} />}
+        <h2 className="govuk-heading-l">Check your email</h2>
+        <div className="govuk-body">
+          You need to confirm your email address.
+          <br />
+          <br />
+          We've sent an email with a code to:
+          <InsetText text={location.state.email} />
+          Enter the code within 4 hours or it will expire.
+          <br />
+          <br />
+          <TextInput
+            className="govuk-input govuk-input--width-10"
+            inputType="text"
+            value={code}
+            name="Enter code"
+            error={error}
+            onChange={(val) => setCode(val)}
+          />
+          <Button
+            className="govuk-button"
+            text="Confirm email address"
+            onClick={handleSubmit}
+          />
+          &nbsp; &nbsp;
+          <Link to="/signup" className="govuk-link">
+            Use a different email
+          </Link>
+          <br />
+          <Link to="/signup" className="govuk-link">
+            Get a new code
+          </Link>
         </div>
       </div>
 

070a3903b1971775b618772a4e6de80e02ff7adf
Paul.Chester <Paul.Chester@uk.leidos.com>
2024-05-29 14:27:17 +0000

    Configured log files EAN-950

--- backend/server/plugins/logging.js
+++ backend/server/plugins/logging.js
@@ -1,9 +1,36 @@
 const config = require('../config')
+const pino = require('pino')
+const fs = require('fs')
+
+const logdir = '/tmp/logs'
+
+if (!fs.existsSync(logdir)) {
+  fs.mkdirSync(logdir, { recursive: true })
+}
+
+const createSonicBoom = (dest) =>
+  pino.destination({ dest: dest, append: true, sync: true })
+
+const streams = [
+  { level: 'debug', stream: createSonicBoom(`${logdir}/debug.log`) },
+  { level: 'info', stream: createSonicBoom(`${logdir}/info.log`) },
+  { level: 'warn', stream: createSonicBoom(`${logdir}/warn.log`) },
+  { level: 'error', stream: createSonicBoom(`${logdir}/error.log`) },
+  { level: 'fatal', stream: createSonicBoom(`${logdir}/fatal.log`) }
+]
+
+const level = config.isDev ? 'debug' : 'info'
+
+const logger = pino(
+  { level: level },
+  pino.multistream(streams, { dedupe: true })
+)
 
 module.exports = {
   plugin: require('hapi-pino'),
   options: {
+    instance: logger,
     logPayload: true,
-    level: config.isDev ? 'debug' : 'warn'
+    level: level
   }
 }

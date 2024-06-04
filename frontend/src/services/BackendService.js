3cfaefaa0cc60a2589c0635557124102be6a2bbf
Paul.Chester <Paul.Chester@uk.leidos.com>
2024-06-04 15:11:42 +0000

    added nginx container for routing EAN-966

--- frontend/src/services/BackendService.js
+++ frontend/src/services/BackendService.js
@@ -2,7 +2,8 @@ import { handleResponse } from './HandleResponse'
 
 export const backendCall = async (data, path, navigate) => {
   let responseData
-  const url = 'http://localhost:5000/api/' + path
+  const domain = process.env.REACT_APP_DOMAIN || 'http://localhost:5000'
+  const url = domain + '/api/' + path
   try {
     const response = await fetch(url, {
       method: 'POST',

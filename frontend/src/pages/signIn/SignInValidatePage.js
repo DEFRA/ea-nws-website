4ae6fb37c027b6430f3a1323e4d522af2e8c5635
camille.lecuyer <camille.lecuyer@leidos.com>
2024-06-04 14:53:12 +0000

    Sign out button and signin validate navigates to home

--- frontend/src/pages/signIn/SignInValidatePage.js
+++ frontend/src/pages/signIn/SignInValidatePage.js
@@ -14,7 +14,7 @@ import {
 } from '../../redux/userSlice'
 import { backendCall } from '../../services/BackendService'
 import { authCodeValidation } from '../../services/validations/AuthCodeValidation'
-export default function SignInValidatePage () {
+export default function SignInValidatePage() {
   const location = useLocation()
   const [error, setError] = useState('')
   const dispatch = useDispatch()
@@ -38,7 +38,7 @@ export default function SignInValidatePage () {
         dispatch(setAuthToken(data.authToken))
         dispatch(setProfile(data.profile))
         dispatch(setRegistrations(data.registrations))
-        navigate('/')
+        navigate('/home')
       }
     }
   }
@@ -46,24 +46,24 @@ export default function SignInValidatePage () {
   return (
     <>
       <Header />
-      <div class='govuk-width-container'>
-        <Link to='/signin' className='govuk-back-link'>
+      <div class="govuk-width-container">
+        <Link to="/signin" className="govuk-back-link">
           Back
         </Link>
         <ErrorSummary errorList={error === '' ? [] : [error]} />
-        <h2 class='govuk-heading-l'>Check your email</h2>
-        <div class='govuk-body'>
+        <h2 class="govuk-heading-l">Check your email</h2>
+        <div class="govuk-body">
           We've sent a code to:
           <InsetText text={location.state.email} />
           <Input
-            name='Enter code'
-            inputType='text'
+            name="Enter code"
+            inputType="text"
             error={error}
             onChange={(val) => setCode(val)}
           />
           <Button
-            className='govuk-button'
-            text='Continue'
+            className="govuk-button"
+            text="Continue"
             onClick={handleSubmit}
           />
         </div>

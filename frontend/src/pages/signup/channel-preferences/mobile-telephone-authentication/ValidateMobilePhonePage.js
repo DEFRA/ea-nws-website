866ab1fe1d9132ea80acbd7355232a283e8daedc
thomas barron <thomas.barron@uk.leidos.com>
2024-06-07 09:08:41 +0000

    linting

--- frontend/src/pages/signup/channel-preferences/mobile-telephone-authentication/ValidateMobilePhonePage.js
+++ frontend/src/pages/signup/channel-preferences/mobile-telephone-authentication/ValidateMobilePhonePage.js
@@ -16,7 +16,7 @@ import {
 } from '../../../../services/ProfileServices'
 import { authCodeValidation } from '../../../../services/validations/AuthCodeValidation'
 
-export default function ValidateMobilePhone() {
+export default function ValidateMobilePhone () {
   const navigate = useNavigate()
   const dispatch = useDispatch()
   const location = useLocation()
@@ -84,55 +84,55 @@ export default function ValidateMobilePhone() {
   return (
     <>
       <Header />
-      <div className="govuk-width-container">
-        <div className="govuk-grid-row">
-          <div className="govuk-grid-column-two-thirds">
+      <div className='govuk-width-container'>
+        <div className='govuk-grid-row'>
+          <div className='govuk-grid-column-two-thirds'>
             <PhaseBanner />
-            <Link onClick={differentMobile} className="govuk-back-link">
+            <Link onClick={differentMobile} className='govuk-back-link'>
               Back
             </Link>
             {error && <ErrorSummary errorList={[error]} />}
-            <h1 className="govuk-heading-l govuk-!-margin-top-6">
+            <h1 className='govuk-heading-l govuk-!-margin-top-6'>
               Check your mobile phone
             </h1>
 
-            <div className="govuk-body">
+            <div className='govuk-body'>
               <p>We've sent a text with a code to:</p>
               <InsetText text={mobile} />
               <p>Use the code within 4 hours or it will expire</p>
               <Input
-                inputType="text"
+                inputType='text'
                 value={code}
-                name="Enter code"
+                name='Enter code'
                 onChange={(val) => setCode(val)}
                 error={error}
-                className="govuk-input govuk-input--width-10"
+                className='govuk-input govuk-input--width-10'
               />
               <Button
-                text="Continue"
-                className="govuk-button"
+                text='Continue'
+                className='govuk-button'
                 onClick={handleSubmit}
               />
               &nbsp; &nbsp;
               <Link
-                to="/signup/contactpreferences/mobile/skipconfirmation"
+                to='/signup/contactpreferences/mobile/skipconfirmation'
                 state={{
                   mobile
                 }}
-                className="govuk-link"
+                className='govuk-link'
               >
                 Skip and confirm later
               </Link>
               <br />
-              <Link onClick={getNewCode} className="govuk-link">
+              <Link onClick={getNewCode} className='govuk-link'>
                 Get a new code
               </Link>
               <br />
               <br />
-              <Link onClick={differentMobile} className="govuk-link">
+              <Link onClick={differentMobile} className='govuk-link'>
                 Enter a different mobile number
               </Link>
-              <div className="govuk-!-margin-bottom-9" />
+              <div className='govuk-!-margin-bottom-9' />
             </div>
           </div>
         </div>
--- frontend/src/pages/signup/user-agreement/DeclarationOfAgreementPage.js
+++ frontend/src/pages/signup/user-agreement/DeclarationOfAgreementPage.js
@@ -7,7 +7,7 @@ import Footer from '../../../gov-uk-components/Footer'
 import Header from '../../../gov-uk-components/Header'
 import PhaseBanner from '../../../gov-uk-components/PhaseBanner'
 
-export default function DeclarationOfAgreementPage() {
+export default function DeclarationOfAgreementPage () {
   const [isChecked, setIsChecked] = useState(false)
   const [error, setError] = useState('')
   const navigate = useNavigate()
@@ -16,30 +16,30 @@ export default function DeclarationOfAgreementPage() {
     if (isChecked === false) {
       setError('Tick to confirm you agree with the terms and conditions')
     } else {
-      //To do - when we have next page
+      // To do - when we have next page
       navigate('/')
     }
   }
   return (
     <>
       <Header />
-      <div className="govuk-width-container">
+      <div className='govuk-width-container'>
         <PhaseBanner />
-        <div className="govuk-body">
-          <Link to="/" className="govuk-back-link">
+        <div className='govuk-body'>
+          <Link to='/' className='govuk-back-link'>
             Back
           </Link>
           {error && <ErrorSummary errorList={[error]} />}
-          <h1 class="govuk-heading-l">Check the terms and conditions</h1>
-          <h3 class="govuk-heading-s">What we will do</h3>
+          <h1 class='govuk-heading-l'>Check the terms and conditions</h1>
+          <h3 class='govuk-heading-s'>What we will do</h3>
           <p>
             We make all reasonable efforts to send the flood warnings you have
             asked for, but we cannot guarantee they will be sent or arrive.
             Warnings may be sent at any time of day or night.
           </p>
 
-          <h3 class="govuk-heading-s">We do not:</h3>
-          <ul class="govuk-list govuk-list--bullet">
+          <h3 class='govuk-heading-s'>We do not:</h3>
+          <ul class='govuk-list govuk-list--bullet'>
             <li>accept responsibility if you fail to receive a warning</li>
             <li>
               guarantee that our online or phone services will be available
@@ -51,17 +51,17 @@ export default function DeclarationOfAgreementPage() {
             </li>
           </ul>
 
-          <h3 class="govuk-heading-s">You are responsible for:</h3>
-          <ul class="govuk-list govuk-list--bullet">
+          <h3 class='govuk-heading-s'>You are responsible for:</h3>
+          <ul class='govuk-list govuk-list--bullet'>
             <li>providing us with accurate contact details</li>
             <li>telling us about any changes to your contact information</li>
           </ul>
 
-          <h3 class="govuk-heading-s">
+          <h3 class='govuk-heading-s'>
             How will we use your personal information
           </h3>
           <p>We may use the personal information you provide to:</p>
-          <ul class="govuk-list govuk-list--bullet">
+          <ul class='govuk-list govuk-list--bullet'>
             <li>send you warnings you've asked for</li>
             <li>
               send you a small number of services or administrative messages
@@ -84,8 +84,8 @@ export default function DeclarationOfAgreementPage() {
           <p>
             The Environment Agency manages the flood warning systems. Our
             <a
-              href="https://www.fws.environment-agency.gov.uk/app/olr/privacy"
-              class="govuk-link"
+              href='https://www.fws.environment-agency.gov.uk/app/olr/privacy'
+              class='govuk-link'
             >
               {' '}
               privacy notice (open new window)
@@ -98,8 +98,8 @@ export default function DeclarationOfAgreementPage() {
             your personal information if you ask for a service in Wales. Read
             how Natural Resources Wales
             <a
-              href="https://www.gov.uk/government/organisations/environment-agency/about/personal-information-charter"
-              class="govuk-link"
+              href='https://www.gov.uk/government/organisations/environment-agency/about/personal-information-charter'
+              class='govuk-link'
             >
               {' '}
               Treat your personal information (opens new window)
@@ -113,17 +113,17 @@ export default function DeclarationOfAgreementPage() {
                 : 'govuk-form-group'
             }
           >
-            {error && <p className="govuk-error-message">{error}</p>}
+            {error && <p className='govuk-error-message'>{error}</p>}
             <Checkbox
               onChange={() => setIsChecked(!isChecked)}
               checked={isChecked}
-              label={'I agree to the terms and conditions'}
-              value={'T&C'}
+              label='I agree to the terms and conditions'
+              value='T&C'
             />
           </div>
           <Button
-            className={'govuk-button'}
-            text={'Continue'}
+            className='govuk-button'
+            text='Continue'
             onClick={handleSubmit}
           />
         </div>

a3eafa761148f6ee406f034e500b1699deaf0f3c
Paul.Chester <Paul.Chester@uk.leidos.com>
2024-06-10 09:59:30 +0000

    Updated new frontend code with api in path EAN-966

--- frontend/src/pages/signup/channel-preferences/mobile-telephone-authentication/AddMobilePhonePage.js
+++ frontend/src/pages/signup/channel-preferences/mobile-telephone-authentication/AddMobilePhonePage.js
@@ -13,7 +13,7 @@ import { addUnverifiedContact } from '../../../../services/ProfileServices'
 import { normalisePhoneNumber } from '../../../../services/formatters/NormalisePhoneNumber'
 import { phoneValidation } from '../../../../services/validations/PhoneValidation'
 
-export default function AddMobilePhonePage () {
+export default function AddMobilePhonePage() {
   const navigate = useNavigate()
   const dispatch = useDispatch()
   const [mobile, setMobile] = useState('')
@@ -27,7 +27,7 @@ export default function AddMobilePhonePage () {
       const data = { authToken: session.authToken, msisdn: mobile }
       const { errorMessage } = await backendCall(
         data,
-        'signup/contactpreferences/mobile/add',
+        'api/signup/contactpreferences/mobile/add',
         navigate
       )
       if (errorMessage !== null) {
@@ -50,32 +50,32 @@ export default function AddMobilePhonePage () {
   return (
     <>
       <Header />
-      <div className='govuk-width-container'>
-        <div className='govuk-grid-row'>
-          <div className='govuk-grid-column-two-thirds'>
+      <div className="govuk-width-container">
+        <div className="govuk-grid-row">
+          <div className="govuk-grid-column-two-thirds">
             <PhaseBanner />
-            <Link to='/signup/contactpreferences' className='govuk-back-link'>
+            <Link to="/signup/contactpreferences" className="govuk-back-link">
               Back
             </Link>
             {error && <ErrorSummary errorList={[error]} />}
-            <h1 className='govuk-heading-l govuk-!-margin-top-6'>
+            <h1 className="govuk-heading-l govuk-!-margin-top-6">
               Enter a mobile number to get flood messages by text
             </h1>
-            <p className='govuk-body'>
+            <p className="govuk-body">
               We recommend using a mobile number where we can reach you 24 hours
               a day
             </p>
             <Input
-              inputType='text'
+              inputType="text"
               value={mobile}
-              name='UK mobile telephone number'
+              name="UK mobile telephone number"
               onChange={(val) => setMobile(val)}
               error={error}
-              className='govuk-input govuk-input--width-20'
+              className="govuk-input govuk-input--width-20"
             />
             <Button
-              text='Continue'
-              className='govuk-button'
+              text="Continue"
+              className="govuk-button"
               onClick={handleSubmit}
             />
           </div>

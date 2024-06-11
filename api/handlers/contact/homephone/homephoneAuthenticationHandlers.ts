e1f732c37da484fe3353f5a9d5b7b29000d78828
camille.lecuyer <camille.lecuyer@leidos.com>
2024-06-11 09:57:31 +0000

    API Update - adding all verification paths

--- api/handlers/contact/homephone/homephoneAuthenticationHandlers.ts
+++ api/handlers/contact/homephone/homephoneAuthenticationHandlers.ts
@@ -26,10 +26,34 @@ async function getHomephoneValidate(
   const { msisdn } = req.payload as { msisdn: string }
   const { code } = req.payload as { code: string }
   console.log('Received LandlineValidate request: ', req.payload)
-
+  const profile = {
+    id: '1',
+    enabled: true,
+    firstName: 'John',
+    lastName: 'Smith',
+    emails: ['matthew.pepper@gmail.com', 'perry.pepper@gmail.com'],
+    mobilePhones: ['07343454590', '07889668367'],
+    homePhones: ['01475721535'],
+    language: 'EN',
+    additionals: [],
+    unverified: {
+      emails: [],
+      mobilePhones: [],
+      homePhones: ['01475721535']
+    },
+    pois: {
+      address: 'Exeter, United Kingdom',
+      coordinates: {
+        latitude: '50726037',
+        longitude: '-3527489'
+      }
+    }
+  }
   // 200 Success
-  if (authToken === 'MockAuthToken') {
-    return res.response(responseCodes.SUCCESS)
+  if (authToken === 'MockAuthToken' && code !== '999999') {
+    return {
+      profile: profile
+    }
   } else {
     return res.response(responseCodes.INVALID_CODE).code(500)
   }

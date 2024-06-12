eae4a28329ade09bbda02d9a98a765dd0f38477c
thomas barron <thomas.barron@uk.leidos.com>
2024-06-12 13:36:15 +0000

    basic styling WIP

--- frontend/src/custom-components/InactivityPopup.js
+++ frontend/src/custom-components/InactivityPopup.js
@@ -1,11 +1,24 @@
 import React from 'react'
-
-export default function InactivityPopup ({ onStayLoggedIn }) {
+import Button from '../gov-uk-components/Button'
+export default function InactivityPopup({ onStayLoggedIn }) {
   return (
-    <div className='inactivity-warning'>
-      <div className='inactivity-warning-content'>
-        <p>You have been inactive for a while. You will be signed out soon.</p>
-        <button onClick={onStayLoggedIn}>Stay Logged In</button>
+    <div className="govuk-grid">
+      <div className="inactivity-warning-content">
+        <h3 class="govuk-heading-s">Youre about to be signed out</h3>
+        <p class="govuk-body">
+          For your security we will sign you out in 2 minutes
+        </p>
+        <Button
+          className={'govuk-button'}
+          onClick={onStayLoggedIn}
+          text={'Stay signed in'}
+        />
+        <p class="govuk-body">
+          <a href="/signout" class="govuk-link">
+            Sign out
+          </a>
+          .
+        </p>
       </div>
     </div>
   )

948233578db7b215787da61e3825f2e7efb041f2
Paul.Chester <Paul.Chester@uk.leidos.com>
2024-06-13 14:59:38 +0000

    Addressed review comments EAN-805

--- backend/server/routes/signup/signup_feedback.js
+++ backend/server/routes/signup/signup_feedback.js
@@ -1,24 +1,20 @@
-
-  module.exports = [
-    {
-      method: ['POST'],
-      path: '/api/signup/feedback',
-      handler: async (request, h) => {
-        try {
-          if (request.payload === null) {
-            return h.response({ message: 'Bad request' }).code(400)
-          }
-          const {feedbackPreference, feedbackText} = request.payload
-  
-          request.log(['info'], 'logging feedback data here')
-          request.log(['info'], request.payload)
-          
-          return request.payload
-        } catch (error) {
-          console.error('Error:', error)
-          return h.response({ message: 'Internal server error' }).code(500)
+module.exports = [
+  {
+    method: ['POST'],
+    path: '/api/signup/feedback',
+    handler: async (request, h) => {
+      try {
+        if (request.payload === null) {
+          return h.response({ message: 'Bad request' }).code(400)
         }
-      } 
+
+        request.log(['info'], request.payload)
+
+        return h.response({ status: 200 }).code(200)
+      } catch (error) {
+        console.error('Error:', error)
+        return h.response({ message: 'Internal server error' }).code(500)
+      }
     }
-  ]
-  
+  }
+]
--- frontend/src/pages/signup/account/SignUpFeedbackPage.js
+++ frontend/src/pages/signup/account/SignUpFeedbackPage.js
@@ -1,61 +1,76 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import Button from '../../../gov-uk-components/Button'
+import CheckboxRadios from '../../../gov-uk-components/CheckBoxRadios'
 import ErrorSummary from '../../../gov-uk-components/ErrorSummary'
 import Footer from '../../../gov-uk-components/Footer'
 import Header from '../../../gov-uk-components/Header'
-import CheckboxRadios from '../../../gov-uk-components/CheckBoxRadios'
 import Input from '../../../gov-uk-components/Input'
-import TextArea from '../../../gov-uk-components/TextArea'
 import PhaseBanner from '../../../gov-uk-components/PhaseBanner'
+import TextArea from '../../../gov-uk-components/TextArea'
 import { backendCall } from '../../../services/BackendService'
 
-export default function FeedbackPage () {
+export default function FeedbackPage() {
   const navigate = useNavigate()
   const [feedbackPreference, setFeedbackPreference] = useState('')
   const [feedbackText, setFeedbackText] = useState('')
   const [optionalFeedbackText, setOptionalFeedbackText] = useState('')
   const [error, setError] = useState('')
   const [textError, setTextError] = useState('')
-  
+
   const feedbackOptions = [
-    {value: 'Very Satisfied', label: 'Very Satisfied'},
-    {value: 'Satisfied', label: 'Satisfied'},
-    {value: 'Neither satisfied or dissatisfied', label: 'Neither satisfied or dissatisfied'},
-    {value: 'Dissatisfied', label: 'Dissatisfied'},
-    {value: 'Very Dissatisfied', label: 'Very Dissatisfied'}
+    { value: 'Very Satisfied', label: 'Very Satisfied' },
+    { value: 'Satisfied', label: 'Satisfied' },
+    {
+      value: 'Neither satisfied or dissatisfied',
+      label: 'Neither satisfied or dissatisfied'
+    },
+    { value: 'Dissatisfied', label: 'Dissatisfied' },
+    { value: 'Very Dissatisfied', label: 'Very Dissatisfied' }
   ]
 
   const handleSubmit = async () => {
-    if (feedbackPreference.length === 0){
+    if (feedbackPreference.length === 0) {
       setError('Select an answer to tell us how you feel about this service')
-    }
-    else{
+    } else {
       setError(null)
       setFeedbackPreference(feedbackPreference)
     }
-    if (feedbackText === ''){
-      setTextError('Tell us anything you like or do not like about this service')
-    }else{
+    if (feedbackText === '') {
+      setTextError(
+        'Tell us anything you like or do not like about this service'
+      )
+    } else {
       setTextError(null)
       setFeedbackText(feedbackText)
     }
 
-    if(feedbackText !== '' && feedbackPreference.length !== 0){
-      const dataToRecord = {feedbackPreference, feedbackText, optionalFeedbackText}
-      await backendCall(dataToRecord, 'api/signup/feedback', navigate)
-
-      navigate('/signup')
+    if (feedbackText !== '' && feedbackPreference.length !== 0) {
+      const dataToRecord = {
+        feedbackPreference,
+        feedbackText,
+        optionalFeedbackText
+      }
+      const { data, errorMessage } = await backendCall(
+        dataToRecord,
+        'api/signup/feedback',
+        navigate
+      )
+      if (errorMessage !== null) {
+        setError(errorMessage.desc)
+      } else {
+        navigate('/signup')
+      }
     }
   }
 
   const setFeedback = (event) => {
     const { value } = event.target
-    setFeedbackPreference((prev) => {
-      if (prev.includes(value)) {
-        return prev.filter((preference) => preference !== value)
+    setFeedbackPreference((pref) => {
+      if (pref.includes(value)) {
+        return pref.filter((preference) => preference !== value)
       } else {
-        return [...prev, value]
+        return [...pref, value]
       }
     })
   }
@@ -63,96 +78,100 @@ export default function FeedbackPage () {
   return (
     <>
       <Header />
-      <div className='govuk-width-container'>
+      <div className="govuk-width-container">
         <PhaseBanner />
-        <Link to='/signup' className='govuk-back-link'>
+        <Link to="/signup" className="govuk-back-link">
           Back
         </Link>
-        { (error && textError) && <ErrorSummary errorList={[error, textError]}  />}
-        { ( textError && !error) && <ErrorSummary errorList={[textError]}  />}
-        { ( !textError && error) && <ErrorSummary errorList={[error]}  />}
-        <h1 className='govuk-heading-l'>Give feedback about signing up</h1>
-        <div className='govuk-body'>
+        {error && textError && <ErrorSummary errorList={[error, textError]} />}
+        {textError && !error && <ErrorSummary errorList={[textError]} />}
+        {!textError && error && <ErrorSummary errorList={[error]} />}
+        <h1 className="govuk-heading-l">Give feedback about signing up</h1>
+        <div className="govuk-body">
           This helps us to improve this service
           <br />
-          <p className='govuk-inset-text'>
-          Only tell us about feedback on this page. If you need to check you have <br/> signed up correctly or have a question about 
-          your flood risk, {' '}
-            <a
-              href='/'
-              className='govuk-link'
-            >
+          <p className="govuk-inset-text">
+            Only tell us about feedback on this page. If you need to check you
+            have <br /> signed up correctly or have a question about your flood
+            risk,{' '}
+            <a href="/" className="govuk-link">
               contact us.
             </a>
           </p>
           <br />
-          <div className={
-                error
-                  ? 'govuk-form-group govuk-form-group--error'
-                  : 'govuk-form-group'
-              }
-            >
-              <fieldset className='govuk-fieldset'>
-          <h2 className='govuk-heading-m'>
-              Overall, how do you feel about this service?
-          </h2>
-          {error && <p className='govuk-error-message'>{error}</p>}
-            <div className='govuk-radios' data-module='govuk-radios'>
-              {feedbackOptions.map((option) => (
-                    <CheckboxRadios
-                      key={option.value}
-                      label={option.label}
-                      value={option.value}
-                      checked={feedbackPreference.includes(option.value)}
-                      onChange={setFeedback}
-                    />
-                  ))}
-                </div>
-              </fieldset>
-            </div>
-          <br />
-          <div className={
-                textError
-                  ? 'govuk-form-group govuk-form-group--error'
-                  : 'govuk-form-group'
-              }
-            >
-              <fieldset className='govuk-fieldset'></fieldset>
-          <h3 className='govuk-label-wrapper'>
-          <label class="govuk-label govuk-label--m" for="more-detail">
-            Is there anything you like or do not like about this
-            <br />
-            service?
-          </label>
-          </h3>
-          <div id="more-detail-hint" class="govuk-hint">
-            Do not include your personal or financial details
+          <div
+            className={
+              error
+                ? 'govuk-form-group govuk-form-group--error'
+                : 'govuk-form-group'
+            }
+          >
+            <fieldset className="govuk-fieldset">
+              <h2 className="govuk-heading-m">
+                Overall, how do you feel about this service?
+              </h2>
+              {error && <p className="govuk-error-message">{error}</p>}
+              <div className="govuk-radios" data-module="govuk-radios">
+                {feedbackOptions.map((option) => (
+                  <CheckboxRadios
+                    key={option.value}
+                    label={option.label}
+                    value={option.value}
+                    checked={feedbackPreference.includes(option.value)}
+                    onChange={setFeedback}
+                  />
+                ))}
+              </div>
+            </fieldset>
           </div>
-          {textError && <p className='govuk-error-message'>{textError}</p>}
-          <TextArea className="govuk-textarea" 
-            id="more-detail" 
-            rows="5"
-            onChange={(val) => setFeedbackText(val)} ></TextArea>
+          <br />
+          <div
+            className={
+              textError
+                ? 'govuk-form-group govuk-form-group--error'
+                : 'govuk-form-group'
+            }
+          >
+            <fieldset className="govuk-fieldset"></fieldset>
+            <h3 className="govuk-label-wrapper">
+              <label class="govuk-label govuk-label--m" for="more-detail">
+                Is there anything you like or do not like about this
+                <br />
+                service?
+              </label>
+            </h3>
+            <div id="more-detail-hint" class="govuk-hint">
+              Do not include your personal or financial details
+            </div>
+            {textError && <p className="govuk-error-message">{textError}</p>}
+            <TextArea
+              className="govuk-textarea"
+              id="more-detail"
+              rows="5"
+              onChange={(val) => setFeedbackText(val)}
+            ></TextArea>
           </div>
         </div>
-        <h4 className='govuk-heading-m'>
-            Take part in research sessions (optional)
-          </h4>
-          <p className='govuk-hint'> Enter email address to take part. We will not use this for any
-          < br /> other reason and will 
-          delete it after 2 years.</p>
-          <Input
-            className='govuk-input govuk-input--width-30'
-            inputType='text'
-            onChange={(val) => setOptionalFeedbackText(val)}
-          />
-          <div>
+        <h4 className="govuk-heading-m">
+          Take part in research sessions (optional)
+        </h4>
+        <p className="govuk-hint">
+          {' '}
+          Enter email address to take part. We will not use this for any
+          <br /> other reason and will delete it after 2 years.
+        </p>
+        <Input
+          className="govuk-input govuk-input--width-30"
+          inputType="text"
+          onChange={(val) => setOptionalFeedbackText(val)}
+        />
+        <div>
           <Button
-              text='Continue'
-              className='govuk-button'
-              onClick={handleSubmit}
-            />
-          </div>
+            text="Continue"
+            className="govuk-button"
+            onClick={handleSubmit}
+          />
+        </div>
       </div>
       <Footer />
     </>

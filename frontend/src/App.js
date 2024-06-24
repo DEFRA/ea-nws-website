b12b994015eeaa471208b779d74b495b761e77e4
thomas barron <thomas.barron@uk.leidos.com>
2024-06-24 14:43:53 +0000

    undo change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,15 +12,7 @@ export default function App () {
   const redirectTimer = useRef(null)
   const [isPopUpOnScreen, setIsPopUpOnScreen] = useState(false)
 
-  const isLastRouteAuth = () => {
-    const prevUrl = document.referrer
-    for (let index = 0; index < authenticatedRoutes.length; index++) {
-        if(prevUrl === authenticatedRoutes[index].path){
-          return true
-        }
-    }
-    return false
-  }
+  
   useEffect(() => {
     if (isPopUpOnScreen === false) {
       const resetInactivityTimer = () => {
@@ -76,7 +68,7 @@ export default function App () {
           <Route
             key={index}
             path={route.path}
-            element={auth || (!isLastRouteAuth() && auth) ? route.component : <Navigate to='/sign-back-in' />}
+            element={auth ? route.component : <Navigate to='/sign-back-in' />}
           />
         ))}
         {routes.map((route, index) => (

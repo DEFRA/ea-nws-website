9ab12a90541256b6e887422c65f961117b4d020f
camille.lecuyer <camille.lecuyer@leidos.com>
2024-06-26 16:41:51 +0000

    showing all contacts

--- frontend/src/pages/signup/review/ContactReviewTable.js
+++ frontend/src/pages/signup/review/ContactReviewTable.js
@@ -4,237 +4,207 @@ import { Link, useNavigate } from 'react-router-dom'
 export default function ContactReviewTable ({ profile }) {
   const navigate = useNavigate()
   const ShowEmailAddress = () => {
-    if (profile.emails[0]) {
-      return (
-        <>
-          <tr className='govuk-table__row'>
-            <td className='govuk-table__header  govuk-!-width-one-half'>
-              By email
-            </td>
-            <td className='govuk-table__cell govuk-!-width-full'>
-              {profile.emails[0]}
-            </td>
-            <td className='govuk-table__cell' />
-            <td className='govuk-table__cell' />
-            <td className='govuk-table__cell'>
-              <Link
-                to='/managecontacts/confirm-delete'
-                state={{
-                  type: 'email',
-                  contact: profile.emails[0]
-                }}
-                className='govuk-link'
-              >
-                Remove
-              </Link>
-            </td>
-          </tr>
-        </>
-      )
-    } else if (profile.unverified.emails[0]) {
-      return (
-        <>
-          <tr className='govuk-table__row'>
-            <td className='govuk-table__header  govuk-!-width-one-half'>
-              By email
-            </td>
-            <td className='govuk-table__cell govuk-!-width-full'>
-              {profile.emails[0]}
-            </td>
-            <td className='govuk-table__cell'>
-              <strong className='govuk-tag govuk-tag--red'>Unconfirmed</strong>
-            </td>
-            <td className='govuk-table__cell'>
-              <Link to='/managecontacts/validate-email' className='govuk-link'>
-                Confirm
-              </Link>
-            </td>
-            <td className='govuk-table__cell'>
-              <Link
-                to='/managecontacts/confirm-delete'
-                state={{
-                  type: 'email',
-                  contact: profile.unverified.emails[0]
-                }}
-                className='govuk-link'
-              >
-                Remove
-              </Link>
-            </td>
-          </tr>
-        </>
-      )
-    }
+    console.log(profile)
     return (
-      <>
-        <tr className='govuk-table__row'>
-          <td className='govuk-table__header  govuk-!-width-one-half'>
-            By email
-          </td>
-          <td className='govuk-table__cell govuk-!-width-full' />
-          <td className='govuk-table__cell' />
-          <td className='govuk-table__cell' />
-          <td className='govuk-table__cell' />
-        </tr>
-      </>
-    )
-  }
+    <>
+    {profile.emails.length > 0 || profile.unverified.emails.length > 0 ? (
+        <tbody className='govuk-table__body'>
+          {profile.emails.map((email, index) => (
+            <tr key={index} className='govuk-table__row'>
+            <td className='govuk-table__header govuk-!-width-one-half'>
+              By Email
+            </td>
+              <td className='govuk-table__cell govuk-!-width-full'>
+                {email}
+              </td>
+              <td className='govuk-table__cell' />
+              <td className='govuk-table__cell' />
+              {email !== profile.emails[0] ? (
+                <td className='govuk-table__cell'>
+                  <Link
+                    to='/managecontacts/confirm-delete'
+                    state={{
+                      type: 'email',
+                      email
+                    }}
+                    className='govuk-link'
+                  >
+                    Remove
+                  </Link>
+                </td>
+              ) : (
+                // empty space in table without this
+                <td className='govuk-table__cell' />
+              )}
+            </tr>
+          ))}
+          {profile.unverified.emails.map((unregisteredEmail, index) => (
+            <tr key={index} className='govuk-table__row'>
+              <td className='govuk-table__header govuk-!-width-one-half'>
+              By Email
+            </td>
+              <td className='govuk-table__cell govuk-!-width-full'>
+                {unregisteredEmail}
+              </td>
+              <td className='govuk-table__cell'>
+                <strong className='govuk-tag govuk-tag--red'>
+                  Unconfirmed
+                </strong>
+              </td>
+              <td className='govuk-table__cell'>
+                
+              </td>
+              <td className='govuk-table__cell'>
+                <Link
+                  to='/managecontacts/confirm-delete'
+                  state={{
+                    type: 'email',
+                    unregisteredEmail
+                  }}
+                  className='govuk-link'
+                >
+                  Remove
+                </Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+
+    ) : null} 
+    </>)
+    }
+    
   const ShowMobileNumber = () => {
-    if (profile.mobilePhones[0]) {
-      return (
-        <>
-          <tr className='govuk-table__row'>
-            <td className='govuk-table__header  govuk-!-width-one-half'>
-              By text
-            </td>
-            <td className='govuk-table__cell govuk-!-width-full'>
-              {profile.mobilePhones[0]}
-            </td>
-            <td className='govuk-table__cell' />
-            <td className='govuk-table__cell' />
-            <td className='govuk-table__cell'>
-              <Link
-                to='/managecontacts/confirm-delete'
-                state={{
-                  type: 'mobile',
-                  contact: profile.mobilePhones[0]
-                }}
-                className='govuk-link'
-              >
-                Remove
-              </Link>
-            </td>
-          </tr>
-        </>
-      )
-    } else if (profile.unverified.mobilePhones[0]) {
-      return (
-        <>
-          <tr className='govuk-table__row'>
-            <td className='govuk-table__header  govuk-!-width-one-half'>
-              By text
-            </td>
-            <td className='govuk-table__cell govuk-!-width-full'>
-              {profile.emails[0]}
-            </td>
-            <td className='govuk-table__cell'>
-              <strong className='govuk-tag govuk-tag--red'>Unconfirmed</strong>
-            </td>
-            <td className='govuk-table__cell'>
-              <Link to='/managecontacts/validate-mobile' className='govuk-link'>
-                Confirm
-              </Link>
-            </td>
-            <td className='govuk-table__cell'>
-              <Link
-                to='/managecontacts/confirm-delete'
-                state={{
-                  type: 'mobile',
-                  contact: profile.unverified.mobilePhones[0]
-                }}
-                className='govuk-link'
-              >
-                Remove
-              </Link>
-            </td>
-          </tr>
-        </>
-      )
-    }
     return (
       <>
-        <tr className='govuk-table__row'>
-          <td className='govuk-table__header  govuk-!-width-one-half'>
-            By text
-          </td>
-          <td className='govuk-table__cell govuk-!-width-full' />
-          <td className='govuk-table__cell' />
-          <td className='govuk-table__cell' />
-          <td className='govuk-table__cell' />
-        </tr>
-      </>
-    )
-  }
+      {profile.mobilePhones.length > 0 || profile.unverified.mobilePhones.length > 0 ? (
+          <tbody className='govuk-table__body'>
+            {profile.mobilePhones.map((mobilePhone, index) => (
+              <tr key={index} className='govuk-table__row'>
+              <td className='govuk-table__header govuk-!-width-one-half'>
+                By text
+              </td>
+                <td className='govuk-table__cell govuk-!-width-full'>
+                  {mobilePhone}
+                </td>
+                <td className='govuk-table__cell' />
+                <td className='govuk-table__cell' />
+                <td className='govuk-table__cell'>
+                  <Link
+                    to='/managecontacts/confirm-delete'
+                    state={{
+                      type: 'mobilePhone',
+                      mobilePhone
+                    }}
+                    className='govuk-link'
+                  >
+                    Remove
+                  </Link>
+                </td>                
+              </tr>
+            ))}
+            {profile.unverified.mobilePhones.map((unregisteredMobilePhone, index) => (
+              <tr key={index} className='govuk-table__row'>
+              <td className='govuk-table__header govuk-!-width-one-half'>
+                By text
+              </td>
+                <td className='govuk-table__cell govuk-!-width-full'>
+                  {unregisteredMobilePhone}
+                </td>
+                <td className='govuk-table__cell'>
+                  <strong className='govuk-tag govuk-tag--red'>
+                    Unconfirmed
+                  </strong>
+                </td>
+                <td className='govuk-table__cell'>
+                  
+                </td>
+                <td className='govuk-table__cell'>
+                  <Link
+                    to='/managecontacts/confirm-delete'
+                    state={{
+                      type: 'mobilePhone',
+                      unregisteredMobilePhone
+                    }}
+                    className='govuk-link'
+                  >
+                    Remove
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+  
+      ) : null} 
+      </>)
+    }
+
+
 
   const ShowHomePhone = () => {
-    if (profile.homePhones[0]) {
-      return (
-        <>
-          <tr className='govuk-table__row'>
-            <td className='govuk-table__header  govuk-!-width-one-half'>
-              By phone call
-            </td>
-            <td className='govuk-table__cell govuk-!-width-full'>
-              {profile.homePhones[0]}
-            </td>
-            <td className='govuk-table__cell' />
-            <td className='govuk-table__cell' />
-            <td className='govuk-table__cell'>
-              <Link
-                to='/managecontacts/confirm-delete'
-                state={{
-                  type: 'homephone',
-                  contact: profile.homePhones[0]
-                }}
-                className='govuk-link'
-              >
-                Remove
-              </Link>
-            </td>
-          </tr>
-        </>
-      )
-    } else if (profile.unverified.homePhones[0]) {
-      return (
-        <>
-          <tr className='govuk-table__row'>
-            <td className='govuk-table__header  govuk-!-width-one-half'>
-              By phone call
-            </td>
-            <td className='govuk-table__cell govuk-!-width-full'>
-              {profile.homePhones[0]}
-            </td>
-            <td className='govuk-table__cell'>
-              <strong className='govuk-tag govuk-tag--red'>Unconfirmed</strong>
-            </td>
-            <td className='govuk-table__cell'>
-              <Link
-                to='/managecontacts/validate-landline'
-                className='govuk-link'
-              >
-                Confirm
-              </Link>
-            </td>
-            <td className='govuk-table__cell'>
-              <Link
-                to='/managecontacts/confirm-delete'
-                state={{
-                  type: 'homephone',
-                  contact: profile.unverified.homePhones[0]
-                }}
-                className='govuk-link'
-              >
-                Remove
-              </Link>
-            </td>
-          </tr>
-        </>
-      )
-    }
     return (
       <>
-        <tr className='govuk-table__row'>
-          <td className='govuk-table__header  govuk-!-width-one-half'>
-            By phone call
-          </td>
-          <td className='govuk-table__cell govuk-!-width-full' />
-          <td className='govuk-table__cell' />
-          <td className='govuk-table__cell' />
-          <td className='govuk-table__cell' />
-        </tr>
-      </>
-    )
-  }
+      {profile.homePhones.length > 0 || profile.unverified.homePhones.length > 0 ? (
+          <tbody className='govuk-table__body'>
+            {profile.homePhones.map((homePhone, index) => (
+              <tr key={index} className='govuk-table__row'>
+              <td className='govuk-table__header govuk-!-width-one-half'>
+                By phone call
+              </td>
+                <td className='govuk-table__cell govuk-!-width-full'>
+                  {homePhone}
+                </td>
+                <td className='govuk-table__cell' />
+                <td className='govuk-table__cell' />
+                <td className='govuk-table__cell'>
+                  <Link
+                    to='/managecontacts/confirm-delete'
+                    state={{
+                      type: 'homePhone',
+                      homePhone
+                    }}
+                    className='govuk-link'
+                  >
+                    Remove
+                  </Link>
+                </td>                
+              </tr>
+            ))}
+            {profile.unverified.homePhones.map((unregisteredHomePhones, index) => (
+              <tr key={index} className='govuk-table__row'>
+              <td className='govuk-table__header govuk-!-width-one-half'>
+                By phone call
+              </td>
+                <td className='govuk-table__cell govuk-!-width-full'>
+                  {unregisteredHomePhones}
+                </td>
+                <td className='govuk-table__cell'>
+                  <strong className='govuk-tag govuk-tag--red'>
+                    Unconfirmed
+                  </strong>
+                </td>
+                <td className='govuk-table__cell'>
+                  
+                </td>
+                <td className='govuk-table__cell'>
+                  <Link
+                    to='/managecontacts/confirm-delete'
+                    state={{
+                      type: 'homePhones',
+                      unregisteredHomePhones
+                    }}
+                    className='govuk-link'
+                  >
+                    Remove
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+  
+      ) : null} 
+      </>)
+    }
 
   return (
     <>

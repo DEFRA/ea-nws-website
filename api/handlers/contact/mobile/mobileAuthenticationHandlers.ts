f9d550526316fffe46908f56bcb37654a1ba8904
camille.lecuyer <camille.lecuyer@leidos.com>
2024-06-27 10:16:40 +0000

    updating api check

--- api/handlers/contact/mobile/mobileAuthenticationHandlers.ts
+++ api/handlers/contact/mobile/mobileAuthenticationHandlers.ts
@@ -11,7 +11,7 @@ async function getMobileStart(
   const { msisdn } = req.payload as { msisdn: string }
   // 200 Success
   if (authToken === 'MockAuthToken') {
-    if (msisdn === '07000000000') {
+    if (msisdn === '+447000000000') {
       console.log('duplicate mobile, responding 500')
       return res.response(responseCodes.DUPLICATE_MOBILE).code(500)
     }

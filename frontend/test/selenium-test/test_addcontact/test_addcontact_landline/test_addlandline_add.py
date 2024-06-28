97eef894572af04b0a6aad887d5ad1e8d98c6aef
camille.lecuyer <camille.lecuyer@leidos.com>
2024-06-28 13:31:24 +0000

    updating ui tests

--- frontend/test/selenium-test/test_addcontact/test_addcontact_landline/test_addlandline_add.py
+++ frontend/test/selenium-test/test_addcontact/test_addcontact_landline/test_addlandline_add.py
@@ -46,7 +46,7 @@ def test_addlandlinestart_incorrectnumber(get_browser):
 
 def test_addlandlinestart_duplicateNumber(get_browser):
     browser = setup_addlandline_test(get_browser)   
-    browser.find_element(By.NAME, "UK landline or mobile telephone number").send_keys("00000000000")
+    browser.find_element(By.NAME, "UK landline or mobile telephone number").send_keys("01000000000")
     browser.find_element(By.CLASS_NAME, "govuk-button").click()
     assert "You have already registered this number on your account - you cannot enter it again" in browser.page_source
     assert browser.current_url == current_page

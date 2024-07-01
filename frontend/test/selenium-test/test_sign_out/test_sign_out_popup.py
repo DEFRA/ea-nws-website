51cbde1de9fa26d8ed6fa27ccfe735f6fdec4be1
thomas barron <thomas.barron@uk.leidos.com>
2024-07-01 13:39:45 +0000

    fix test

--- frontend/test/selenium-test/test_sign_out/test_sign_out_popup.py
+++ frontend/test/selenium-test/test_sign_out/test_sign_out_popup.py
@@ -11,7 +11,6 @@ def test_popup_stay_signin_button(get_browser):
     browser = get_browser
     browser.get(index_url)
     browser.find_element(By.CLASS_NAME,"govuk-button").click()
-    # change this value when production / pushed dev
     time.sleep(12)
     browser.find_element(By.CLASS_NAME,"dialog").click()
     try:
@@ -25,7 +24,6 @@ def test_popup_logout_button(get_browser):
     browser = get_browser
     browser.get(index_url)
     browser.find_element(By.CLASS_NAME,"govuk-button").click()
-    # change this value when production / pushed dev
     time.sleep(12)
     browser.find_element(By.LINK_TEXT,"Sign out").click()
     assert browser.current_url == url_signout
@@ -35,15 +33,15 @@ def test_stay_active(get_browser):
     browser = get_browser
     browser.get(index_url)
     browser.find_element(By.CLASS_NAME,"govuk-button").click()
-    # change this value when production / pushed dev
-    time.sleep(8)
-    action = ActionChains(browser)
-    action.click()
+    time.sleep(7)
+    browser.find_element(By.LINK_TEXT,"Home page").click()
     time.sleep(5)
     try:
-         assert browser.find_element(By.CLASS_NAME,"govuk-heading-s").is_displayed()
+         browser.find_element(By.CLASS_NAME,"govuk-heading-s").is_displayed()
+         assert False
     except NoSuchElementException:
         assert True
+
 
 
 def test_auto_logout(get_browser):
@@ -51,7 +49,6 @@ def test_auto_logout(get_browser):
     browser = get_browser
     browser.get(index_url)
     browser.find_element(By.CLASS_NAME,"govuk-button").click()
-    # change this value when production / pushed dev
     time.sleep(16)
     assert browser.current_url == auto_signout_url
 

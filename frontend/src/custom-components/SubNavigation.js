f469726a74bcbccdc2829bc0c1b2f0c93a4fb371
Paul.Chester <Paul.Chester@uk.leidos.com>
2024-07-01 15:05:37 +0000

    Added custom navigation for home page EAN-1018

---
+++ frontend/src/custom-components/SubNavigation.js
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom'
+import '../custom.css'
+
+export default function SubNavigation ({ pages, currentPage }) {
+  return (
+    <nav className="sub-navigation" aria-label="Sub navigation">
+      <ul className="sub-navigation__list">
+      {pages.map((page, index) => (
+        <li key={index} className="sub-navigation__item">
+          <Link 
+            to={page.link}
+            className="sub-navigation__link" 
+            aria-current={currentPage === page.link ? 'page' : 'no'}
+          >
+            {page.title}
+          </Link>
+        </li>
+      ))}
+      </ul>
+    </nav>
+  )
+}
--- frontend/src/pages/home/HomePage.js
+++ frontend/src/pages/home/HomePage.js
@@ -1,8 +1,10 @@
 import * as React from 'react'
-import { Link } from 'react-router-dom'
+import { useLocation } from 'react-router'
+import AccountNavigation from '../../custom-components/AccountNavigation'
 import Footer from '../../gov-uk-components/Footer'
 import Header from '../../gov-uk-components/Header'
 import PhaseBanner from '../../gov-uk-components/PhaseBanner'
+
 
 export default function HomePage () {
   return (
@@ -10,29 +12,8 @@ export default function HomePage () {
       <Header />
       <div className='govuk-width-container'>
         <PhaseBanner />
+        <AccountNavigation currentPage={useLocation().pathname} />
         <h1 className='govuk-heading-l'>Home</h1>
-        <Link
-          // this needs updated when we have the page
-          to='/home'
-          className='govuk-button govuk-button--secondary'
-        >
-          Add new location
-        </Link>
-        &nbsp; &nbsp;
-        <Link
-          // this needs updated when we have the page
-          to='/home'
-          className='govuk-button govuk-button--secondary'
-        >
-          Unsubscribe from flood alerts
-        </Link>
-        &nbsp; &nbsp;
-        <Link
-          to='/managecontacts'
-          className='govuk-button govuk-button--secondary'
-        >
-          Your email addresses and numbers
-        </Link>
       </div>
       <Footer />
     </>

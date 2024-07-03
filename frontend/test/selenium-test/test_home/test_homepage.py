a69608a0a46785c5b3a4e79d2602e27e4685c722
Paul.Chester <Paul.Chester@uk.leidos.com>
2024-07-03 13:02:44 +0000

    Fixed tests EAN-1018

--- frontend/test/selenium-test/test_home/test_homepage.py
+++ frontend/test/selenium-test/test_home/test_homepage.py
@@ -9,7 +9,8 @@ def check_exists_by_xpath(browser, xpath):
     try:
         browser.find_element(By.XPATH, xpath)
     except NoSuchElementException:
-        assert False
+        return False
+    return True
 
 def setup_homepage_test(get_browser, session):
     browser = get_browser
@@ -31,17 +32,17 @@ def test_homepage_render(get_browser):
 def test_homepage_render_table(get_browser):
     browser = setup_homepage_test(get_browser, 1) 
     table_xpath = f"//table[@class='govuk-table']"
-    check_exists_by_xpath(browser, table_xpath)
+    assert check_exists_by_xpath(browser, table_xpath)
 
 def test_homepage_render_button(get_browser):
     browser = setup_homepage_test(get_browser, 1) 
     button_xpath = f"//button[@class='govuk-button govuk-button--secondary']"
-    check_exists_by_xpath(browser, button_xpath)
+    assert check_exists_by_xpath(browser, button_xpath)
 
 def test_homepage_render_details(get_browser):
     browser = setup_homepage_test(get_browser, 1) 
     details_xpath = f"//summary[@class='govuk-details__summary']"
-    check_exists_by_xpath(browser, details_xpath)
+    assert check_exists_by_xpath(browser, details_xpath)
 
 def test_homepage_expand_details(get_browser):
     browser = setup_homepage_test(get_browser, 1) 
@@ -49,27 +50,27 @@ def test_homepage_expand_details(get_browser):
     details_link = browser.find_element(By.XPATH, details_xpath)
     browser.execute_script("arguments[0].click();", details_link)
     time.sleep(1)
-    assert browser.find_element(By.XPATH,f"//summary[@class='govuk-details__text']").isDisplayed()
+    assert "You must keep at least one location on your account" in browser.page_source
 
 def test_homepage_no_remove(get_browser):
     browser = setup_homepage_test(get_browser, 1) 
     remove_xpath = f"//a[text()='Remove']"
-    not check_exists_by_xpath(browser, remove_xpath)
+    assert not check_exists_by_xpath(browser, remove_xpath)
 
 def test_homepage_no_details(get_browser):
     browser = setup_homepage_test(get_browser, 2) 
     details_xpath = f"//summary[@class='govuk-details__summary']"
-    check_exists_by_xpath(browser, details_xpath)
+    assert not check_exists_by_xpath(browser, details_xpath)
 
 def test_homepage_render_remove(get_browser):
     browser = setup_homepage_test(get_browser, 2) 
     remove_xpath = f"//a[text()='Remove']"
-    check_exists_by_xpath(browser, remove_xpath)
+    assert check_exists_by_xpath(browser, remove_xpath)
 
 def test_homepage_render_pagination(get_browser):
     browser = setup_homepage_test(get_browser, 3) 
     pagination_xpath = f"//a[@class='govuk-link govuk-pagination__link']"
-    check_exists_by_xpath(browser, pagination_xpath)
+    assert check_exists_by_xpath(browser, pagination_xpath)
 
 def test_homepage_click_pagination(get_browser):
     browser = setup_homepage_test(get_browser, 3) 
@@ -78,5 +79,5 @@ def test_homepage_click_pagination(get_browser):
     browser.execute_script("arguments[0].click();", pagination_link)
     time.sleep(1)
     pagination_xpath = f"//a[@rel='prev']"
-    check_exists_by_xpath(browser, pagination_xpath)
+    assert check_exists_by_xpath(browser, pagination_xpath)
 

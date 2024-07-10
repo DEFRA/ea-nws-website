deb441d769f20a0dc89e6937e0b365f9791b345e
megan.gallagher <megan.gallagher@ukcldos.com>
2024-07-10 09:54:46 +0000

    updated spacing

--- frontend/src/pages/signup/success/SignUpSuccessPage.js
+++ frontend/src/pages/signup/success/SignUpSuccessPage.js
@@ -13,37 +13,36 @@ export default function SuccessPage () {
         <PhaseBanner />
           <div className="govuk-grid-row">
             <div className="govuk-grid-column-two-thirds">
-              <Link onClick={() => navigate(-1)} className='govuk-back-link'>Back</Link>
               <ConfirmationPanel
                 title='Your flood messages are set up'
                 body='you have also created your account.'
               />
-            <div className='govuk-body'>
+            <div className='govuk-body govuk-!-margin-top-6'>
             <p>
               We have sent you an email confirmation. If you have not received this within 2 hours, check your spam.
             </p>
-            <h1 class='govuk-heading-m'>Next Steps</h1>
-            <p>
+            <h1 class='govuk-heading-m govuk-!-margin-top-6'>Next Steps</h1>
+            <p className='govuk-!-margin-top-6'>
               You'll now receive flood messages for your location. If any are issued.
             </p>
-            <p>
+            <p className='govuk-!-margin-top-6'>
               These will be sent from Floodline at the Environment Agency.
             </p>
-            <h2 class='govuk-heading-m'>If you want to add more locations or contacts</h2>
-            <p className='govuk-body'>
+            <h2 class='govuk-heading-m govuk-!-margin-top-6'>If you want to add more locations or contacts</h2>
+            <p className='govuk-body govuk-!-margin-top-6'>
               You can now use your account to add more {' '}
               <Link to='/home' className='govuk-link'>
                 locations.
               </Link>
             </p>
-            <p>
+            <p className='govuk-!-margin-top-6'>
               You can also add more {' '}
               <Link to='/managecontacts' className='govuk-link'>
                 email addresses or phone numbers 
               </Link>
               to receive flood messages.
             </p>
-            <p>
+            <p className='govuk-!-margin-top-6'>
               {' '}
               <Link to='/signup/feedback' className='govuk-link'>
                 What do you think of the service?

68abe2e5545825d834e9ac26f3aa89b59a3a58fd
Paul.Chester <Paul.Chester@uk.leidos.com>
2024-07-18 09:40:15 +0000

    Refactored updateProfile to take session.profile EAN-1026

--- frontend/src/pages/account/ChangeBusinessDetailsPage.js
+++ frontend/src/pages/account/ChangeBusinessDetailsPage.js
@@ -15,7 +15,7 @@ import { businessDetailsValidation } from '../../services/validations/BusinessDe
 export default function ChangeBusinessDetailsPage () {
   const navigate = useNavigate()
   const session = useSelector((state) => state.session)
-  const profile = JSON.parse(JSON.stringify(session.profile))
+  const profile = session.profile
   const [businessNameError, setBusinessNameError] = useState('')
   const [jobTitleError, setJobTitleError] = useState('')
   const [error, setError] = useState('')
@@ -24,14 +24,16 @@ export default function ChangeBusinessDetailsPage () {
   const [businessName, setBusinessName] = useState(getAdditionals(profile, 'businessName'))
   const [jobTitle, setJobTitle] = useState(getAdditionals(profile, 'jobTitle'))
 
-  const handleSubmit = async (event) => {
+  const handleSubmit = async () => {
     const { validationErrorBusiness, validationErrorJob } = businessDetailsValidation(businessName, jobTitle)
     setBusinessNameError(validationErrorBusiness)
     setJobTitleError(validationErrorJob)
-    updateAdditionals(profile, 'businessName', businessName)
-    updateAdditionals(profile, 'jobTitle', jobTitle)
+    const updatedProfile = updateAdditionals(
+      profile,
+      [{id: 'businessName', value: businessName}, {id: 'jobTitle', value: jobTitle}]
+    )
 
-    const dataToSend = { profile, authToken }
+    const dataToSend = { profile: updatedProfile, authToken }
     if (!validationErrorBusiness && !validationErrorJob) {
       const { errorMessage, data } = await backendCall(
         dataToSend,
@@ -47,8 +49,8 @@ export default function ChangeBusinessDetailsPage () {
         navigate('/account', {
           state: {
             changeBusinessDetails: true,
-            businessName: getAdditionals(profile, 'businessName'),
-            jobTitle: getAdditionals(profile, 'jobTitle')
+            businessName: getAdditionals(data.profile, 'businessName'),
+            jobTitle: getAdditionals(data.profile, 'jobTitle')
           }
         })
       }

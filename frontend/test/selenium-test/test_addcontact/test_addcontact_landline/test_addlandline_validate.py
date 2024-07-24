d4cfa673bc5ff5170c6e813c7f3a4aa9f549ce63
thomas barron <thomas.barron@uk.leidos.com>
2024-07-24 11:45:20 +0000

    restore to how it was originally on develop

--- frontend/test/selenium-test/test_addcontact/test_addcontact_landline/test_addlandline_validate.py
+++ frontend/test/selenium-test/test_addcontact/test_addcontact_landline/test_addlandline_validate.py
@@ -8,7 +8,6 @@ index = "http://localhost:3000/index"
 current_url = "http://localhost:3000/managecontacts/validate-landline" 
 previous_page = "http://localhost:3000/managecontacts/add-landline"
 next_page = "http://localhost:3000/managecontacts"
-different_landline_url = "http://localhost:3000/signup/contactpreferences/landline/choosenumber"
 
 def setup_validatelandline_test(get_browser):
     browser = get_browser
@@ -26,25 +25,6 @@ def setup_validatelandline_test(get_browser):
     time.sleep(1)
     browser.find_element(By.NAME, "UK landline or mobile telephone number").send_keys("01632960001")
     browser.find_element(By.CLASS_NAME, "govuk-button").click()
-    time.sleep(1)
-    return browser
-
-def setup_validate_only_landline(get_browser):
-    browser = get_browser
-    browser.get(index)
-    button_xpath = f"//button[text()='Activate/Deactivate Mock Session 1']"
-    mock_session_link = browser.find_element(By.XPATH, button_xpath)
-    browser.execute_script("arguments[0].click();", mock_session_link)
-    link_xpath = f"//a[text()='Manage Contacts page']"
-    link_link = browser.find_element(By.XPATH, link_xpath)
-    browser.execute_script("arguments[0].click();", link_link)
-    time.sleep(1)
-    button_xpath = f"//button[text()='Add a telephone number']"
-    add_mobile_link = browser.find_element(By.XPATH, button_xpath)
-    browser.execute_script("arguments[0].click();", add_mobile_link)
-    time.sleep(1)
-    browser.find_element(By.NAME, "UK landline or mobile telephone number").send_keys("01632960001")
-    browser.find_element(By.ID, "idPhone call").click()
     time.sleep(1)
     return browser
 
@@ -94,26 +74,23 @@ def test_addlandlinevalidate_resendCode(get_browser):
     time.sleep(1)
     assert browser.current_url == current_url
 
-def test_addlandlinevalidate_enteradifferentnumber_when_number_added(get_browser):
+##this one fail####
+def test_addlandlinevalidate_enteradifferentnumber(get_browser):
     browser = setup_validatelandline_test(get_browser)  
     browser.find_element(By.LINK_TEXT, "Enter a different telephone number").click()
     time.sleep(1)
-    assert browser.current_url == different_landline_url
-
-def test_addlandlinevalidate_enteradifferentnumber(get_browser):
-    browser = setup_validate_only_landline(get_browser)  
-    browser.find_element(By.LINK_TEXT, "Enter a different telephone number").click()
-    time.sleep(1)
     assert browser.current_url == previous_page
 
+
+### this one also fail####
 def test_addlandlinevalidate_enteradifferentLandline_correctLandline(get_browser):
-    browser = setup_validate_only_landline(get_browser)  
+    browser = setup_validatelandline_test(get_browser)  
     assert "+441632960001" in browser.page_source
     browser.find_element(By.LINK_TEXT, "Enter a different telephone number").click()
-    time.sleep(1)
+    time.sleep(3)
     assert browser.current_url == previous_page
     browser.find_element(By.NAME, "UK landline or mobile telephone number").send_keys("01410000000")
     browser.find_element(By.CLASS_NAME, "govuk-button").click()
-    time.sleep(1)
+    time.sleep(3)
     assert browser.current_url == current_url
     assert "+441410000000" in browser.page_source

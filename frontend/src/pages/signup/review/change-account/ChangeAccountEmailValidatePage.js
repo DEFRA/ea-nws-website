e0cba5e654824439649c11f318f94489a0ba158f
camille.lecuyer <camille.lecuyer@leidos.com>
2024-07-25 10:24:39 +0000

    Adding new pages

---
+++ frontend/src/pages/signup/review/change-account/ChangeAccountEmailValidatePage.js
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import ValidateEmailLayout from '../../../../common-layouts/email/ValidateEmailLayout'
+import { setProfile } from '../../../../redux/userSlice'
+import { backendCall } from '../../../../services/BackendService'
+
+export default function ChangeAccountEmailValidationPage () {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const [error, setError] = useState('')
+
+  const DifferentEmail = () => {
+    navigate('/signup/review/change-account-email')
+  }
+
+  const updateProfile = async (profile, authToken) => {
+    const profileEmails = profile.emails
+    const emailsLength = profileEmails.length
+    profileEmails[0] = profileEmails[emailsLength - 1]
+    profileEmails.pop()
+    profile.emails = profileEmails
+
+    const dataToSend = { profile, authToken }
+    const { errorMessage } = await backendCall(
+      dataToSend,
+      'api/profile/update',
+      navigate
+    )
+
+    if (errorMessage !== null) {
+      setError(errorMessage)
+    } else {
+      dispatch(setProfile(profile))
+      navigate('/signup/review', {
+        state: {
+          changeEmail: true,
+          email: profile.emails[0]
+        }
+      })
+    }
+  }
+
+  return (
+    <ValidateEmailLayout
+      DifferentEmail={DifferentEmail}
+      NavigateToPreviousPage={DifferentEmail}
+      buttonText='Confirm email address'
+      changeSignIn
+      profileError={error}
+      updateProfile={updateProfile}
+    />
+  )
+}

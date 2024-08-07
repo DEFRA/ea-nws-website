75002c2f4620ac19a937b23a054e0e292409f80b
jibran.haider <muhammadjibran.haider@leidos.com>
2024-08-07 16:44:46 +0000

    Remove return statement. EAN-636

--- api/handlers/account/deleteAccountHandler.ts
+++ api/handlers/account/deleteAccountHandler.ts
@@ -3,17 +3,16 @@ import Hapi from '@hapi/hapi'
 import type { Context } from 'openapi-backend'
 
 async function getDeleteAccount(
-    context: Context,
-    req: Hapi.Request,
-    res: Hapi.ResponseToolkit
+  context: Context,
+  req: Hapi.Request,
+  res: Hapi.ResponseToolkit
 ) {
-    const { authToken } = req.payload as { authToken: string }
+  const { authToken } = req.payload as { authToken: string }
 
-    if (authToken !== 'MockAuthToken') {
-        console.log("invalid credentials, responding 101")
-        return res.response(responseCodes.UNAUTHORIZED).code(500)
-    }
-    return { ...responseCodes.SUCCESS, Desc: 'Account deleted' }
+  if (authToken !== 'MockAuthToken') {
+    console.log('invalid credentials, responding 101')
+    return res.response(responseCodes.UNAUTHORIZED).code(500)
+  }
 }
 
 module.exports = { getDeleteAccount }

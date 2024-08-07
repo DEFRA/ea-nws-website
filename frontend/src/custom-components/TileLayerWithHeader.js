0ceffcde960d1d5ad5b814cda4d3bf81ed4224b3
Paul.Chester <Paul.Chester@uk.leidos.com>
2024-08-07 12:35:12 +0000

    Updated to avoid network errors and constant re-renders EAN-1068

--- frontend/src/custom-components/TileLayerWithHeader.js
+++ frontend/src/custom-components/TileLayerWithHeader.js
@@ -1,13 +1,15 @@
-import { createElementObject, createTileLayerComponent, updateGridLayer, withPane } from '@react-leaflet/core'
-import L from 'leaflet'
+import { createElementObject, createTileLayerComponent, updateGridLayer, withPane } from '@react-leaflet/core';
+import L from 'leaflet';
 
-export default function TileLayerWithHeader ({ url, token }) {
+export default function TileLayerWithHeader({url, token, bounds}) {
+
   function CreateTileLayerWithHeader ({ url, ...options }, context) {
     L.TileLayer.WithHeader = L.TileLayer.extend({
       createTile (coords, done) {
         const url = this.getTileUrl(coords)
         const img = document.createElement('img')
-        fetch(url, { headers: { Authorization: `Bearer ${options.token}` }, mode: 'cors' })
+        const token = this.options.token
+        fetch(url, { headers: { Authorization: `Bearer ${token}` }, mode: 'cors' })
           .then((val) => val.blob())
           .then((blob) => {
             img.src = URL.createObjectURL(blob)
@@ -19,18 +21,17 @@ export default function TileLayerWithHeader ({ url, token }) {
     const layer = new L.TileLayer.WithHeader(url, withPane(options, context))
     return createElementObject(layer, context)
   }
-
+  
   function updateTileLayerWithHeader (layer, props, prevProps) {
     updateGridLayer(layer, props, prevProps)
-    const { url } = props
+    const { url, token } = props
     if (url != null && url !== prevProps.url) {
       layer.setUrl(url)
     }
   }
 
-  const TileLayerWithHeader = createTileLayerComponent(CreateTileLayerWithHeader, updateTileLayerWithHeader)
-
+  const TileLayerWithHeader = createTileLayerComponent(CreateTileLayerWithHeader,updateTileLayerWithHeader)
   return (
-    <TileLayerWithHeader url={url} token={token} />
+    <TileLayerWithHeader url={url} token={token} bounds={bounds} />
   )
 }

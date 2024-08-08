787212cf0973c68d1cfa0672b8ff2b5fcb238a81
camille.lecuyer <camille.lecuyer@leidos.com>
2024-08-08 10:44:21 +0000

    removing console.log

--- frontend/src/common-layouts/landline/ValidateLandlineLayout.js
+++ frontend/src/common-layouts/landline/ValidateLandlineLayout.js
@@ -107,14 +107,12 @@ export default function ValidateLandlineLayout({
     if (signUpComplete) {
       DifferentHomePhone(homePhone)
     } else {
-      console.log('HERE')
       const unverifiedMobile =
         profile.unverified.mobilePhones[
           profile.unverified.mobilePhones.length - 1
         ]
       const verifiedMobile =
         profile.mobilePhones[profile.mobilePhones.length - 1]
-      console.log('broke')
       if (unverifiedMobile === undefined && verifiedMobile === undefined) {
         DifferentHomePhone(homePhone)
       } else {

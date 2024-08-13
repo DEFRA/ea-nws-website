10e533ff0eb942d75122edbeb9c99cae70282497
camille.lecuyer <camille.lecuyer@leidos.com>
2024-08-13 15:34:34 +0000

    more location signup tests

---
+++ frontend/test/selenium-test/test_signup/test_location/test_signup_location_severe_warning.py
@@ -0,0 +1,77 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+
+home_page = "http://localhost:3000/"
+index = "http://localhost:3000/index"
+previous_page = "http://localhost:3000/signup/register-location/search-results"
+severe_warning_area_page = "http://localhost:3000/signup/register-location/location-in-severe-warning-area"
+next_page = "http://localhost:3000/signup/register-location/location-in-alert-area"
+search_page = "http://localhost:3000/signup/register-location/search"
+english_postcode_severe_warning = "SW1A 0AA"
+
+def setup_location_severewarning_test(get_browser):
+    browser = get_browser
+    browser.get(index)
+    button_xpath = f"//button[text()='Activate/Deactivate Empty profile - Used for sign up tests']"
+    mock_empty_profile_link = browser.find_element(By.XPATH, button_xpath)
+    browser.execute_script("arguments[0].click();", mock_empty_profile_link)
+    time.sleep(1)
+    browser.get(home_page)
+    signup_button_xpath = f"//button[text()='Sign up for the first time']"
+    signup_link = browser.find_element(By.XPATH, signup_button_xpath)
+    browser.execute_script("arguments[0].click();", signup_link)
+    time.sleep(1)
+    address_postcode_radio_xpath = "//label[text()='Address with postcode']/preceding-sibling::input[@type='radio']"
+    address_postcode_radio = browser.find_element(By.XPATH, address_postcode_radio_xpath)
+    browser.execute_script("arguments[0].click();", address_postcode_radio)
+    time.sleep(1)
+    address_postcode_input_xpath = "//label[text()='Address with postcode']/ancestor::div/following-sibling::div//input[@type='text']"
+    address_postcode_input = browser.find_element(By.XPATH, address_postcode_input_xpath)
+    address_postcode_input.send_keys(english_postcode_severe_warning)
+    continue_button_xpath = f"//button[text()='Continue']"
+    continue_button = browser.find_element(By.XPATH, continue_button_xpath)
+    browser.execute_script("arguments[0].click();", continue_button)
+    time.sleep(3)
+    address_warning_link_xpath = f"//a[contains(text(), 'House Of Commons')]"
+    address_warning_link = browser.find_element(By.XPATH, address_warning_link_xpath)
+    browser.execute_script("arguments[0].click();", address_warning_link)
+    time.sleep(3)
+    return browser
+
+def test_location_severe_warning_render(get_browser):
+    browser = setup_location_severewarning_test(get_browser) 
+    assert browser.current_url == severe_warning_area_page
+    assert "You can get severe flood warnings and flood warnings for this location" in browser.page_source
+    assert "House Of Commons" in browser.page_source
+    assert "Severe flood warnings and flood warnings area" in browser.page_source
+    assert "could be a danger to life or property" in browser.page_source
+    assert "Flood warnings are usually sent 30 minutes to 2 hours before flooding" in browser.page_source
+    assert "Confirm you want this location" in browser.page_source
+    assert "Choose different location" in browser.page_source
+
+def test_location_severe_warning_back(get_browser):
+    browser = setup_location_severewarning_test(get_browser) 
+    browser.find_element(By.CLASS_NAME, "govuk-back-link").click()
+    time.sleep(3)
+    assert browser.current_url == previous_page
+
+def test_location_severe_warning_continue(get_browser):
+    browser = setup_location_severewarning_test(get_browser) 
+    continue_button_xpath = f"//button[text()='Confirm you want this location']"
+    continue_button = browser.find_element(By.XPATH, continue_button_xpath)
+    browser.execute_script("arguments[0].click();", continue_button)
+    time.sleep(3)
+    assert browser.current_url == next_page
+
+def test_location_severe_warning_differentlocation(get_browser):
+    browser = setup_location_severewarning_test(get_browser) 
+    time.sleep(3)
+    differentlocation_button_xpath = f"//a[text()='Choose different location']"
+    differentlocation_button = browser.find_element(By.XPATH, differentlocation_button_xpath)
+    browser.execute_script("arguments[0].click();", differentlocation_button)
+    time.sleep(3)
+    assert browser.current_url == previous_page
--- frontend/test/selenium-test/test_signup/test_location/test_signup_search_location_result.py
+++ frontend/test/selenium-test/test_signup/test_location/test_signup_search_location_result.py
@@ -1,22 +1,31 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 import time
 
+home_page = "http://localhost:3000/"
 index = "http://localhost:3000/index"
-previousPage = "http://localhost:3000/signup/register-location/search"
-nextPage = "http://localhost:3000/"
+previous_page = "http://localhost:3000/signup/register-location/search"
+next_page = "http://localhost:3000/"
 url = "http://localhost:3000/signup/register-location/search-results"
-english_postcode = "SW1A 0AA"
+english_postcode_severe_warning = "SW1A 0AA"
+english_postcode_alert = "NW10 8TZ"
+english_postcode_nearby_warning = "WD25 7LR"
+severe_warning_area_page = "http://localhost:3000/signup/register-location/location-in-severe-warning-area"
+alert_page = "http://localhost:3000/signup/register-location/location-in-alert-area"
+proximity_severe_page = "http://localhost:3000/signup/register-location/location-in-proximity-area/severe"
+#english_postcode_noflood = "E1W 1YN"
 
-def setup_search_location_result_test(get_browser):
+def setup_search_location_result_test(get_browser, postcode):
     browser = get_browser
     browser.get(index)
     button_xpath = f"//button[text()='Activate/Deactivate Empty profile - Used for sign up tests']"
     mock_empty_profile_link = browser.find_element(By.XPATH, button_xpath)
     browser.execute_script("arguments[0].click();", mock_empty_profile_link)
     time.sleep(1)
-    browser.get(previousPage)
+    browser.get(home_page)
     signup_button_xpath = f"//button[text()='Sign up for the first time']"
     signup_link = browser.find_element(By.XPATH, signup_button_xpath)
     browser.execute_script("arguments[0].click();", signup_link)
@@ -27,20 +36,69 @@ def setup_search_location_result_test(get_browser):
     time.sleep(1)
     address_postcode_input_xpath = "//label[text()='Address with postcode']/ancestor::div/following-sibling::div//input[@type='text']"
     address_postcode_input = browser.find_element(By.XPATH, address_postcode_input_xpath)
-    address_postcode_input.send_keys(english_postcode)
+    address_postcode_input.send_keys(postcode)
     continue_button_xpath = f"//button[text()='Continue']"
     continue_button = browser.find_element(By.XPATH, continue_button_xpath)
     browser.execute_script("arguments[0].click();", continue_button)
-    time.sleep(1)
+    time.sleep(3)
     return browser
 
 def test_search_location_result_render(get_browser):
-    browser = setup_search_location_result_test(get_browser) 
+    browser = setup_search_location_result_test(get_browser, english_postcode_severe_warning) 
     assert browser.current_url == url
+    assert "Select an address" in browser.page_source
+    assert "Postcode: SW1A 0AA" in browser.page_source
+
+def test_search_location_change_postcode(get_browser):
+    browser = setup_search_location_result_test(get_browser, english_postcode_severe_warning) 
+    change_postcode_link_xpath = f"//a[text()='Change postcode']"
+    change_postcode_link = browser.find_element(By.XPATH, change_postcode_link_xpath)
+    browser.execute_script("arguments[0].click();", change_postcode_link)
+    assert browser.current_url == previous_page
     assert "Check if you can get flood messages for your location" in browser.page_source
     assert "Select how you want to search" in browser.page_source
 
+def test_search_location_cannot_find_address(get_browser):
+    browser = setup_search_location_result_test(get_browser, english_postcode_severe_warning) 
+    cannot_find_address_link_xpath = "//span[contains(text(), 'I cannot find my address here')]"
+    cannot_find_address_link = browser.find_element(By.XPATH, cannot_find_address_link_xpath)
+    browser.execute_script("arguments[0].click();", cannot_find_address_link)
+    assert browser.current_url == url
+    assert "You can view flood message areas" in browser.page_source
+
 def test_search_location_back_button(get_browser):
-    browser = setup_search_location_result_test(get_browser) 
+    browser = setup_search_location_result_test(get_browser, english_postcode_severe_warning) 
     browser.find_element(By.CLASS_NAME, "govuk-back-link").click()
-    assert browser.current_url == previousPage
+    time.sleep(1)
+    assert browser.current_url == previous_page
+
+def test_search_location_severe_warning(get_browser):
+    browser = setup_search_location_result_test(get_browser, english_postcode_severe_warning) 
+    address_warning_link_xpath = f"//a[contains(text(), 'House Of Commons')]"
+    address_warning_link = browser.find_element(By.XPATH, address_warning_link_xpath)
+    browser.execute_script("arguments[0].click();", address_warning_link)
+    time.sleep(3)
+    assert browser.current_url == severe_warning_area_page
+    assert "You can get severe flood warnings and flood warnings for this location" in browser.page_source
+
+def test_search_location_alert(get_browser):
+    browser = setup_search_location_result_test(get_browser, english_postcode_alert) 
+    address_alert_link_xpath = "//a[contains(text(), '48, Kingfisher Way')]"
+    address_alert_link = browser.find_element(By.XPATH, address_alert_link_xpath)
+    browser.execute_script("arguments[0].click();", address_alert_link)
+    time.sleep(3)
+    assert browser.current_url == alert_page
+    assert "You can get flood alerts for this location" in browser.page_source
+
+def test_search_location_nearby_warning_render(get_browser):
+    browser = setup_search_location_result_test(get_browser, english_postcode_nearby_warning) 
+    address_nearby_link_xpath = f"//a[contains(text(),'Warner Bros Studio Tour')]"
+    address_nearby_link = browser.find_element(By.XPATH, address_nearby_link_xpath)
+    browser.execute_script("arguments[0].click();", address_nearby_link)
+    time.sleep(3)
+    assert browser.current_url == proximity_severe_page
+    assert "You can get flood messages near this location" in browser.page_source
+    assert "1. River Gade at Kings Langley and Croxley" in browser.page_source
+    assert "2. River Colne at Watford" in browser.page_source
+    assert "Select a nearby area" in browser.page_source 
+    assert "Skip to other areas nearby" in browser.page_source 

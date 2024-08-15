d76f1e69b7860b927a0eb5c04e8e9d608abed548
ronan.peacock <ronan.peacock@leidos.com>
2024-08-15 12:45:50 +0000

    Mobile key formatting

--- frontend/src/custom-components/FloodWarningKey.js
+++ frontend/src/custom-components/FloodWarningKey.js
@@ -1,24 +1,43 @@
 import { faCircle } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+import { isMobile } from 'react-device-detect'
 
 export default function FloodWarningKey({ type }) {
+  // Larger circle for mobile key, default otherwise
+  const iconSize = isMobile ? 'xl' : 'lg'
+
   return (
     <>
-      <div className='flood-warning-key govuk-!-margin-top-6'>
-        <b>Key</b>{' '}
+      <div
+        className='flood-warning-key govuk-!-margin-top-6'
+        style={
+          isMobile
+            ? {
+                display: 'flex',
+                alignItems: 'center'
+              }
+            : {}
+        }
+      >
+        {/* Space needed between "Key" and icon on mobile */}
+        <b
+          style={type !== 'both' && isMobile ? { marginRight: '0.75rem' } : {}}
+        >
+          Key
+        </b>
         {type === 'both' && (
           <>
             <FontAwesomeIcon
               icon={faCircle}
-              className='govuk-!-padding-left-1 govuk-!-padding-right-1'
-              style={{ color: '#f70202' }}
+              className='govuk-!-padding-left-1 govuk-!-padding-right-1 warning-key-color'
+              size={iconSize}
             />{' '}
             Severe flood warnings and flood warnings area
             <FontAwesomeIcon
               icon={faCircle}
-              className='govuk-!-padding-left-7 govuk-!-padding-right-1'
-              style={{ color: '#ffa200' }}
+              className='govuk-!-padding-left-7 govuk-!-padding-right-1 alert-key-color'
+              size={iconSize}
             />{' '}
             Flood alert area
           </>
@@ -27,8 +46,8 @@ export default function FloodWarningKey({ type }) {
           <>
             <FontAwesomeIcon
               icon={faCircle}
-              className='govuk-!-padding-left-1 govuk-!-padding-right-1'
-              style={{ color: '#f70202' }}
+              className='govuk-!-padding-left-1 govuk-!-padding-right-1 warning-key-color'
+              size={iconSize}
             />{' '}
             Severe flood warnings and flood warnings area
           </>
@@ -37,8 +56,8 @@ export default function FloodWarningKey({ type }) {
           <>
             <FontAwesomeIcon
               icon={faCircle}
-              className='govuk-!-padding-left-1 govuk-!-padding-right-1'
-              style={{ color: '#ffa200' }}
+              className='govuk-!-padding-left-1 govuk-!-padding-right-1 alert-key-color'
+              size={iconSize}
             />{' '}
             Flood alert area
           </>

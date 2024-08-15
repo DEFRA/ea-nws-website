0235b15aeb34dd4ce3316615e8aff7654a854b70
cameron.purves <cameron.purves@leidos.com>
2024-08-15 09:44:03 +0000

    reverted flood warning key changes

--- frontend/src/custom-components/FloodWarningKey.js
+++ frontend/src/custom-components/FloodWarningKey.js
@@ -2,46 +2,48 @@ import { faCircle } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
-const FloodWarningKey = ({ type, mobileView }) => {
-  const renderKeyItem = (iconClass, text) => (
-    <div className='key-items'>
-      <b className='key-title'>Key</b>
-      <FontAwesomeIcon
-        icon={faCircle}
-        size={mobileView ? '2xl' : 'xl'}
-        className={`icon ${iconClass}`}
-      />
-      <span>{text}</span>
-    </div>
-  )
-
+export default function FloodWarningKey({ type }) {
   return (
-    <div className='flood-warning-key'>
-      {type === 'both' && (
-        <div className='key-items'>
-          <b className='key-title'>Key</b>
-          <FontAwesomeIcon
-            icon={faCircle}
-            size={mobileView ? '2xl' : 'xl'}
-            className='icon severe'
-          />
-          <span>Severe flood warnings and flood warnings area</span>
-          <FontAwesomeIcon
-            icon={faCircle}
-            size={mobileView ? '2xl' : 'xl'}
-            className='icon alert'
-          />
-          <span>Flood alert area</span>
-        </div>
-      )}
-      {type === 'severe' &&
-        renderKeyItem(
-          'severe',
-          'Severe flood warnings and flood warnings area'
+    <>
+      <div className='flood-warning-key govuk-!-margin-top-6'>
+        <b>Key</b>{' '}
+        {type === 'both' && (
+          <>
+            <FontAwesomeIcon
+              icon={faCircle}
+              className='govuk-!-padding-left-1 govuk-!-padding-right-1'
+              style={{ color: '#f70202' }}
+            />{' '}
+            Severe flood warnings and flood warnings area
+            <FontAwesomeIcon
+              icon={faCircle}
+              className='govuk-!-padding-left-7 govuk-!-padding-right-1'
+              style={{ color: '#ffa200' }}
+            />{' '}
+            Flood alert area
+          </>
         )}
-      {type === 'alert' && renderKeyItem('alert', 'Flood alert area')}
-    </div>
+        {type === 'severe' && (
+          <>
+            <FontAwesomeIcon
+              icon={faCircle}
+              className='govuk-!-padding-left-1 govuk-!-padding-right-1'
+              style={{ color: '#f70202' }}
+            />{' '}
+            Severe flood warnings and flood warnings area
+          </>
+        )}
+        {type === 'alert' && (
+          <>
+            <FontAwesomeIcon
+              icon={faCircle}
+              className='govuk-!-padding-left-1 govuk-!-padding-right-1'
+              style={{ color: '#ffa200' }}
+            />{' '}
+            Flood alert area
+          </>
+        )}
+      </div>
+    </>
   )
 }
-
-export default FloodWarningKey

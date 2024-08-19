0c3ee4135918ed8dd6715a2cdbed49ba9a892b2b
jibran.haider <muhammadjibran.haider@leidos.com>
2024-08-19 15:55:08 +0000

    Fix tests manual signout tests for organisation. EAN-550

--- frontend/test/selenium-test/test_sign_out/test_sign_out_manually.py
+++ frontend/test/selenium-test/test_sign_out/test_sign_out_manually.py
@@ -42,22 +42,21 @@ def test_cit_feedback_link(get_browser):
     time.sleep(1)
     assert browser.current_url == url_feedback
 
-# TODO: Need to fix theses 2 tests below, failing due to header
 # Test organisation signout button
-# def test_org_signout_button(get_browser):
-#     browser = get_browser
-#     browser.get(url_index)
-#     click_button(browser, 'Activate/Deactivate Mock Session 1')
-#     click_link(browser, 'Organisation home page')
-#     click_link(browser, 'Sign Out')
-#     assert browser.current_url == url_org_signout
+def test_org_signout_button(get_browser):
+    browser = get_browser
+    browser.get(url_index)
+    click_button(browser, 'Activate/Deactivate Mock Session 1')
+    click_link(browser, 'Organisation home page')
+    click_link(browser, 'Sign Out')
+    assert browser.current_url == url_org_signout
 
-# # Test organisation signin button on signout page
-# def test_org_signin_button(get_browser):
-#     browser = get_browser
-#     browser.get(url_index)
-#     click_button(browser, 'Activate/Deactivate Mock Session 1')
-#     click_link(browser, 'Organisation home page')
-#     click_link(browser, 'Sign Out')
-#     click_button(browser, 'Sign in')
-#     assert browser.current_url == url_org_signin
+# Test organisation signin button on signout page
+def test_org_signin_button(get_browser):
+    browser = get_browser
+    browser.get(url_index)
+    click_button(browser, 'Activate/Deactivate Mock Session 1')
+    click_link(browser, 'Organisation home page')
+    click_link(browser, 'Sign Out')
+    click_button(browser, 'Sign in')
+    assert browser.current_url == url_org_signin

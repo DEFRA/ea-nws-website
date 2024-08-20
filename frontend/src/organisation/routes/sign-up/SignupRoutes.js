95fd53903789b7eab25387b93ff926cc65c4ae27
megan.gallagher <megan.gallagher@ukcldos.com>
2024-08-20 08:15:55 +0000

    updated path

--- frontend/src/organisation/routes/sign-up/SignupRoutes.js
+++ frontend/src/organisation/routes/sign-up/SignupRoutes.js
@@ -1,9 +1,9 @@
 import CheckYourAnswersPage from '../../pages/sign-up/review/CheckYourAnswersPage'
-import SubmissionSuccessPage from '../../pages/sign-up/submission/SubmissionSuccessPage'
+import SignUpOrganisationSuccessPage from '../../pages/sign-up/success/SignUpOrganisationSuccessPage'
 
 const organisationSignupRoutes = [
   { path: '/organisation/signup/review', component: <CheckYourAnswersPage /> },
-  { path: '/organisation/signup/success', component: <SubmissionSuccessPage /> }
+  { path: '/organisation/signup/success', component: <SignUpOrganisationSuccessPage /> }
 ]
 
 export default organisationSignupRoutes

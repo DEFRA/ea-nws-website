0555999f9f30a4eb967727099f06dbe2a8bf29d4
ronan.peacock <ronan.peacock@leidos.com>
2024-08-22 15:52:47 +0000

    Log clean up function

--- backend/server/services/LPM-S3-Transfer.js
+++ backend/server/services/LPM-S3-Transfer.js
@@ -8,14 +8,28 @@ const client = new S3Client()
 // Returns whether the log data is older than the 2 day threshold
 const isLogOld = (logFilePath) => {
   try {
-    const logEntries = JSON.parse(
-      fs.readFileSync(logFilePath, { encoding: 'utf-8' }).split('\n')
+    const lastLogEntry = JSON.parse(
+      fs.readFileSync(logFilePath, 'utf-8').trim().split('\n').pop()
     )
-    const lastLogEntry = JSON.parse(logEntries[logEntries.length - 1])
     return lastLogEntry.time < Date.now() - 2 * 24 * 60 * 60 * 1000
   } catch (err) {
     return false
   }
+}
+
+const logCleanUp = async (directory, excludedFiles) => {
+  const savedLogs = fs
+    .readdirSync(directory)
+    .filter((file) => file.endsWith('.log') && !excludedFiles.includes(file))
+
+  console.log(savedLogs)
+  savedLogs.forEach((log) => {
+    const logFilePath = path.join(directory, log)
+    console.log(`Processing - ${logFilePath}`)
+    if (isLogOld(logFilePath)) {
+      fs.unlinkSync(logFilePath)
+    }
+  })
 }
 
 const uploadToBucket = async (filePath, bucketName) => {
@@ -40,23 +54,21 @@ const uploadToBucket = async (filePath, bucketName) => {
 const processLogs = async (directory, bucketName) => {
   const logFiles = ['error.log', 'fatal.log', 'info.log', 'warn.log']
 
+  await logCleanUp(directory, logFiles)
+
   for (const logFile of logFiles) {
     const logFilePath = path.join(directory, logFile)
 
-    if (fs.existsSync(logFilePath) && fs.statSync(logFilePath).size != 0) {
-      if (isLogOld(logFilePath)) {
-        fs.unlinkSync(logFilePath)
-      } else {
-        const newName = `${path.basename(logFile, '.log')}_${Date.now()}.log`
-        const newPath = path.join(directory, newName)
+    if (fs.existsSync(logFilePath) && fs.statSync(logFilePath).size !== 0) {
+      const newName = `${path.basename(logFile, '.log')}_${Date.now()}.log`
+      const newPath = path.join(directory, newName)
 
-        try {
-          fs.renameSync(logFilePath, newPath)
-          await uploadToBucket(newPath, bucketName)
-          fs.openSync(logFilePath, 'w')
-        } catch (err) {
-          console.log(err)
-        }
+      try {
+        fs.renameSync(logFilePath, newPath)
+        await uploadToBucket(newPath, bucketName)
+        fs.openSync(logFilePath, 'w')
+      } catch (err) {
+        console.log(err)
       }
     }
   }
@@ -64,9 +76,7 @@ const processLogs = async (directory, bucketName) => {
 
 const scheduledLPMTransfer = async () => {
   const logDirectory = path.join(__dirname, '../../NWS-logs')
-  // const bucketName = await getSecretKeyValue('nws/aws', 'S3Bucket')
-
-  const bucketName = 'ean-951'
+  const bucketName = await getSecretKeyValue('nws/aws', 'S3Bucket')
 
   await processLogs(logDirectory, bucketName)
 }

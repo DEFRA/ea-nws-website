77e5056f381e17bc5398d765bfe73dd1c6a9a624
megan.gallagher <megan.gallagher@ukcldos.com>
2024-08-27 12:08:21 +0000

    Added warning text and tests

--- frontend/src/organisation/pages/add-locations-in-bulk/unmatched-locations/dont-match-locations/DoNotAddLocationsPage.js
+++ frontend/src/organisation/pages/add-locations-in-bulk/unmatched-locations/dont-match-locations/DoNotAddLocationsPage.js
@@ -2,11 +2,14 @@ import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import BackLink from '../../../../../common/components/custom/BackLink'
 import Button from '../../../../../common/components/gov-uk/Button'
+import WarningText from '../../../../../common/components/gov-uk/WarningText'
 
 export default function DoNotAddLocationsPage () {
   const navigate = useNavigate()
 
-  const handleSubmit = async (event) => {}
+  const handleSubmit = async () => {
+    navigate('/organisation/home')
+  }
 
   return (
     <>
@@ -14,23 +17,28 @@ export default function DoNotAddLocationsPage () {
       <main className='govuk-main-wrapper govuk-!-padding-top-4'>
         <div className='govuk-grid-row'>
           <div className='govuk-grid-column-two-thirds'>
+            <h1 className='govuk-heading-l govuk-!-margin-top-6'>Do not add locations</h1>
             <div className='govuk-body'>
-              <h1 className='govuk-heading-l'>Do not add locations</h1>
+              <WarningText text={'If you continue, any locations not matched will not be added to this account and cannot be saved.'} />
+              <p>
               <>
                 {' '}
                 <Link to='/' className='govuk-link'>
                     Download a file of all the locations not matched
                 </Link>{' '}
-                    if you want to add these later, You'll need to update these address and reupload them.
+                    if you want to add these later. You'll need to update these address and reupload them.
                 </>
-                <Button
-                    className='govuk-button'
-                    text='Continue'
-                    onClick={handleSubmit}
-                />
+              </p>
+              </div>
+            <br /> 
+              <Button
+                className='govuk-button'
+                text='Continue'
+                onClick={handleSubmit}
+              />
             </div>
+           
           </div>
-        </div>
       </main>
     </>
   )

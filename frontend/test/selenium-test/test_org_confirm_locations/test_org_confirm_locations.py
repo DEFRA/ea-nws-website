6eea7bcb5a2ea812d9c108b035cda1033a90a3fa
megan.gallagher <megan.gallagher@ukcldos.com>
2024-09-03 14:08:51 +0000

    fixed selenium tests

--- frontend/test/selenium-test/test_org_confirm_locations/test_org_confirm_locations.py
+++ frontend/test/selenium-test/test_org_confirm_locations/test_org_confirm_locations.py
@@ -2,12 +2,13 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+from common import *
 
 url = "http://localhost:3000/organisation/manage-locations/confirm"
 
 def test__render_page(get_browser):
     browser = get_browser
-    browser.get(url)
+    navigate_to_auth_page_via_index(browser, url)
     assert "Why do some locations not match?" in browser.page_source
     assert browser.current_url == url
 

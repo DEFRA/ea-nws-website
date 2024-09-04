90ad0ce6a87c612628cbacfafc97fd4f925eac5a
jibran.haider <muhammadjibran.haider@leidos.com>
2024-09-04 12:57:15 +0000

    Fix organisation address test. EAN-568

--- frontend/test/selenium-test/test_org_registration/test_org_address.py
+++ frontend/test/selenium-test/test_org_registration/test_org_address.py
@@ -14,9 +14,9 @@ def test__empty_input(get_browser):
     assert browser.current_url == url
 
 def test_invalid_input(get_browser):
-    browser = get_browser 
+    browser = get_browser
     browser.get(url)
-    input_field = browser.find_element(By.NAME, "Organisation postcode")
+    input_field = browser.find_element(By.NAME, "Postcode")
     input_field.send_keys("invalid postcode")
     browser.find_element(By.CLASS_NAME, "govuk-button").click()
     assert "Enter a postcode in the correct format, like KT3 3QQ" in browser.page_source
@@ -25,7 +25,7 @@ def test_invalid_input(get_browser):
 def test_valid_input(get_browser):
     browser = get_browser
     browser.get(url)
-    input_field = browser.find_element(By.NAME, "Organisation postcode")
+    input_field = browser.find_element(By.NAME, "Postcode")
     input_field.send_keys("KT3 3QQ")
     browser.find_element(By.CLASS_NAME, "govuk-button").click()
     time.sleep(1)

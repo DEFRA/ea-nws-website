72e30724a2fe8927b63d53fa622daca475aa605c
camille.lecuyer <camille.lecuyer@leidos.com>
2024-09-04 10:53:21 +0000

    adding org profile mock in the index

--- frontend/src/common/pages/Index.js
+++ frontend/src/common/pages/Index.js
@@ -227,6 +227,60 @@ export default function IndexPage() {
     ]
   }
 
+  const mockOrgOne = {
+    id: '',
+    enabled: true,
+    firstname: 'John',
+    lastname: 'Smith',
+    emails: ['matthew.pepper@gmail.com'],
+    mobilePhones: ['07343454590'],
+    homePhones: ['01475721535'],
+    language: 'EN',
+    additionals: [
+      {
+        id: 'organisation',
+        value: {
+          name: 'THE 123 GROUP LIMITED',
+          address: 'Boulton House 17-21 Chorlton Street, Manchester, M1 3HY',
+          compHouseNum: '05897191',
+          emergencySector: false,
+          isAdminRegistering: true,
+          alternativeContact: {
+            firstName: 'Julia Joyce',
+            lastName: 'Breen',
+            email: 'julia@email.com',
+            telephone: '01339944433',
+            jobTitle: 'Director'
+          }
+        }
+      },
+      { id: 'signUpComplete', value: false }
+    ],
+    unverified: {
+      emails: [],
+      mobilePhones: [],
+      homePhones: []
+    },
+    pois: [
+      {
+        name: 'Big Ben, London, SW1A 0AA',
+        address: 'UPRN',
+        coordinates: {
+          latitude: 51.5007,
+          longitude: 0.1246
+        }
+      },
+      {
+        name: 'Kingfisher Way, London, NW10 8TZ',
+        address: 'UPRN',
+        coordinates: {
+          latitude: 51.550738,
+          longitude: -0.257635
+        }
+      }
+    ]
+  }
+
   function mockSession(profile) {
     if (mockSessionActive === false) {
       const authToken = 'MockAuthToken'
@@ -379,6 +433,11 @@ export default function IndexPage() {
                 className='govuk-button'
                 text='Activate/Deactivate Mock Session 3'
                 onClick={() => mockSession(mockThree)}
+              />
+              <Button
+                className='govuk-button'
+                text='Activate/Deactivate Mock Org Session 1'
+                onClick={() => mockSession(mockOrgOne)}
               />
               <ul className='govuk-list'>
                 <li>

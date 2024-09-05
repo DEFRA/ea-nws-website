f27c75cd39fc176a590675dfa5a165b9b8712887
camille.lecuyer <camille.lecuyer@leidos.com>
2024-09-05 10:52:27 +0000

    updated tests to use common.py

--- frontend/test/selenium-test/test_signup/test_review/test_signup_organisation_review.py
+++ frontend/test/selenium-test/test_signup/test_review/test_signup_organisation_review.py
@@ -2,21 +2,16 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+from common import *
 
-index_page = "http://localhost:3000/index"
-home_page = "http://localhost:3000/organisation/home"
-review_page = "http://localhost:3000/organisation/sign-up/review"
-change_organisationdetails_page = "http://localhost:3000/"
-change_main_administrator_page = "http://localhost:3000/"
-finish_page = "http://localhost:3000/organisation/sign-up/success"
-change_alternative_contact_page = ""
+review_page = url_org_signup.get('review')
+change_organisationdetails_page = url_org_man_loc.get('change').get('org_details')
+change_main_administrator_page = url_org_man_loc.get('change').get('main_admin')
+success_page = url_org_signup.get('success')
+change_alternative_contact_page = url_org_man_loc.get('change').get('alternative_contact')
 
 def setup_reviewpage_test(get_browser):
-    browser = get_browser
-    browser.get(index_page)
-    button_xpath = f"//button[text()='Activate/Deactivate Mock Org Session 1']"
-    mock_session_link = browser.find_element(By.XPATH, button_xpath)
-    browser.execute_script("arguments[0].click();", mock_session_link)
+    browser = activate_mock_org_1(get_browser)
     link_xpath = f"//a[text()='Sign up organisation review']"
     link_link = browser.find_element(By.XPATH, link_xpath)
     browser.execute_script("arguments[0].click();", link_link)
@@ -33,29 +28,16 @@ def test_reviewpage_render(get_browser):
 
 def test_reviewpage_successpage(get_browser):
     browser = setup_reviewpage_test(get_browser)
-    button_xpath = f"//button[text()='Finish and submit']"
-    finish_link = browser.find_element(By.XPATH, button_xpath)
-    browser.execute_script("arguments[0].click();", finish_link)
-    time.sleep(1)
-    assert browser.current_url == finish_page
+    click_button(browser, 'Finish and submit', success_page)
 
 def test_reviewpage_change_orgname(get_browser):
     browser = setup_reviewpage_test(get_browser)
-    button_xpath = f"//a[text()='Change']"
-    change_orgName_link = browser.find_element(By.XPATH, button_xpath)
-    browser.execute_script("arguments[0].click();", change_orgName_link)
-    assert browser.current_url == change_organisationdetails_page   
+    click_link(browser, 'Change', change_organisationdetails_page) 
 
 def test_reviewpage_change_mainAdmin_name(get_browser):
     browser = setup_reviewpage_test(get_browser)
-    button_xpath = f"//a[text()='Change']"
-    change_admin_link = browser.find_element(By.XPATH, button_xpath)
-    browser.execute_script("arguments[5].click();", change_admin_link)
-    assert browser.current_url == change_main_administrator_page  
+    click_link_more_than_one_text(browser, 'Change', 5, change_main_administrator_page)
 
 def test_reviewpage_change_alternativeContact_name(get_browser):
     browser = setup_reviewpage_test(get_browser)
-    button_xpath = f"//a[text()='Change']"
-    change_alternate_contact_name_link = browser.find_element(By.XPATH, button_xpath)
-    browser.execute_script("arguments[7].click();", change_alternate_contact_name_link)
-    assert browser.current_url == change_alternative_contact_page 
+    click_link_more_than_one_text(browser, 'Change', 7, change_main_administrator_page)

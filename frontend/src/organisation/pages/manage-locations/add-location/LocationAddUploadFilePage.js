60adcf1875de520fbd1831faf1477dc936fa4f5c
jibran.haider <muhammadjibran.haider@leidos.com>
2024-09-11 11:51:11 +0000

    Fix warnings. EAN-1107

--- frontend/src/organisation/pages/manage-locations/add-location/LocationAddUploadFilePage.js
+++ frontend/src/organisation/pages/manage-locations/add-location/LocationAddUploadFilePage.js
@@ -8,7 +8,7 @@ import LoadingSpinner from '../../../../common/components/custom/LoadingSpinner'
 import Button from '../../../../common/components/gov-uk/Button'
 import ErrorSummary from '../../../../common/components/gov-uk/ErrorSummary'
 
-export default function LocationAddUploadFilePage() {
+export default function LocationAddUploadFilePage () {
   const navigate = useNavigate()
   const [errorFileType, setErrorFileType] = useState('')
   const [errorFileSize, setErrorFileSize] = useState('')
@@ -109,59 +109,63 @@ export default function LocationAddUploadFilePage() {
           {(errorFileType || errorFileSize) && (
             <ErrorSummary errorList={[errorFileType, errorFileSize]} />
           )}
-          {!uploading ? (
-            <div className='govuk-grid-column-full'>
-              <h1 className='govuk-heading-l'>Upload file</h1>
-              {!errorFileType && !errorFileSize ? (
-                <div className='govuk-form-group'>
-                  <p className='govuk-hint'>File can be .xls, .xlsx, .csv</p>
-                  <input
-                    type='file'
-                    class='govuk-file-upload'
-                    id='file-upload'
-                    onChange={setFile}
-                  />
-                </div>
-              ) : (
-                <div className='govuk-form-group govuk-form-group--error'>
-                  <p className='govuk-hint'>File can be .xls, .xlsx, .csv</p>
-                  {errorFileType && (
-                    <p id='file-upload-1-error' class='govuk-error-message'>
-                      <span class='govuk-visually-hidden'>Error:</span>
-                      {errorFileType}
-                    </p>
-                  )}
-                  {errorFileSize && (
-                    <p id='file-upload-1-error' class='govuk-error-message'>
-                      <span class='govuk-visually-hidden'>Error:</span>
-                      {errorFileSize}
-                    </p>
-                  )}
-                  <input
-                    type='file'
-                    class='govuk-file-upload govuk-file-upload--error'
-                    id='file-upload'
-                    onChange={setFile}
-                  />
-                </div>
+          {!uploading
+            ? (
+              <div className='govuk-grid-column-full'>
+                <h1 className='govuk-heading-l'>Upload file</h1>
+                {!errorFileType && !errorFileSize
+                  ? (
+                    <div className='govuk-form-group'>
+                      <p className='govuk-hint'>File can be .xls, .xlsx, .csv</p>
+                      <input
+                        type='file'
+                        className='govuk-file-upload'
+                        id='file-upload'
+                        onChange={setFile}
+                      />
+                    </div>
+                    )
+                  : (
+                    <div className='govuk-form-group govuk-form-group--error'>
+                      <p className='govuk-hint'>File can be .xls, .xlsx, .csv</p>
+                      {errorFileType && (
+                        <p id='file-upload-1-error' className='govuk-error-message'>
+                          <span className='govuk-visually-hidden'>Error:</span>
+                          {errorFileType}
+                        </p>
+                      )}
+                      {errorFileSize && (
+                        <p id='file-upload-1-error' className='govuk-error-message'>
+                          <span className='govuk-visually-hidden'>Error:</span>
+                          {errorFileSize}
+                        </p>
+                      )}
+                      <input
+                        type='file'
+                        className='govuk-file-upload govuk-file-upload--error'
+                        id='file-upload'
+                        onChange={setFile}
+                      />
+                    </div>
+                    )}
+                <Button
+                  text='Upload'
+                  className='govuk-button'
+                  onClick={handleUpload}
+                />
+                <Link
+                  onClick={() => navigate(-1)}
+                  className='govuk-body govuk-link inline-link'
+                >
+                  Cancel
+                </Link>
+              </div>
+              )
+            : (
+              <div className='hods-loading-spinner'>
+                <LoadingSpinner>Uploading</LoadingSpinner>
+              </div>
               )}
-              <Button
-                text='Upload'
-                className='govuk-button'
-                onClick={handleUpload}
-              />
-              <Link
-                onClick={() => navigate(-1)}
-                className='govuk-body govuk-link inline-link'
-              >
-                Cancel
-              </Link>
-            </div>
-          ) : (
-            <div class='hods-loading-spinner'>
-              <LoadingSpinner>Uploading</LoadingSpinner>
-            </div>
-          )}
         </div>
       </main>
     </>

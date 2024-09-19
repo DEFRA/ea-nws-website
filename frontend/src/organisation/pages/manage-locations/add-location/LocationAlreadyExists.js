a1bd374382cf3d860e9aae39fa5e4fe5ae1ec56f
thomas barron <thomas.barron@uk.leidos.com>
2024-09-19 13:39:15 +0000

    WIP: create basic template

---
+++ frontend/src/organisation/pages/manage-locations/add-location/LocationAlreadyExists.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import BackLink from '../../../../common/components/custom/BackLink'
+import OrganisationAccountNavigation from '../../../../common/components/custom/OrganisationAccountNavigation'
+import { useNavigate } from 'react-router'
+import { Link } from 'react-router-dom'
+
+export default function LocationAlreadyExists () {
+  const navigate = useNavigate()
+  return (
+    <>
+      <OrganisationAccountNavigation />
+      <BackLink onClick={() => navigate(-1)} />
+      <main className='govuk-main-wrapper govuk-!-padding-top-4'>
+        <div className='govuk-grid-row'>
+          <div className='govuk-grid-column-two-thirds' />
+          <h1 className='govuk-heading-l'> "enter location var" already exists in this account</h1>
+          <h2 className='govuk-heading-m'>What do you want to do next?</h2>
+          <ul className='govuk-list'>
+            <p className='govuk-body'> You can:</p>
+            <li>
+              <Link 
+              to="/"
+              className='govuk-link'
+              >
+                use a differnt location name for this location
+              </Link>
+            </li>
+            <li>
+              <Link 
+              to="/"
+              className='govuk-link'
+              >
+                edit the existing location in this account
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </main>
+    </>
+  )
+}

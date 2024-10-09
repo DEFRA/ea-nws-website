08f86dee9559ebbb7d6408cc8627113e97e7ec25
camille.lecuyer <camille.lecuyer@leidos.com>
2024-10-09 16:20:44 +0000

    import correction

--- frontend/src/organisation/pages/manage-locations/add-location/unmatched-location/NotInEnglandPage.js
+++ frontend/src/organisation/pages/manage-locations/add-location/unmatched-location/NotInEnglandPage.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import NotInEnglandLayout from '../../../../layouts/add-location/unmatched-locations/NotInEnglandLayout'
+import NotInEnglandLayout from '../../../../layouts/location/unmatched-locations/NotInEnglandLayout'
 import { orgManageLocationsUrls } from '../../../../routes/manage-locations/ManageLocationsRoutes'
 
-export default function NotInEnglandPage () {
+export default function NotInEnglandPage() {
   const navigate = useNavigate()
 
   const NavigateToPreviousPage = () => {

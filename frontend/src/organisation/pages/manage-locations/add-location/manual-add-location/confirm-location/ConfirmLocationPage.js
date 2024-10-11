b1ddec64c0ea6db6e93d02913656327522c3099f
camille.lecuyer <camille.lecuyer@leidos.com>
2024-10-11 14:38:50 +0000

    lint

--- frontend/src/organisation/pages/manage-locations/add-location/manual-add-location/confirm-location/ConfirmLocationPage.js
+++ frontend/src/organisation/pages/manage-locations/add-location/manual-add-location/confirm-location/ConfirmLocationPage.js
@@ -1,7 +1,7 @@
 import { React } from 'react'
 import ConfirmLocationLayout from '../../../../../layouts/location/manual-add-or-edit-location/confirm-location/ConfirmLocationLayout'
 
-export default function ConfirmLocationPage() {
+export default function ConfirmLocationPage () {
   return (
     <>
       <ConfirmLocationLayout />

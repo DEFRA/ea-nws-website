a08dfcb97815b1ee0beafede0e78877ab21e1ea0
camille.lecuyer <camille.lecuyer@leidos.com>
2024-10-14 13:25:47 +0000

    folder cleanup

--- frontend/src/organisation/layouts/location/add-or-edit-location/edit-shape/CannotChangeLocationManuallyLayout.js
+++ frontend/src/organisation/layouts/location/add-or-edit-location/edit-shape/CannotChangeLocationManuallyLayout.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import BackLink from '../../../../../../../common/components/custom/BackLink'
-import OrganisationAccountNavigation from '../../../../../../../common/components/custom/OrganisationAccountNavigation'
+import BackLink from '../../../../../common/components/custom/BackLink'
+import OrganisationAccountNavigation from '../../../../../common/components/custom/OrganisationAccountNavigation'
 
 export default function CannotChangeLocationManuallyLayout ({
   NavigateToPreviousPage,
--- frontend/src/organisation/pages/manage-locations/view-location/edit-location/edit-line-location/CannotChangeLocationLinePage.js
+++ frontend/src/organisation/pages/manage-locations/view-location/edit-location/edit-line-location/CannotChangeLocationLinePage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import CannotChangeLocationManuallyLayout from '../../../../../layouts/location/view/view-location-information/edit-location/edit-shape/CannotChangeLocationManuallyLayout'
+import CannotChangeLocationManuallyLayout from '../../../../../layouts/location/add-or-edit-location/edit-shape/CannotChangeLocationManuallyLayout'
 export default function CannotChangeLocationLinePage () {
   const navigate = useNavigate()
 

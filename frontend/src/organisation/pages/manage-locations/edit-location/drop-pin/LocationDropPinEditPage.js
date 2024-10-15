739cedb6972ecb6e56eaaf388912ad0d1047ab39
James.Mainwaring <james.r.mainwaring@uk.leidos.com>
2024-10-15 10:04:15 +0000

    Commit work in progress

---
+++ frontend/src/organisation/pages/manage-locations/edit-location/drop-pin/LocationDropPinEditPage.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import LocationDropPinSearchResultsLayout from '../../../../layouts/location/manual-add-or-edit-location/search/drop-pin/LocationDropPinSearchResultsLayout'
+import { orgManageLocationsUrls } from '../../../../routes/manage-locations/ManageLocationsRoutes'
+
+export default function LocationDropPinEditPage () {
+  const navigate = useNavigate()
+
+  const NavigateToNextPage = () => {
+    navigate(orgManageLocationsUrls.add.search.dropPinSearchResults)
+  }
+
+  const NavigateToPreviousPage = () => {
+    navigate(-1)
+  }
+
+  const NavigateToNotInEnglandPage = () => {
+    navigate(orgManageLocationsUrls.add.error.dropPinNotInEngland)
+  }
+
+  const differentSearchUrl = orgManageLocationsUrls.add.search.dropPinSearch
+
+  return (
+    <LocationDropPinSearchResultsLayout
+      Type='edit'
+      NavigateToNextPage={NavigateToNextPage}
+      NavigateToPreviousPage={NavigateToPreviousPage}
+      NavigateToNotInEnglandPage={NavigateToNotInEnglandPage}
+      DifferentSearchUrl={differentSearchUrl}
+    />
+  )
+}

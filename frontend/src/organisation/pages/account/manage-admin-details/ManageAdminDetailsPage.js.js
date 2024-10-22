b639267d602600adc8dcb1078fb7c7c2b5444011
thomas barron <thomas.barron@uk.leidos.com>
2024-10-22 11:44:33 +0000

    lint

--- frontend/src/organisation/pages/account/manage-admin-details/ManageAdminDetailsPage.js.js
+++ frontend/src/organisation/pages/account/manage-admin-details/ManageAdminDetailsPage.js.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import BackLink from '../../../../common/components/custom/BackLink'
-import MainAdministratorTable from '../../sign-up/review/MainAdministratorTable'
+//import MainAdministratorTable from '../../sign-up/review/MainAdministratorTable'
 import OrganisationAccountNavigation from '../../../../common/components/custom/OrganisationAccountNavigation'
 import { useNavigate } from 'react-router'
 import { useSelector } from 'react-redux'
 export default function ManageAdmindetailsPage () {
-    //WIP page
-    const profile = useSelector((state) => state.session.profile)
-    const navigate = useNavigate()
+  // WIP page
+  //const profile = useSelector((state) => state.session.profile)
+  const navigate = useNavigate()
 
   const navigateBack = (event) => {
     event.preventDefault()
@@ -19,10 +19,10 @@ export default function ManageAdmindetailsPage () {
       <OrganisationAccountNavigation />
       <BackLink onClick={navigateBack} />
       <main className='govuk-main-wrapper govuk-!-padding-top-4'>
-      <div className='govuk-grid-row '>
-        <h2 className='govuk-heading-m'>Manage administratir details</h2>
-        <h3 className='govuk-heading-s'>Contact details</h3>
-      </div>
+        <div className='govuk-grid-row '>
+          <h2 className='govuk-heading-m'>Manage administratir details</h2>
+          <h3 className='govuk-heading-s'>Contact details</h3>
+        </div>
       </main>
     </>
   )

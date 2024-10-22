7abdafa67136e91c4e550ad531468eefdf692d72
thomas barron <thomas.barron@uk.leidos.com>
2024-10-22 08:26:17 +0000

    create route for first page

--- frontend/src/organisation/routes/account/AccountRoutes.js
+++ frontend/src/organisation/routes/account/AccountRoutes.js
@@ -1,11 +1,28 @@
 import ManageOrganisationDetailsPage from '../../pages/account/ManageOrganisationDetailsPage'
+import ManageAdmindetailsPage from '../../pages/account/manage-admin-details/ManageAdminDetailsPage.js.js'
 
-// home
+const urlOrg = '/organisation'
+const urlManageAdmin = urlOrg + '/manage-admin'
+
+const accountUrls = {
+  organisation:{
+    orgDetails: urlOrg + '/manage-organisation-details'
+  },
+  admin:{ 
+    details: urlManageAdmin + '/admin-details'
+
+  }
+}
+
 const accountRoutes = [
   {
-    path: '/organisation/manage-organisation-details',
-    component: <ManageOrganisationDetailsPage />
+    path: accountUrls.organisation.orgDetails,
+    component: <ManageOrganisationDetailsPage/>
+  },
+  {
+    path: accountUrls.admin.details,
+    component: <ManageAdmindetailsPage/>
   }
 ]
 
-export default accountRoutes
+export {accountRoutes,accountUrls}

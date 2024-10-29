faaf9583f1aed5c8ddc980b5d3ffe528460653e2
camille.lecuyer <camille.lecuyer@leidos.com>
2024-10-29 09:10:42 +0000

    correcting tests

--- frontend/test/selenium-test/test_org_manage_contacts/add_contact/test_org_add_contact.py
+++ frontend/test/selenium-test/test_org_manage_contacts/add_contact/test_org_add_contact.py
@@ -22,26 +22,29 @@ def test_click_continue_empty(get_browser):
     assert 'Enter first name' in get_browser.page_source
     assert 'Enter last name' in get_browser.page_source
 
-def test_click_continue_input_too_long(get_browser):
+def test_click_failure_input_firstname_too_long(get_browser):
     navigate_to_auth_page_via_index(get_browser,url_add_contact_details)
     enter_input_text(get_browser, 'First name', too_long_input)
-    enter_input_text(get_browser, 'Last name', too_long_input)
     assert 'There is a problem' in get_browser.page_source
     assert 'First name must be 20 characters or less' in get_browser.page_source
-    assert 'Last name must be 20 characters or less' in get_browser.page_source
-    click_button(get_browser, 'Continue', url_add_contact_details)
     enter_input_text(get_browser, 'First name', right_input)
-    enter_input_text(get_browser, 'Last name', right_input)
     assert 'There is a problem' not in get_browser.page_source
     assert 'First name must be 20 characters or less' not in get_browser.page_source
+
+def test_click_failure_input_lastname_too_long(get_browser):
+    navigate_to_auth_page_via_index(get_browser,url_add_contact_details)
+    enter_input_text(get_browser, 'Last name', too_long_input)
+    assert 'There is a problem' in get_browser.page_source
+    assert 'Last name must be 20 characters or less' in get_browser.page_source
+    enter_input_text(get_browser, 'Last name', right_input)
+    assert 'There is a problem' not in get_browser.page_source
     assert 'Last name must be 20 characters or less' not in get_browser.page_source
 
-def test_click_continue_input_too_long_job_title(get_browser):
+def test_click_failure_input_jobtitle_too_long(get_browser):
     navigate_to_auth_page_via_index(get_browser,url_add_contact_details)
     enter_input_text(get_browser, 'Job title (optional)', too_long_input)
     assert 'There is a problem' in get_browser.page_source
     assert 'Job title must be 20 characters or less' in get_browser.page_source
-    click_button(get_browser, 'Continue', url_add_contact_details)
     enter_input_text(get_browser, 'Job title (optional)', right_input)
     assert 'There is a problem' not in get_browser.page_source
     assert 'Job title must be 20 characters or less' not in get_browser.page_source

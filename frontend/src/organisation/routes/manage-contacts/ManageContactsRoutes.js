25a7445055472a912cbcd55df951a80d60755e31
camille.lecuyer <camille.lecuyer@leidos.com>
2024-10-30 10:55:06 +0000

    updating routes

--- frontend/src/organisation/routes/manage-contacts/ManageContactsRoutes.js
+++ frontend/src/organisation/routes/manage-contacts/ManageContactsRoutes.js
@@ -1,10 +1,12 @@
 import AddContactDetailsPage from '../../pages/manage-contact/add-contact/AddContactDetailsPage'
+import AddContactNotesPage from '../../pages/manage-contact/add-contact/AddContactNotesPage'
 
 const urlManageContactsOrg = 'organisation/manage-contacts'
 
 const orgManageContactsUrls = {
   add: {
-    addNew: urlManageContactsOrg + '/add'
+    addNew: urlManageContactsOrg + '/add',
+    notes: urlManageContactsOrg + '/add/notes'
   }
 }
 
@@ -12,6 +14,10 @@ const orgManageContactsRoutes = [
   {
     path: orgManageContactsUrls.add.addNew,
     component: <AddContactDetailsPage />
+  },
+  {
+    path: orgManageContactsUrls.add.notes,
+    component: <AddContactNotesPage />
   }
 ]
 

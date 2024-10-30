8faaf31b9f601d80fe153a37403c9c716a64d4c4
camille.lecuyer <camille.lecuyer@leidos.com>
2024-10-30 16:09:12 +0000

    adding route

--- frontend/src/organisation/routes/manage-contacts/ManageContactsRoutes.js
+++ frontend/src/organisation/routes/manage-contacts/ManageContactsRoutes.js
@@ -1,10 +1,12 @@
+import AddContactChannelsPage from '../../pages/manage-contact/add-contact/AddContactChannelsPage'
 import AddContactDetailsPage from '../../pages/manage-contact/add-contact/AddContactDetailsPage'
 
 const urlManageContactsOrg = 'organisation/manage-contacts'
 
 const orgManageContactsUrls = {
   add: {
-    addNew: urlManageContactsOrg + '/add'
+    addNew: urlManageContactsOrg + '/add',
+    channels: urlManageContactsOrg + '/add/channels'
   }
 }
 
@@ -12,6 +14,10 @@ const orgManageContactsRoutes = [
   {
     path: orgManageContactsUrls.add.addNew,
     component: <AddContactDetailsPage />
+  },
+  {
+    path: orgManageContactsUrls.add.channels,
+    component: <AddContactChannelsPage />
   }
 ]
 

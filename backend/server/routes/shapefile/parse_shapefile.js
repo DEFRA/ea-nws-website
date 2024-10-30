dacc7fbdd13e3d634a97406dc50e707134728d2c
ronan.peacock <ronan.peacock@leidos.com>
2024-10-30 14:53:03 +0000

    Upload to S3 and retrieve

--- backend/server/routes/shapefile/parse_shapefile.js
+++ backend/server/routes/shapefile/parse_shapefile.js
@@ -1,8 +1,12 @@
-const fs = require('fs')
+const {
+  S3Client,
+  GetObjectCommand,
+  CreateMultipartUploadCommand,
+  UploadPartCommand,
+  CompleteMultipartUploadCommand
+} = require('@aws-sdk/client-s3')
 const unzipper = require('unzipper')
-const shapefile = require('shapefile')
-const { error } = require('console')
-const { ok } = require('assert')
+const stream = require('stream')
 
 module.exports = [
   {
@@ -10,37 +14,45 @@ module.exports = [
     path: '/api/shapefile/parse',
     handler: async (request, h) => {
       try {
-        if (!request.payload || !request.payload.file) {
+        if (!request.payload) {
           createGenericErrorResponse(h)
         }
 
-        // Unzip shapefile archive in memory
-        const zipStream = request.payload.file._data
-        const files = {}
-        await zipStream
-          .pipe(unzipper.Parse())
-          .on('entry', async (entry) => {
-            const fileName = entry.path.toLowerCase()
-            const fileExt = fileName.split('.').pop()
+        const { name, fileType } = request.payload
 
-            if (fileExt === 'shp' || fileExt === 'spx' || fileExt === 'dbf') {
-              files[fileExt] = await entry.buffer()
-            } else {
-              entry.autodrain()
+        if (name && fileType) {
+          const zipFileKey = `${Date.now()}_${name}`
+
+          const client = new S3Client()
+          const s3BucketName = '#' // Probably want to retrieve this from secrets manager
+
+          // Stream zip file from S3
+          const zippedFileStream = client.send(
+            new GetObjectCommand({
+              Bucket: s3BucketName,
+              Key: zipFileKey
+            })
+          ).Body
+          const passThroughStream = new stream.PassThrough()
+
+          // Process zip file
+          zippedFileStream.pipe(unzipper.Parse()).on('entry', async (entry) => {
+            const { path: fileName } = entry
+            if (
+              !fileName.endsWith('.shp') &&
+              !fileName.endsWith('.dbf') &&
+              !fileName.endsWith('shx')
+            ) {
+              entry.autodrain() // Skip non-shapefiles
+              return
             }
           })
-          .promise()
-
-        if (!files.shp || !files.dbf) {
+        } else {
           return h.response({
-            status: 400,
-            errorMessage: 'Invalid shapefile structure'
+            status: 500,
+            errorMessage: 'File name and/or type not provided'
           })
         }
-
-        const geojson = await shapefile.read(files.shp, files.dbf, files.shx)
-
-        return h.response({ status: 200, data: { geojson } })
       } catch (error) {
         createGenericErrorResponse(h)
       }

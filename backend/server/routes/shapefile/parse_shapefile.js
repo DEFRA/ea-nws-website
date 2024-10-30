1c057a05fc41880dd2a733c75e93e3763bb6614d
ronan.peacock <ronan.peacock@leidos.com>
2024-10-30 15:03:22 +0000

    Logic changes

--- backend/server/routes/shapefile/parse_shapefile.js
+++ backend/server/routes/shapefile/parse_shapefile.js
@@ -7,6 +7,8 @@ const {
 } = require('@aws-sdk/client-s3')
 const unzipper = require('unzipper')
 const stream = require('stream')
+const { CLIENT_RENEG_WINDOW } = require('tls')
+const { createGenericErrorResponse } = require('../../services/GenericErrorResponse')
 
 module.exports = [
   {
@@ -46,7 +48,49 @@ module.exports = [
               entry.autodrain() // Skip non-shapefiles
               return
             }
+
+            // Prepare for multipart upload to S3
+            const multipartUpload = await client.send(new CreateMultipartUploadCommand {
+              Bucket: s3BucketName,
+              Key: `unzipped/${fileName}`,
+              ContentType: 'application/octet-stream'
+            })
+            let chunkNum = 1
+            const uploadPromises =[]
+            const bufferStream = new stream.PassThrough
+
+            entry.pipe(bufferStream)
+              .on('data', async (chunk) => {
+                const uploadPartParams = {
+                  Bucket: s3BucketName,
+                  Key: `unzipped/${fileName}`,
+                  UploadId: multipartUpload.UploadId,
+                  PartNumber: partNumber,
+                  Body: chunk
+                }
+                uploadPromises.push(client.send(new UploadPartCommand(uploadPartParams)))
+                chunkNum += 1
+              })
+              .on('end', async () => {
+                const completedUpload = await Promise.all(uploadPromises)
+                const completedParams = {
+                  Bucket: s3BucketName,
+                  Key: `unzipped/${fileName}`,
+                  UploadId: multipartUpload.UploadId,
+                  MultipartUpload: {
+                    Parts: completedUpload.map((part, i) => ({
+                      ETag: part.ETag,
+                      PartNumber: i + 1
+                    }))
+                }}
+                await client.send(new CompleteMultipartUploadCommand(completedParams))
+              })
+              .on('error', (err) => {
+                console.log("Error streaming zip: ", err)
+                createGenericErrorResponse(h)
+              })
           })
+
         } else {
           return h.response({
             status: 500,

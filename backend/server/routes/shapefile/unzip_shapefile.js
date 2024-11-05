d1a0ffeaa54e2807d5ed097219144835e18fa3db
ronan.peacock <ronan.peacock@leidos.com>
2024-11-05 10:28:51 +0000

    Different file unzip

--- backend/server/routes/shapefile/unzip_shapefile.js
+++ backend/server/routes/shapefile/unzip_shapefile.js
@@ -4,10 +4,11 @@ const {
   CreateMultipartUploadCommand,
   UploadPartCommand,
   CompleteMultipartUploadCommand,
-  InventoryS3BucketDestinationFilterSensitiveLog
+  InventoryS3BucketDestinationFilterSensitiveLog,
+  PutObjectCommand
 } = require('@aws-sdk/client-s3')
 const unzipper = require('unzipper')
-const { PassThrough } = require('stream')
+const { PassThrough, pipeline } = require('stream')
 const {
   createGenericErrorResponse
 } = require('../../services/GenericErrorResponse')
@@ -22,62 +23,55 @@ module.exports = [
           return createGenericErrorResponse(h)
         }
 
-        const { fileName } = request.payload
-
-        if (fileName) {
-          const client = new S3Client()
-          const s3BucketName = await getSecretKeyValue(
-            'nws/aws',
-            'bulkUploadBucket'
-          )
-
-          if (!s3BucketName) {
-            console.error('S3 Bucket value undefined in Secrets Manager')
-            return h.response({ status: 500, message: 'Secret error' })
-          }
-
-          const zipFilePath = `zip-uploads/${fileName}`
-          const outputFolder = `zip-uploads/${fileName.replace('.zip', '')}/`
-
-          // Retrieve zip file from bucket
-          const getObjectCmd = new GetObjectCommand({
-            Bucket: s3BucketName,
-            Key: zipFilePath
+        const { zipFileName } = request.payload
+        if (!zipFileName) {
+          return h.response({
+            status: 500,
+            message: 'No zip file provided'
           })
-          const zipFileStream = await client.send(getObjectCmd)
-
-          // Unzip and send each file back to bucket
-          const unzipStream = zipFileStream.Body.pipe(unzipper.Parse())
-          for await (const entry of unzipStream) {
-            const { path: filePath, type } = entry
-            if (type === 'File') {
-              // Create stream to upload each extracted file
-              const passThroughStream = new PassThrough()
-              const uploadParams = {
-                Bucket: s3BucketName,
-                Key: `${outputFolder}${filePath}`,
-                Body: passThroughStream
-              }
-
-              // Start multipart upload
-              const uploadCmd = new CreateMultipartUploadCommand(uploadParams)
-              const uploadResponse = await client.send(uploadCmd)
-              entry.pipe(passThroughStream)
-
-              // Complete upload
-              await client.send(
-                new CompleteMultipartUploadCommand({
-                  Bucket: s3BucketName,
-                  Key: `${outputFolder}${filePath}`,
-                  UploadId: uploadResponse.UploadId
-                })
-              )
-            } else {
-              // Skip non-files
-              entry.autodrain()
-            }
-          }
         }
+        const zipFileKey = `zip-uploads/${zipFileName}`
+        const client = new S3Client()
+        const s3BucketName = await getSecretKeyValue(
+          'nws/aws',
+          'bulkUploadBucket'
+        )
+
+        // Retrieve zip file from S3
+        const zipFileStream = await client.send(
+          new GetObjectCommand({
+            Bucket: s3BucketName,
+            Key: zipFileKey
+          })
+        ).Body
+
+        const outputFolder = `${zipFileKey.replace('.zip', '/')}`
+
+        // Unzip and upload each file back to bucket
+        await new Promise((resolve, reject) => {
+          pipeline(
+            zipFileStream,
+            unzipper.Parse(),
+            async (entryStream) => {
+              for await (const entry of entryStream) {
+                const filePath = `${outputFolder}${entry.path}`
+                const uploadStream = new PassThrough()
+                await client.send(
+                  new PutObjectCommand({
+                    Bucket: s3BucketName,
+                    Key: filePath,
+                    Body: uploadStream
+                  })
+                )
+                entry.pipe(uploadStream)
+              }
+            },
+            (error) => {
+              if (error) reject(error)
+              else resolve()
+            }
+          )
+        })
 
         return h.response({
           status: 200,

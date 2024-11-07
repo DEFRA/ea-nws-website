8cefc512de1575af0244c23b9bd6f9dd0d9d910d
James.Mainwaring <james.r.mainwaring@uk.leidos.com>
2024-11-07 11:32:56 +0000

    Address review comments

--- frontend/src/organisation/pages/manage-locations/add-location/optional-information/NotesPage.js
+++ frontend/src/organisation/pages/manage-locations/add-location/optional-information/NotesPage.js
@@ -1,20 +1,15 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router'
 import NotesLayout from '../../../../layouts/optional-info/NotesLayout'
 import { orgManageLocationsUrls } from '../../../../routes/manage-locations/ManageLocationsRoutes'
 
 export default function AddNotesPage () {
   const navigate = useNavigate()
-  const predefinedBoundaryFlow = useSelector(
-    (state) => state.session.predefinedBoundaryFlow
-  )
 
   const navigateToNextPage = () => {
-    if (predefinedBoundaryFlow) {
-      navigate(orgManageLocationsUrls.add.predefinedBoundary.addAnother)
-    }
-    // TODO: add 'else if' for other flows as needed
+    // TODO: Navigate to correct destination when available.
+    //       Notes test will need updating afterwards.
+    navigate(orgManageLocationsUrls.add.predefinedBoundary.addAnother)
   }
 
   return (
--- frontend/test/selenium-test/test_org_add_optional_location/test_add_notes.py
+++ frontend/test/selenium-test/test_org_add_optional_location/test_add_notes.py
@@ -2,8 +2,6 @@ import time
 from common import *
 
 # Urls
-url_add_options = url_org_man_loc.get('add').get('options')
-url_add_predefined_boundary = url_org_man_loc.get('add').get('predefinedBoundary').get('add')
 url_action_plan= url_org_man_loc.get('optionalLocation').get('addActionPlan')
 url_notes  = url_org_man_loc.get('optionalLocation').get('addNotes')
 url_next_page = url_org_man_loc.get('add').get('predefinedBoundary').get('addAnother')
@@ -12,25 +10,6 @@ max_chars = 500
 text_too_long = 'a' * (max_chars + 1)
 text_just_right = 'b' * max_chars
 text_under_req  = 'c' * (max_chars - 1)
-
-def render_add_location_page(browser):
-    navigate_to_auth_page_via_index(browser, url_add_options)
-    assert check_h1_heading(browser, 'How do you want to add locations?')
-
-# When Continue is clicked on the Notes page, the next page is determined
-# by the current flow. For these tests we will use the Add Predefined Boundary
-# flow. This will allow us to know which page should be the next page when
-# Continue is clicked.
-def navigate_to_notes_page_via_boundary_flow(browser):
-    render_add_location_page(browser)
-    select_input_radio_option(browser, 'PredefinedBoundaries')
-    click_button(browser, 'Continue', url_add_predefined_boundary)
-    select_dropdown_option(browser, 'BoundaryType', 'aoi-county')
-    time.sleep(2)
-    select_dropdown_option(browser, 'Boundary', 'Hampshire County')
-    time.sleep(2)
-    click_button(browser, 'Add predefined boundary', url_action_plan)
-    click_button(browser, 'Continue', url_notes)
 
 def test_render(get_browser):
     navigate_to_auth_page_via_index(get_browser, url_notes)
@@ -44,21 +23,21 @@ def test_back_button(get_browser):
     click_link(get_browser, "Back", url_action_plan)
 
 def test_continue_empty(get_browser):
-    navigate_to_notes_page_via_boundary_flow(get_browser)
+    navigate_to_auth_page_via_index(get_browser, url_notes)
     click_button(get_browser, 'Continue', url_next_page)
 
 def test_continue_filled_text_length_just_right(get_browser):
-    navigate_to_notes_page_via_boundary_flow(get_browser)
+    navigate_to_auth_page_via_index(get_browser, url_notes)
     enter_textarea_text(get_browser, 'govuk-textarea', text_just_right, 'id')
     click_button(get_browser, 'Continue', url_next_page)
 
 def test_continue_filled_text_length_too_long_failure(get_browser):
-    navigate_to_notes_page_via_boundary_flow(get_browser)
+    navigate_to_auth_page_via_index(get_browser, url_notes)
     enter_textarea_text(get_browser, 'govuk-textarea', text_too_long, 'id')
     click_button(get_browser, 'Continue', url_notes)
     assert check_error_summary(get_browser)
 
 def test_continue_filled_text_length_under_req(get_browser):
-    navigate_to_notes_page_via_boundary_flow(get_browser)
+    navigate_to_auth_page_via_index(get_browser, url_notes)
     enter_textarea_text(get_browser, 'govuk-textarea', text_under_req, 'id')
     click_button(get_browser, 'Continue', url_next_page)

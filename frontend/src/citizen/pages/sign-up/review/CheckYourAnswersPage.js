f0a6847eb3f9d83bc9716a22c2a8375c34db3c16
Paul.Chester <Paul.Chester@uk.leidos.com>
2024-11-08 11:48:51 +0000

    Fixed styling and more unverifieed fixes EAN-1252

--- frontend/src/citizen/pages/sign-up/review/CheckYourAnswersPage.js
+++ frontend/src/citizen/pages/sign-up/review/CheckYourAnswersPage.js
@@ -54,14 +54,11 @@ export default function CheckYourAnswersPage () {
           <div className='govuk-grid-column-three-quarters'>
             <h2 className='govuk-heading-l'>Check your answers</h2>
             <LocationReviewTable locations={profile.pois} />
-            <br />
             <FloodMessageReviewTable registration={registration} />
-            <br />
             <ContactReviewTable
               profile={profile}
               contactPreferences={contactPreferences}
             />
-            <br />
             <AccountDetailsTable profile={profile} />
           </div>
         </div>

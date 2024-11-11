ae1a9b849541c0cb41173567fd43fc7010fb057e
ronan.peacock <ronan.peacock@leidos.com>
2024-11-11 17:17:15 +0000

    Unzip success

--- backend/server/routes/shapefile/unzip_shapefile.js
+++ backend/server/routes/shapefile/unzip_shapefile.js
@@ -1,10 +1,8 @@
-const {
-  S3Client,
-  GetObjectCommand,
-  PutObjectCommand
-} = require('@aws-sdk/client-s3')
+const { S3Client, GetObjectCommand } = require('@aws-sdk/client-s3')
+const { Upload } = require('@aws-sdk/lib-storage')
+const getSecretKeyValue = require('../../services/SecretsManager')
 const unzipper = require('unzipper')
-const { PassThrough, pipeline } = require('stream')
+const { PassThrough } = require('stream')
 const {
   createGenericErrorResponse
 } = require('../../services/GenericErrorResponse')
@@ -27,56 +25,81 @@ module.exports = [
           })
         }
         const zipFileKey = `zip-uploads/${zipFileName}`
-        const client = new S3Client()
+        const s3Client = new S3Client()
         const s3BucketName = await getSecretKeyValue(
           'nws/aws',
           'bulkUploadBucket'
         )
 
+        console.log(`Attempting to retrieve ${zipFileKey}`)
         // Retrieve zip file from S3
-        const zipFileStream = await client.send(
+        const zipFileStream = await s3Client.send(
           new GetObjectCommand({
             Bucket: s3BucketName,
             Key: zipFileKey
           })
-        ).Body
+        )
+        console.log(`Retrieved! zipFileStream: ${zipFileStream}`)
+
+        if (!zipFileStream) {
+          console.log(`No zip stream`)
+          return h.response({
+            status: 500,
+            message: 'Zip file could not be retrieved from bucket'
+          })
+        }
 
         const outputFolder = `${zipFileKey.replace('.zip', '/')}`
+        console.log(`New output folder is ${outputFolder}`)
 
-        // Unzip and upload each file back to bucket
         await new Promise((resolve, reject) => {
-          pipeline(
-            zipFileStream,
-            unzipper.Parse(),
-            async (entryStream) => {
-              for await (const entry of entryStream) {
-                const filePath = `${outputFolder}${entry.path}`
-                const uploadStream = new PassThrough()
-                await client.send(
-                  new PutObjectCommand({
+          const entryPromises = []
+
+          // Unzip each file in the zip stream
+          zipFileStream.Body.pipe(unzipper.Parse())
+            .on('entry', async (entry) => {
+              const filePath = `${outputFolder}${entry.path}`
+              const uploadStream = new PassThrough()
+
+              // Stream the file back to the bucket, stored in the new subfolder
+              try {
+                const uploadPromise = new Upload({
+                  client: s3Client,
+                  params: {
                     Bucket: s3BucketName,
                     Key: filePath,
                     Body: uploadStream
+                  }
+                })
+                entry.pipe(uploadStream)
+
+                entryPromises.push(
+                  uploadPromise.done().then(() => {
+                    console.log(`Uploaded ${filePath} successfully`)
                   })
                 )
-                entry.pipe(uploadStream)
+              } catch (err) {
+                console.log(`Error uploading ${filePath}: ${err}`)
               }
-            },
-            (error) => {
-              if (error) reject(new Error(`Pipeline Error: ${error.message}`))
-              else resolve()
-            }
-          )
+            })
+            .on('finish', async () => {
+              // Wait for all promises to finish before resolving
+              await Promise.all(entryPromises)
+                .then(() => resolve())
+                .catch((error) => reject(error))
+            })
+            .on('error', (err) => reject(new Error(`Pipeline Error: ${err}`)))
         })
 
         return h.response({
           status: 200,
-          message: 'Files unzipped and uploaded successfully'
+          data: 'Files unzipped and uploaded successfully'
         })
       } catch (error) {
+        console.log(error)
         return h.response({
           status: 500,
-          message: error
+          errorMessage: error
         })
       }
     }
--- frontend/src/organisation/layouts/location/upload/UploadFileLayout.js
+++ frontend/src/organisation/layouts/location/upload/UploadFileLayout.js
@@ -7,7 +7,7 @@ import ErrorSummary from '../../../../common/components/gov-uk/ErrorSummary'
 import { backendCall } from '../../../../common/services/BackendService'
 import { orgManageLocationsUrls } from '../../../routes/manage-locations/ManageLocationsRoutes'
 
-export default function UploadFileLayout ({
+export default function UploadFileLayout({
   uploadMethod // Currently either "bulk" or "shape"
 }) {
   const navigate = useNavigate()
@@ -137,23 +137,22 @@ export default function UploadFileLayout ({
         // Unzip the uploaded file and send output back to S3
         else if (uploadMethod === 'shape') {
           console.log(`Attempting file unzip for ${uniqFileName}`)
-          const unzipResponse = await backendCall(
+          const { data, errorMessage } = await backendCall(
             { zipFileName: uniqFileName },
             'api/shapefile/unzip',
             navigate
           )
-          console.log(`unzipResponse: ${JSON.stringify(unzipResponse)}`)
-          console.log(`${unzipResponse.status}: ${unzipResponse.errorMessage}`)
-          if (unzipResponse.ok) {
+          if (data) {
             // Proceed to next page
             console.log('File unzipped successfully')
           } else {
             // Proceed to error page
-            console.log('File unzip failure')
+            console.log(`Error unzipping file: ${errorMessage}`)
           }
         }
       }
     } catch (err) {
+      console.log(err)
       setUploading(false)
       setErrorFileType('Error uploading file')
     }
@@ -165,65 +164,63 @@ export default function UploadFileLayout ({
 
       <main className='govuk-main-wrapper govuk-!-padding-top-4'>
         <div className='govuk-grid-row'>
-          {!uploading
-            ? (
-              <>
-                {(errorFileType || errorFileSize) && (
-                  <ErrorSummary errorList={[errorFileType, errorFileSize]} />
-                )}
-                <div className='govuk-grid-column-full'>
-                  <h1 className='govuk-heading-l'>Upload file</h1>
-                  <div
-                    className={
+          {!uploading ? (
+            <>
+              {(errorFileType || errorFileSize) && (
+                <ErrorSummary errorList={[errorFileType, errorFileSize]} />
+              )}
+              <div className='govuk-grid-column-full'>
+                <h1 className='govuk-heading-l'>Upload file</h1>
+                <div
+                  className={
                     errorFileSize || errorFileType
                       ? 'govuk-form-group govuk-form-group--error'
                       : 'govuk-form-group'
                   }
-                  >
-                    <p className='govuk-hint'>{fileTypeHint}</p>
-                    {errorFileType && (
-                      <p id='file-upload-1-error' className='govuk-error-message'>
-                        {errorFileType}
-                      </p>
-                    )}
-                    {errorFileSize && (
-                      <p id='file-upload-2-error' className='govuk-error-message'>
-                        {errorFileSize}
-                      </p>
-                    )}
-                    <input
-                      type='file'
-                      className={
+                >
+                  <p className='govuk-hint'>{fileTypeHint}</p>
+                  {errorFileType && (
+                    <p id='file-upload-1-error' className='govuk-error-message'>
+                      {errorFileType}
+                    </p>
+                  )}
+                  {errorFileSize && (
+                    <p id='file-upload-2-error' className='govuk-error-message'>
+                      {errorFileSize}
+                    </p>
+                  )}
+                  <input
+                    type='file'
+                    className={
                       errorFileSize || errorFileType
                         ? 'govuk-file-upload govuk-file-upload--error'
                         : 'govuk-file-upload'
                     }
-                      id='file-upload'
-                      onChange={setValidSelectedFile}
-                    />
-                  </div>
-                  <Button
-                    text='Upload'
-                    className='govuk-button'
-                    onClick={handleUpload}
+                    id='file-upload'
+                    onChange={setValidSelectedFile}
                   />
-                  <Link
-                    onClick={() => navigate(-1)}
-                    className='govuk-body govuk-link inline-link'
-                  >
-                    Cancel
-                  </Link>
                 </div>
-              </>
-              )
-            : (
-              <div className='govuk-!-text-align-centre'>
-                <h1 className='govuk-heading-l'>Uploading</h1>
-                <div className='govuk-body'>
-                  <Spinner size='75' />
-                </div>
+                <Button
+                  text='Upload'
+                  className='govuk-button'
+                  onClick={handleUpload}
+                />
+                <Link
+                  onClick={() => navigate(-1)}
+                  className='govuk-body govuk-link inline-link'
+                >
+                  Cancel
+                </Link>
               </div>
-              )}
+            </>
+          ) : (
+            <div className='govuk-!-text-align-centre'>
+              <h1 className='govuk-heading-l'>Uploading</h1>
+              <div className='govuk-body'>
+                <Spinner size='75' />
+              </div>
+            </div>
+          )}
         </div>
       </main>
     </>

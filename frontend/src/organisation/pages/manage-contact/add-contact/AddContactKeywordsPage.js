c6eea09a10f19d506a5f274f63144550c0ee5448
camille.lecuyer <camille.lecuyer@leidos.com>
2024-11-22 10:10:43 +0000

    lintfix

--- frontend/src/organisation/pages/manage-contact/add-contact/AddContactKeywordsPage.js
+++ frontend/src/organisation/pages/manage-contact/add-contact/AddContactKeywordsPage.js
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router'
 import KeywordsLayout from '../../../layouts/optional-info/KeywordsLayout'
 import { orgManageContactsUrls } from '../../../routes/manage-contacts/ManageContactsRoutes'
 
-export default function AddContactKeywordsPage() {
+export default function AddContactKeywordsPage () {
   const navigate = useNavigate()
 
   const NavigateToNextPage = () => {

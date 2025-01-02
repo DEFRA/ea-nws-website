771f223fda0cf308ec89a5281b7307fc3d9bdcad
thomas barron <thomas.barron@uk.leidos.com>
2025-01-02 16:15:50 +0000

    WIP: fixing citizen banner

--- frontend/src/common/components/custom/CitizenAccountNavigation.js
+++ frontend/src/common/components/custom/CitizenAccountNavigation.js
@@ -1,11 +1,13 @@
 import SubNavigation from './SubNavigation'
 
 export default function CitizenAccountNavigation ({ currentPage }) {
+  // get flood warnings need to be black and not blue
   const pages = [
-    { title: 'Home', link: '/home' },
-    { title: 'Your email addresses and numbers', link: '/managecontacts' },
-    { title: 'Your account', link: '/account' },
-    { title: 'Contact us', link: '/contact' }
+    {title: 'Get glood warnings'},
+    { title: 'Locations and message preferences', link: '/home' },
+    { title: 'Email addresses and numbers', link: '/managecontacts' },
+    { title: 'Account', link: '/account' },
+    { title: 'Contact', link: '/contact' }
   ]
 
   return <SubNavigation pages={pages} currentPage={currentPage} />

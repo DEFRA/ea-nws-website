5c3235e73a414a2f37ae8577c8210a76c6fb07b3
jibran.haider <muhammadjibran.haider@leidos.com>
2025-01-07 17:14:04 +0000

    Create layout for unmatched locations dashboard. EAN-1117

---
+++ frontend/src/organisation/layouts/location/upload-locations-with-csv/unmatched-locations/UnmatchedLocationsDashboardLayout.js
@@ -0,0 +1,187 @@
+import { Link, useLocation } from 'react-router-dom'
+import Button from '../../../../../common/components/gov-uk/Button'
+import NotificationBanner from '../../../../../common/components/gov-uk/NotificationBanner'
+import WarningText from '../../../../../common/components/gov-uk/WarningText'
+
+export default function UnmatchedLocationsDashboardLayout ({
+  navigateToNextPage,
+  flow
+}) {
+  const location = useLocation()
+  const locations = []
+
+  // Default values for null location.state
+  const addedLocations = location?.state?.added || 0
+  const notAddedLocations = location?.state?.notAdded || 0
+
+  const handleButton = () => {
+    // TODO: Add route to next page
+  }
+
+  const unmatchedLocationText =
+    (flow === 'unmatched-locations-not-found' && 'not found') ||
+    (flow === 'unmatched-locations-not-in-england' && 'not in England')
+
+  // Location info
+  const info = () => {
+    if (flow === 'unmatched-locations-not-found') {
+      return (
+        <>
+          <p>
+            Some of the locations uploaded could not be found. This may be
+            because the information in the template was not added correctly or
+            has details missing. For example, the X and Y coordinates contained
+            a letter in them or the street name did not match the postcode.
+          </p>
+          <p>
+            You can choose how you want to find these locations manually in this
+            account, then add them to your organisation's locations.
+          </p>
+        </>
+      )
+    } else if (flow === 'unmatched-locations-not-in-england') {
+      return (
+        <>
+          <p>
+            Some of the locations uploaded are not in England and cannot be
+            added to this account.
+          </p>
+          <p>
+            If you think this is not correct and some locations are in England
+            you can check each location's details and change them, either by
+            using a postcode, X and Y coordinates or by dropping a pin on a map.
+          </p>
+        </>
+      )
+    }
+  }
+
+  // Location warning text
+  const warningText = () => {
+    if (flow === 'unmatched-locations-not-found') {
+      return (
+        <WarningText
+          text={
+            <>
+              Any locations not found cannot be added to this account and will
+              not be saved if you do not find them now.
+              <br />
+              <br />
+              If you want to add these locations to this account later you'll
+              need to either reupload them in a new CSV file or add each one
+              manually to this account.
+            </>
+          }
+        />
+      )
+    } else if (flow === 'unmatched-locations-not-in-england') {
+      return (
+        <WarningText
+          text={
+            <>
+              Locations not in England cannot be added to this account and will
+              not be saved if you do not check them now.
+              <br />
+              <br />
+              If you want to check these locations later and then add them to
+              this account you'll need to either reupload them in a new CSV file
+              or add each one manually.
+            </>
+          }
+        />
+      )
+    }
+  }
+
+  return (
+    <>
+      <NotificationBanner
+        className='govuk-notification-banner govuk-notification-banner--success govuk-!-margin-top-5'
+        title='Success'
+        text={`${addedLocations} locations added`}
+      />
+      <main className='govuk-main-wrapper govuk-!-padding-top-1'>
+        <div className='govuk-grid-row'>
+          <div className='govuk-grid-column-one-half'>
+            <h1 className='govuk-heading-l'>
+              {notAddedLocations} locations {unmatchedLocationText}
+            </h1>
+            <div className='govuk-body'>
+              {info()}
+
+              <Button
+                className='govuk-button govuk-button--secondary govuk-!-margin-top-3'
+                text={`Print locations ${unmatchedLocationText}`}
+                // onClick={handleButton} // TODO: Add print format
+              />
+
+              {warningText()}
+            </div>
+          </div>
+
+          <div className='govuk-grid-column-full'>
+            <p className='govuk-!-margin-bottom-0 locations-table-panel'>
+              {notAddedLocations}
+              {notAddedLocations === 1 ? ' location' : ' locations'}{' '}
+            </p>
+            <table class='govuk-table govuk-table--small-text-until-tablet'>
+              <thead class='govuk-table__head'>
+                <tr class='govuk-table__row'>
+                  <th scope='col' class='govuk-table__header'>
+                    Location name
+                  </th>
+                  <th scope='col' class='govuk-table__header'>
+                    Address uploaded
+                  </th>
+                  <th scope='col' class='govuk-table__header'>
+                    Postcode
+                  </th>
+                  <th scope='col' class='govuk-table__header'>
+                    X and Y coordinates
+                  </th>
+                  <th scope='col' class='govuk-table__header' />
+                </tr>
+              </thead>
+              <tbody class='govuk-table__body'>
+                {locations &&
+                  locations.map((location, index) => {
+                    return (
+                      <tr class='govuk-table__row' key={index}>
+                        <th scope='row' class='govuk-table__header'>
+                          {location.additionals.locationName}
+                        </th>
+                        <td class='govuk-table__cell'>
+                          {location.additionals.other.full_address}
+                        </td>
+                        <td class='govuk-table__cell'>
+                          {location.additionals.other.postcode}
+                        </td>
+                        <td class='govuk-table__cell'>
+                          {location.additionals.other.x_coordinate},{' '}
+                          {location.additionals.other.y_coordinate}
+                        </td>
+                        <td class='govuk-table__cell'>
+                          <Link>Find this location</Link>
+                        </td>
+                      </tr>
+                    )
+                  })}
+              </tbody>
+            </table>
+
+            <div className='govuk-body govuk-!-padding-top-5'>
+              <Button
+                text={`Finish ${
+                  (flow === 'unmatched-locations-not-found' && 'finding') ||
+                  (flow === 'unmatched-locations-not-in-england' && 'checking')
+                } locations`}
+                className='govuk-button govuk-button'
+                onClick={handleButton}
+              />
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  )
+}
--- frontend/src/organisation/pages/manage-locations/add-location/upload-locations-with-csv/unmatched-locations/not-found/LocationsNotFoundDashboardPage.js
+++ frontend/src/organisation/pages/manage-locations/add-location/upload-locations-with-csv/unmatched-locations/not-found/LocationsNotFoundDashboardPage.js
@@ -1,129 +1,21 @@
-import { Link, useLocation } from 'react-router-dom'
-import Button from '../../../../../../../common/components/gov-uk/Button'
-import NotificationBanner from '../../../../../../../common/components/gov-uk/NotificationBanner'
-import WarningText from '../../../../../../../common/components/gov-uk/WarningText'
+import { React } from 'react'
+import { useNavigate } from 'react-router'
+import UnmatchedLocationsDashboardLayout from '../../../../../../layouts/location/upload-locations-with-csv/unmatched-locations/UnmatchedLocationsDashboardLayout'
+import { orgManageLocationsUrls } from '../../../../../../routes/manage-locations/ManageLocationsRoutes'
 
 export default function LocationsNotFoundDashboardPage () {
-  const location = useLocation()
-  const locations = []
+  const navigate = useNavigate()
 
-  // Default values for null location.state
-  const addedLocations = location?.state?.added || 0
-  const notAddedLocations = location?.state?.notAdded || 0
-
-  const handleButton = () => {
-    // TODO: Add route to next page
-  }
+  const navigateToNextPage = () =>
+    // TODO: navigate to correct page
+    navigate(orgManageLocationsUrls.unmatchedLocations.notFound.find)
 
   return (
     <>
-      <NotificationBanner
-        className='govuk-notification-banner govuk-notification-banner--success govuk-!-margin-top-5'
-        title='Success'
-        text={`${addedLocations} locations added`}
+      <UnmatchedLocationsDashboardLayout
+        navigateToNextPage={navigateToNextPage}
+        flow='unmatched-locations-not-found'
       />
-      <main className='govuk-main-wrapper govuk-!-padding-top-1'>
-        <div className='govuk-grid-row'>
-          <div className='govuk-grid-column-one-half'>
-            <h1 className='govuk-heading-l'>
-              {notAddedLocations} locations not found
-            </h1>
-            <div className='govuk-body'>
-              <p>
-                Some of the locations uploaded could not be found. This may be
-                because the information in the template was not added correctly
-                or has details missing. For example, the X and Y coordinates
-                contained a letter in them or the street name did not match the
-                postcode.
-              </p>
-
-              <p>
-                You can choose how you want to find these locations manually in
-                this account, then add them to your organisation's locations.
-              </p>
-
-              <Button
-                className='govuk-button govuk-button--secondary govuk-!-margin-top-3'
-                text='Print locations not found'
-                // onClick={handleButton} // TODO: Add print format
-              />
-
-              <WarningText
-                text={
-                  <>
-                    Any locations not found cannot be added to this account and
-                    will not be saved if you do not find them now.
-                    <br />
-                    <br />
-                    If you want to add these locations to this account later
-                    you'll need to either reupload them in a new CSV file or add
-                    each one manually to this account.
-                  </>
-                }
-              />
-            </div>
-          </div>
-
-          <div className='govuk-grid-column-full'>
-            <p className='govuk-!-margin-bottom-0 locations-table-panel'>
-              {notAddedLocations}
-              {notAddedLocations === 1 ? ' location' : ' locations'}{' '}
-            </p>
-            <table class='govuk-table govuk-table--small-text-until-tablet'>
-              <thead class='govuk-table__head'>
-                <tr class='govuk-table__row'>
-                  <th scope='col' class='govuk-table__header'>
-                    Location name
-                  </th>
-                  <th scope='col' class='govuk-table__header'>
-                    Address uploaded
-                  </th>
-                  <th scope='col' class='govuk-table__header'>
-                    Postcode
-                  </th>
-                  <th scope='col' class='govuk-table__header'>
-                    X and Y coordinates
-                  </th>
-                  <th scope='col' class='govuk-table__header' />
-                </tr>
-              </thead>
-              <tbody class='govuk-table__body'>
-                {locations &&
-                  locations.map((location, index) => {
-                    return (
-                      <tr class='govuk-table__row' key={index}>
-                        <th scope='row' class='govuk-table__header'>
-                          {location.additionals.locationName}
-                        </th>
-                        <td class='govuk-table__cell'>
-                          {location.additionals.other.full_address}
-                        </td>
-                        <td class='govuk-table__cell'>
-                          {location.additionals.other.postcode}
-                        </td>
-                        <td class='govuk-table__cell'>
-                          {location.additionals.other.x_coordinate},{' '}
-                          {location.additionals.other.y_coordinate}
-                        </td>
-                        <td class='govuk-table__cell'>
-                          <Link>Find this location</Link>
-                        </td>
-                      </tr>
-                    )
-                  })}
-              </tbody>
-            </table>
-
-            <div className='govuk-body govuk-!-padding-top-5'>
-              <Button
-                text='Finish finding locations'
-                className='govuk-button govuk-button'
-                onClick={handleButton}
-              />
-            </div>
-          </div>
-        </div>
-      </main>
     </>
   )
 }

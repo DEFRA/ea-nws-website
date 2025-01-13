c3f0ad1b4887cd08e38c9e7cea5e1715006013b7
thomas barron <thomas.barron@uk.leidos.com>
2025-01-13 13:44:30 +0000

    fix citizen banner

--- frontend/src/common/components/gov-uk/PhaseBanner.js
+++ frontend/src/common/components/gov-uk/PhaseBanner.js
@@ -5,16 +5,16 @@ export default function Header () {
     <>
       <div className='govuk-phase-banner govuk-body'>
         <div className='govuk-width-container'>
-          <p>
+          <p className='govuk-!-margin-bottom-0 '  style={{ display: 'flex', alignItems: 'center' }}>
             <strong className='govuk-tag govuk-phase-banner__content__tag'>
-              Alpha
+              Beta
             </strong>
-            <span className='govuk-phase-banner__text'>
-              This is a new service – your{' '}
+            <span className = 'govuk-phase-banner__text'>
+              This is a new service. Help us improve it and {' '}
               <Link className='govuk-link' to='/signup/feedback' target='_blank'>
-                feedback
+                give your feedback (opens in new tab)
               </Link>{' '}
-              will help us to improve it.
+             
             </span>
           </p>
         </div>

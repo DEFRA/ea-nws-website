1a8cccf5fc3fe1a6c9b760234f61b06b25828db2
jibran.haider <muhammadjibran.haider@leidos.com>
2025-01-15 15:27:52 +0000

    Update process_status and elasticache files from EAN-1064. EAN-1117

--- backend/server/routes/bulk_uploads/process_status.js
+++ backend/server/routes/bulk_uploads/process_status.js
@@ -2,7 +2,18 @@ const {
   createGenericErrorResponse
 } = require('../../services/GenericErrorResponse')
 
-const { getJsonData } = require('../../services/elasticache')
+const {
+  findLocationByName,
+  getJsonData,
+  setJsonData
+} = require('../../services/elasticache')
+
+const isDuplicate = async (orgId, locationName) => {
+  const locationArr = await findLocationByName(orgId, locationName)
+  return locationArr.length !== 0
+}
+
+const DUPLICATE = 'duplicate'
 
 module.exports = [
   {
@@ -13,13 +24,44 @@ module.exports = [
         if (!request.payload) {
           return createGenericErrorResponse(h)
         }
-        const { fileName } = request.payload
+        const { orgId, fileName } = request.payload
 
         if (fileName) {
           const elasticacheKey = 'bulk_upload:' + fileName.split('.')[0]
 
           const result = await getJsonData(elasticacheKey)
           if (result) {
+            if (result.data) {
+              // Check invalid locations for duplicates
+              result.data.invalid.forEach(async (location) => {
+                if (await isDuplicate(orgId, location.Location_name)) {
+                  // An invalid location should already have at least one error
+                  // but do not assume this is the case
+                  if (Array.isArray(location.error)) {
+                    location.error.push(DUPLICATE)
+                  } else {
+                    location.error = [DUPLICATE]
+                  }
+                }
+              })
+              // Now check valid locations and move any duplicates
+              // to invalid locations and mark as duplicates. Note that
+              // we don't use forEach in this case because we could be
+              // removing elements from valid locations.
+              for (let i = 0; i < result.data.valid.length; i++) {
+                const location = result.data.valid[i]
+                if (await isDuplicate(orgId, location.Location_name)) {
+                  result.data.valid.splice(i, 1)
+                  // It seems reasonable to assume that a valid location
+                  // will have no existing errors at this stage
+                  location.error = [DUPLICATE]
+                  result.data.invalid.push(location)
+                  --i
+                }
+              }
+              // Write any changes back to elasticache
+              await setJsonData(elasticacheKey, result)
+            }
             return h.response({ status: 200, data: result })
           } else {
             return h.response({ status: 200 })

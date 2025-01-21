a1cbaea5dc7cf6b0cc65744aa91ab57fe21fec12
Paul.Chester <Paul.Chester@uk.leidos.com>
2025-01-21 16:55:22 +0000

    Added gtm for analytics EAN-1426

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import TagManager from 'react-gtm-module'
 import { Provider } from 'react-redux'
 import App from './App'
 import './common/css/custom.css'
 import store from './common/redux/store'
+import { backendCall } from './common/services/BackendService'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const getGtmId = async () => {
+  const { data } = await backendCall(
+    'data',
+    'api/values/gtm'
+  )
+  if (data) {
+    return data
+  } else {
+    return null
+  }
+}
+
+const tagManagerArgs = {
+  gtmId: await getGtmId()
+}
+
+TagManager.initialize(tagManagerArgs)
 
 root.render(
   <React.StrictMode>

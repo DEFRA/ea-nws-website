516b781ef26f1c7541c922ef8937793ab05997fe
camille.lecuyer <camille.lecuyer@leidos.com>
2025-01-24 09:23:24 +0000

    merge fixes

--- frontend/src/citizen/pages/sign-up/success/SignUpSuccessPage.js
+++ frontend/src/citizen/pages/sign-up/success/SignUpSuccessPage.js
@@ -5,7 +5,7 @@ import Button from '../../../../common/components/gov-uk/Button'
 import ConfirmationPanel from '../../../../common/components/gov-uk/Panel'
 import { backendCall } from '../../../../common/services/BackendService'
 
-export default function SignUpSuccessPage() {\
+export default function SignUpSuccessPage() {
   const navigate = useNavigate()
   const profile = useSelector((state) => state.session.profile)
   const [servicePhase, setServicePhase] = useState(false)

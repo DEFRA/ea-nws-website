19275edb7d51f4c6011c1770f52fe752436ae089
ronan.peacock <ronan.peacock@leidos.com>
2025-02-07 16:35:59 +0000

    Integrate real data

--- frontend/src/organisation/pages/manage-locations/link-locations/LinkLocationsPage.js
+++ frontend/src/organisation/pages/manage-locations/link-locations/LinkLocationsPage.js
@@ -1,18 +1,25 @@
-import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
+import { distance, point } from '@turf/turf'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router'
 import { Link } from 'react-router-dom'
 import BackLink from '../../../../common/components/custom/BackLink'
 import Button from '../../../../common/components/gov-uk/Button'
 import Checkbox from '../../../../common/components/gov-uk/CheckBox'
 import { setCurrentLocationChildrenIDs } from '../../../../common/redux/userSlice'
+import {
+  getSurroundingFloodAreas,
+  getSurroundingFloodAreasFromShape
+} from '../../../../common/services/WfsFloodDataService'
 import { infoUrls } from '../../../routes/info/InfoRoutes'
 
 export default function LinkLocationsPage() {
   const navigate = useNavigate()
   const dispatch = useDispatch()
 
+  const currentLocation = useSelector((state) => state.session.currentLocation)
   const [childrenIDs, setChildrenIDs] = useState([])
+  const [floodAreas, setFloodAreas] = useState([])
 
   const handleCheckboxChange = (areaId) => {
     setChildrenIDs((prev) =>
@@ -27,45 +34,57 @@ export default function LinkLocationsPage() {
     navigate('#') // TODO: Navigate to correct next page
   }
 
-  // TODO: Implement real data
-  const tempData = [
-    {
-      id: '1234',
-      name: 'Properties closest to the River Thames',
-      address: 'All Saints Church, Bisham to Little Marlow',
-      coordinates: { lat: 51.57, lon: -0.78 },
-      alertTypes: ['Severe', 'Warning'],
-      distance: 'Xm',
-      areaType: 'Flood area'
-    },
-    {
-      id: '5678',
-      name: 'River Thames at Bisham village',
-      address: 'Bisham village and Marlow town',
-      coordinates: { lat: 51.57, lon: -0.78 },
-      alertTypes: ['Severe', 'Warning'],
-      distance: 'Xm',
-      areaType: 'Flood area'
-    },
-    {
-      id: '9101',
-      name: 'River Thames at Hurley and Harleyford',
-      address: 'Hurley and Harleyford, Berkshire',
-      coordinates: { lat: 51.55, lon: -0.77 },
-      alertTypes: ['Warning', 'Alert'],
-      distance: 'Xm',
-      areaType: 'Flood area'
-    },
-    {
-      id: '1121',
-      name: 'River Thames from Hurley to Cookham',
-      address: 'Hurley to Cookham, Berkshire',
-      coordinates: { lat: 51.56, lon: -0.76 },
-      alertTypes: ['Alert'],
-      distance: 'Xm',
-      areaType: 'Flood area'
+  useEffect(() => {
+    async function fetchFloodAreas() {
+      if (!currentLocation) return
+
+      let alertAreas = []
+      let warningAreas = []
+      if (currentLocation.geometry) {
+        const { alertArea, warningArea } =
+          await getSurroundingFloodAreasFromShape(currentLocation.geometry)
+
+        alertAreas = alertArea.features
+        warningAreas = warningArea.features
+      } else {
+        const { alertArea, warningArea } = await getSurroundingFloodAreas(
+          currentLocation.coordinates.latitude,
+          currentLocation.coordinates.longitude
+        )
+        alertAreas = alertArea.features
+        warningAreas = warningArea.features
+      }
+
+      const locationPoint = point([
+        currentLocation.coordinates.longitude,
+        currentLocation.coordinates.latitude
+      ])
+
+      const floodAreasWithDistances = [...alertAreas, ...warningAreas].map(
+        (area) => {
+          const floodPoint = point([
+            area.geometry.coordinates[0][0][0][0],
+            area.geometry.coordinates[0][0][0][1]
+          ])
+
+          const distanceM = distance(locationPoint, floodPoint, {
+            units: 'meters'
+          })
+
+          return {
+            ...area,
+            properties: {
+              ...area.properties,
+              distance: `${Math.round(distanceM)}m`
+            }
+          }
+        }
+      )
+      setFloodAreas(floodAreasWithDistances)
     }
-  ]
+
+    fetchFloodAreas()
+  }, [currentLocation])
 
   const table = (
     <>
@@ -109,19 +128,20 @@ export default function LinkLocationsPage() {
           </tr>
         </thead>
         <tbody className='govuk-table__body'>
-          {tempData.map((area) => (
+          {floodAreas.map((area) => (
             <tr key={area.id} className='govuk-table__row'>
               <td className='govuk-table__cell'>
                 <Link to='#' className='govuk-link'>
-                  {area.name}
+                  {area.properties.TA_Name}
                 </Link>
               </td>
-              <td className='govuk-table__cell'>{area.distance}</td>
-              <td className='govuk-table__cell'>{area.areaType}</td>
               <td className='govuk-table__cell'>
-                {area.severeWarnings} severe flood warnings <br />
-                {area.warnings} flood warnings <br />
-                {area.alerts} flood alerts
+                {area.properties.distance ?? 'Xm'}
+              </td>
+              <td className='govuk-table__cell'>{area.properties.category}</td>
+              <td className='govuk-table__cell'>
+                0 severe flood warnings <br />
+                0 flood warnings <br />0 flood alerts
               </td>
               <td className='govuk-table__cell'>
                 <div
@@ -132,9 +152,11 @@ export default function LinkLocationsPage() {
                   }}
                 >
                   <Checkbox
-                    value={area.id}
-                    onChange={() => handleCheckboxChange(area.id)}
-                    checked={childrenIDs.includes(area.id)}
+                    value={area.properties.FWS_TACODE}
+                    onChange={() =>
+                      handleCheckboxChange(area.properties.FWS_TACODE)
+                    }
+                    checked={childrenIDs.includes(area.properties.FWS_TACODE)}
                   />
                 </div>
               </td>
@@ -154,8 +176,9 @@ export default function LinkLocationsPage() {
             <br />
             <h1 className='govuk-heading-l'>Select nearby flood areas</h1>
             <p className='govuk-body'>
-              Location_IDX is near to these flood areas. You can select 1 or
-              more nearby flood areas you want to link this location to.
+              {currentLocation.name} is near to these flood areas. You can
+              select 1 or more nearby flood areas you want to link this location
+              to.
               <br />
               <Link to={infoUrls.floodAreas} className='govuk-link inline-link'>
                 What are flood areas?

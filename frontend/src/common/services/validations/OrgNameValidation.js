3b8cf48ab2957af98dcf41cfa5364307c041d347
ronan.peacock <ronan.peacock@leidos.com>
2025-04-17 10:37:26 +0000

    Validation changes (awaiting confirmation from UCD)

--- frontend/src/common/services/validations/OrgNameValidation.js
+++ frontend/src/common/services/validations/OrgNameValidation.js
@@ -3,10 +3,17 @@ const orgNameValidation = (orgName) => {
     return "Enter your organisation's name"
   }
 
-  const orgNameLength = 50
+  const orgNameLength = 160
   if (orgName.length > orgNameLength) {
     return `Organisation name must be ${orgNameLength} characters or fewer`
   }
+
+  // Only letters, digits, spaces, and any of . , : ; # -
+  const validPattern = /^[A-za-z0-9 .,:;#-]+$/
+  if (!validPattern.test(orgName)) {
+    return `TODO: Change once message is confirmed with UCD`
+  }
+
   return ''
 }
 

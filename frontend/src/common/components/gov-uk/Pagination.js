155d9cc10da1d90a370aa2a0a6f20f62aa6d9b7e
cameron.purves <cameron.purves@leidos.com>
2025-05-09 13:29:24 +0000

    removed duplicates

--- frontend/src/common/components/gov-uk/Pagination.js
+++ frontend/src/common/components/gov-uk/Pagination.js
@@ -113,9 +113,7 @@ export default function Pagination({
                     <path d='m6.5938-0.0078125-6.7266 6.7266 6.7441 6.4062 1.377-1.449-4.1856-3.9768h12.896v-2h-12.984l4.2931-4.293-1.414-1.414z' />
                   </svg>
                   <span className='govuk-pagination__link-title'>Previous</span>
-                  <span className='govuk-visually-hidden'>
-                    Navigate to previous page: {currentPage - 1} of {totalPages}
-                  </span>
+
                   <span className='govuk-pagination__link-label'>
                     {currentPage - 1} of {totalPages}
                   </span>
@@ -148,9 +146,6 @@ export default function Pagination({
                     <path d='m8.107-0.0078125-1.4136 1.414 4.2926 4.293h-12.986v2h12.896l-4.1855 3.9766 1.377 1.4492 6.7441-6.4062-6.7246-6.7266z' />
                   </svg>
                   <span className='govuk-pagination__link-title'>Next</span>
-                  <span className='govuk-visually-hidden'>
-                    Navigate to next page: {currentPage + 1} of {totalPages}
-                  </span>
                   <span className='govuk-pagination__link-label'>
                     {currentPage + 1} of {totalPages}
                   </span>
@@ -184,7 +179,6 @@ export default function Pagination({
                 <path d='m6.5938-0.0078125-6.7266 6.7266 6.7441 6.4062 1.377-1.449-4.1856-3.9768h12.896v-2h-12.984l4.2931-4.293-1.414-1.414z' />
               </svg>
               <span className='govuk-pagination__link-title'>Previous</span>
-              <span className='govuk-visually-hidden'> page</span>
             </a>
           </div>
           <ul className='govuk-pagination__list'>
@@ -202,9 +196,7 @@ export default function Pagination({
               }}
               aria-label={`Navigate to next page: ${currentPage + 1}`}
             >
-              <span className='govuk-pagination__link-title'>
-                Next<span className='govuk-visually-hidden'> page</span>
-              </span>
+              <span className='govuk-pagination__link-title'>Next</span>
               <svg
                 className='govuk-pagination__icon govuk-pagination__icon--next'
                 xmlns='http://www.w3.org/2000/svg'

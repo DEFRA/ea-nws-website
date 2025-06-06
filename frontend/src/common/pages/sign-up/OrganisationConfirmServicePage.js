716bbe6322a16cada84574638fe6aa98b4fd88d7
shakir.khan <shakir.m.khan@uk.leidos.com>
2025-06-06 14:30:51 +0000

    EAN-2010: Remove <br> from heading tag

--- frontend/src/common/pages/sign-up/OrganisationConfirmServicePage.js
+++ frontend/src/common/pages/sign-up/OrganisationConfirmServicePage.js
@@ -14,9 +14,8 @@ export default function OrganisationConfirmServicePage () {
         <div className='govuk-grid-row'>
           <div className='govuk-grid-column-two-thirds'>
             <div className='govuk-body govuk-!-margin-top-6'>
-              <h1 className='govuk-heading-m govuk-!-margin-top-6'>
-                You've chosen the professional<br />
-                (organisation) service
+              <h1 className='govuk-heading-l govuk-!-margin-top-6'>
+                You've chosen the professional (organisation) service
               </h1>
               <p className='govuk-!-margin-top-6'>
                 To continue you'll need your:

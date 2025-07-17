8ab16a9d7e0a88660efe7f590842ee79b8e99b52
shakir.khan <shakir.m.khan@uk.leidos.com>
2025-07-17 10:27:23 +0000

    EAN-2217: table rows to stacked layout for mobile for citizen locations table

--- frontend/src/citizen/pages/manage-locations/SubscribedLocationTable.js
+++ frontend/src/citizen/pages/manage-locations/SubscribedLocationTable.js
@@ -142,7 +142,7 @@ export default function SubscribedLocationTable({ setError }) {
     }
 
     return (
-      <table className='govuk-table'>
+      <table className='govuk-table responsive-table'>
         <thead className='govuk-table__head'>
           <tr className='govuk-table__row'>
             <th
@@ -157,7 +157,7 @@ export default function SubscribedLocationTable({ setError }) {
         </thead>
         <tbody className='govuk-table__body'>
           {nonGroupFloodLocation.map((location, index) => (
-            <tr key={index} className='govuk-table__row'>
+            <tr key={index} className='govuk-table__row non-grouped'>
               {addressColumn(location)}
               {locations.length === 1 && <td className='govuk-table__cell' />}
               {locations.length === 1 && <td className='govuk-table__cell' />}
@@ -171,7 +171,7 @@ export default function SubscribedLocationTable({ setError }) {
             Object.entries(groupFloodLocation).map(
               ([key, locations], index) => (
                 <>
-                  <tr key={index} className='govuk-table__row'>
+                  <tr key={index} className='govuk-table__row grouped'>
                     <td
                       className='govuk-table__cell govuk-!-width-full govuk-!-padding-top-6'
                       style={{ borderBottom: '2px solid #fff' }}

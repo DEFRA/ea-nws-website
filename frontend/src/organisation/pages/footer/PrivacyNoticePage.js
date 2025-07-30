66aa711787570261a25ed0376c9b489343c5f3eb
jibran.haider <muhammadjibran.haider@leidos.com>
2025-07-30 12:40:12 +0000

    Create layout for privacy notice and add one for organisation. EAN-1216

--- frontend/src/organisation/pages/footer/PrivacyNoticePage.js
+++ frontend/src/organisation/pages/footer/PrivacyNoticePage.js
@@ -1,43 +1,9 @@
-import React, { useEffect, useState } from 'react'
-import { Helmet } from 'react-helmet'
-import { useNavigate } from 'react-router'
-import BackLink from '../../../common/components/custom/BackLink'
-import PrivateBetaPrivacyNoticePage from '../../../common/pages/private-beta/PrivateBetaPrivacyNoticePage'
-import { backendCall } from '../../../common/services/BackendService'
+import PrivacyNoticeLayout from '../../../common/layouts/footer-link-layouts/PrivacyNoticeLayout'
 
-export default function PrivacyNoticePage () {
-  const navigate = useNavigate()
-
-  const [servicePhase, setServicePhase] = useState(false)
-
-  async function getServicePhase () {
-    const { data } = await backendCall(
-      'data',
-      'api/service/get_service_phase'
-    )
-    setServicePhase(data)
-  }
-
-  useEffect(() => {
-    getServicePhase()
-  }, [])
-
+export default function PrivacyNoticePage() {
   return (
     <>
-      <Helmet>
-        <title>Privacy notice - Get flood warnings (professional) - GOV.UK</title>
-      </Helmet>
-      {servicePhase === 'beta'
-        ? (
-          <>
-            <PrivateBetaPrivacyNoticePage />
-          </>
-          )
-        : (
-          <>
-            <BackLink onClick={() => navigate(-1)} />
-            <h1 className='govuk-heading-l'>Privacy notice</h1>
-          </>)}
+      <PrivacyNoticeLayout isOrg={true} />
     </>
   )
 }

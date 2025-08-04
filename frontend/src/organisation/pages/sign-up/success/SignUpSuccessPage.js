a266c4b11e9a71dd8a5141388681f6e8ce8b50e1
ronan.peacock <ronan.peacock@leidos.com>
2025-08-04 12:19:16 +0000

    Change h1 to h2

--- frontend/src/organisation/pages/sign-up/success/SignUpSuccessPage.js
+++ frontend/src/organisation/pages/sign-up/success/SignUpSuccessPage.js
@@ -109,9 +109,9 @@ export default function SignUpSuccessPage() {
             <BackLink onClick={() => navigate(-1)} />
             <ConfirmationPanel title='Organisation details submitted for approval' />
             <div className='govuk-body govuk-!-margin-top-6'>
-              <h1 className='govuk-heading-m govuk-!-margin-top-6'>
+              <h2 className='govuk-heading-m govuk-!-margin-top-6'>
                 What happens next
-              </h1>
+              </h2>
               <p className='govuk-!-margin-top-6'>
                 We'll check the details you've submitted so we can verify your
                 organisation.

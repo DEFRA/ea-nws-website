a785a25ac15c085dfee674e0fda26c41e2bbe3cc
shakir.khan <shakir.m.khan@uk.leidos.com>
2025-08-19 08:12:12 +0000

    EAN-1770: Fixed sorting using location name

--- frontend/src/organisation/pages/manage-locations/view-location/locations-dashboard/ViewLocationsDashboardPage.js
+++ frontend/src/organisation/pages/manage-locations/view-location/locations-dashboard/ViewLocationsDashboardPage.js
@@ -198,8 +198,8 @@ export default function ViewLocationsDashboardPage() {
 
       // Sort objects by location name alphabetically
       locationsUpdate.sort((a, b) => {
-        const nameA = (a.additionals.locationName || '').toLowerCase()
-        const nameB = (b.additionals.locationName || '').toLowerCase()
+        const nameA = (a.name || '').toLowerCase()
+        const nameB = (b.name || '').toLowerCase()
         if (nameA < nameB) return -1
         if (nameA > nameB) return 1
         return 0

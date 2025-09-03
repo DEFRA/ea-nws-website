5bd4bee6bf90b17ef762bf227bfa5b001ccc43bb
shakir.khan <shakir.m.khan@uk.leidos.com>
2025-09-03 13:21:15 +0000

    EAN-2369: Fixed placename search that is withing the warning area

--- frontend/src/citizen/pages/manage-locations/add-location/LocationSearchResultsPage.js
+++ frontend/src/citizen/pages/manage-locations/add-location/LocationSearchResultsPage.js
@@ -18,9 +18,7 @@ export default function LocationSearchResultsPage() {
     } else if (isInWarningArea || isInAlertArea) {
       navigate('/manage-locations/add/location-in-flood-areas')
     } else if (isWithinWarningAreaProximity || isWithinAlertAreaProximity) {
-      navigate(
-        '/manage-locations/add/location-cannot-get-direct-flood-messages'
-      )
+      navigate('/manage-locations/add/location-near-flood-areas')
     } else if (isError) {
       navigate('/error')
     } else {
